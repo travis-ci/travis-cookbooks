16e40162a906ec9daac03e14f517e99450bff635
Michael S. Klishin <michael@novemberain.com>
2011-06-15 22:51:10 +0400

    It is rvm::multi

--- vagrant_base/rvm/recipes/multi.rb
+++ vagrant_base/rvm/recipes/multi.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: rvm
-# Recipe:: travis
+# Recipe:: multi
 # Copyright 2011, Travis CI development team
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
