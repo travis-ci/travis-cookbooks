5a69b967a4650005b21d82d3a377e7dbea106a57
Michael S. Klishin <michael@novemberain.com>
2011-06-19 20:34:03 +0400

    Customized imagemagick cookbook for our (rmagick) needs
     * imagemagick::rmagick installed rmagick into the global gemset, we do not need that
     * however, we do want extra libraries from imagemagick::rmagick recipe, so I moved that to the default recipe

---
+++ vagrant_base/imagemagick/metadata.rb
@@ -0,0 +1,12 @@
+maintainer       "Opscode, Inc."
+maintainer_email "cookbooks@opscode.com"
+license          "Apache 2.0"
+description      "Installs/Configures imagemagick"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.2.0"
+
+recipe "imagemagick", "Installs imagemagick package"
+
+%w{fedora centos rhel ubuntu debian}.each do |os|
+  supports os
+end
