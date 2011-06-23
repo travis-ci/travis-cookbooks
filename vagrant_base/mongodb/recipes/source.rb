ded78f0f6e494203b8ad01d3813305fe076381dc
Sebastian Kippe <info@sebastiankippe.de>
2011-06-23 12:54:29 +0200

    remove unnecessary stuff from mongodb cookbook, fix the rest

--- vagrant_base/mongodb/recipes/source.rb
+++ vagrant_base/mongodb/recipes/source.rb
@@ -21,37 +21,36 @@
 
 platform = node[:kernel][:machine]
 
+include_recipe "build-essential"
+
 user "mongodb" do
   comment "MongoDB Administrator"
   system true
   shell "/bin/false"
 end
 
-[node[:mongodb][:dir], "#{node[:mongodb][:dir]}/bin"].each do |dir|
+remote_file "/tmp/mongodb-#{node[:mongodb][:version]}.tar.gz" do
+  source node[:mongodb][:source]
+  checksum node[:mongodb][platform][:checksum]
+  action :create_if_missing
+  not_if { `ps -A -o command | grep "[m]ongo"`.include? node[:mongodb][:version] }
+end
+
+[node[:mongodb][:dir], "#{node[:mongodb][:dir]}/bin", node[:mongodb][:datadir]].each do |dir|
   directory dir do
     owner "mongodb"
     group "mongodb"
-    mode "0755"
+    mode 0755
     recursive true
   end
 end
 
-unless `ps -A -o command | grep "[m]ongo"`.include? node[:mongodb][:version]
-  # ensuring we have this directory
-  directory "/opt/src"
-
-  remote_file "/opt/src/mongodb-#{node[:mongodb][:version]}.tar.gz" do
-    source node[:mongodb][:source]
-    checksum node[:mongodb][platform][:checksum]
-    action :create_if_missing
-  end
-
-  bash "Setting up MongoDB #{node[:mongodb][:version]}" do
-    cwd "/opt/src"
-    code <<-EOH
-      tar -zxf mongodb-#{node[:mongodb][:version]}.tar.gz --strip-components=2 -C #{node[:mongodb][:dir]}/bin
-    EOH
-  end
+bash "Setting up MongoDB #{node[:mongodb][:version]}" do
+  cwd "/tmp"
+  code <<-EOH
+    tar -zxf mongodb-#{node[:mongodb][:version]}.tar.gz --strip-components=2 -C #{node[:mongodb][:dir]}/bin
+  EOH
+  not_if { `ps -A -o command | grep "[m]ongo"`.include? node[:mongodb][:version] }
 end
 
 environment = File.read('/etc/environment')
@@ -59,4 +58,31 @@ unless environment.include? node[:mongodb][:dir]
   File.open('/etc/environment', 'w') { |f| f.puts environment.gsub(/PATH="/, "PATH=\"#{node[:mongodb][:dir]}/bin:") }
 end
 
-node[:mongodb][:installed_from] = "src"
+file node[:mongodb][:logfile] do
+  owner "mongodb"
+  group "mongodb"
+  mode 0644
+  action :create_if_missing
+  backup false
+end
+
+template node[:mongodb][:config] do
+  source "mongodb.conf.erb"
+  owner "mongodb"
+  group "mongodb"
+  mode 0644
+  backup false
+end
+
+template "/etc/init.d/mongodb" do
+  source "mongodb.init.erb"
+  mode 0755
+  backup false
+end
+
+service "mongodb" do
+  supports :start => true, :stop => true, "force-stop" => true, :restart => true, "force-reload" => true, :status => true
+  action [:enable, :start]
+  subscribes :restart, resources(:template => node[:mongodb][:config])
+  subscribes :restart, resources(:template => "/etc/init.d/mongodb")
+end
