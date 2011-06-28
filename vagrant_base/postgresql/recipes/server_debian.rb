4a4dd10519850d934f47f0b3c52ed91b6cd8bc24
Michael S. Klishin <michael@novemberain.com>
2011-06-29 00:30:48 +0400

    A lame way to make PostgreSQL cookbook's debian server recipe idempotent

--- vagrant_base/postgresql/recipes/server_debian.rb
+++ vagrant_base/postgresql/recipes/server_debian.rb
@@ -67,4 +67,8 @@ end
 bash "create user" do
   user "postgres"
   code "createuser --no-password --no-superuser --createdb --no-createrole vagrant"
+
+  # a very lame way to make sure 2nd and other runs do not fail.
+  # the proper way is to simply use a .sql script to set up users. MK.
+  ignore_failure true
 end
