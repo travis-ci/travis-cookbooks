298a4df2c6b0b050dde3acab0c6fbc95f1c45a61
Michael S. Klishin <michael@novemberain.com>
2011-07-02 19:45:18 +0400

    Add postgresql::travis_server recipe, no need to pollute the Debian one with Travis-specific rules

--- vagrant_base/postgresql/recipes/server_debian.rb
+++ vagrant_base/postgresql/recipes/server_debian.rb
@@ -58,12 +58,3 @@ template "#{node[:postgresql][:dir]}/postgresql.conf" do
   mode 0600
   notifies :restart, resources(:service => "postgresql")
 end
-
-bash "create user" do
-  user "postgres"
-  code "createuser --no-password --no-superuser --createdb --no-createrole vagrant"
-
-  # a very lame way to make sure 2nd and other runs do not fail.
-  # the proper way is to simply use a .sql script to set up users. MK.
-  ignore_failure true
-end
