38b6cbc3218c7a6bde69fbb3d87bd87e2b3d918c
Sven Fuchs <svenfuchs@artweb-design.de>
2011-07-03 07:19:11 -0700

    grant HUNNY_BUNNY_BE_COOL_BADGE to antares!

--- vagrant_base/travis_build_environment/files/default/vagrant/travis_environment.sh
+++ vagrant_base/travis_build_environment/files/default/vagrant/travis_environment.sh
@@ -3,6 +3,8 @@ export TRAVIS=true
 
 # without this magic variable, nothing can possibly work. MK.
 export HAS_JOSH_K_SEAL_OF_APPROVAL=true
+# http://www.youtube.com/watch?v=JMV_r4LjQIo&NR=1
+export HAS_ANTARES_HUNNY_BUNNY_BE_COOL_BADGE=true
 
 export RAILS_ENV=test
 export MERB_ENV=test
