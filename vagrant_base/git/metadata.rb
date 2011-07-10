2fe330716c32e675ec830ba3d0aa01bc7d2911cb
Michael S. Klishin <michael@novemberain.com>
2011-07-10 12:49:02 +0400

    Remove git::server recipe that Travis doesn't need

--- vagrant_base/git/metadata.rb
+++ vagrant_base/git/metadata.rb
@@ -5,7 +5,6 @@ description       "Installs git and/or sets up a Git server daemon"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version           "0.9.0"
 recipe            "git", "Installs git"
-recipe            "git::server", "Sets up a runit_service for git daemon"
 
 %w{ ubuntu debian arch}.each do |os|
   supports os
--- vagrant_base/git/recipes/default.rb
+++ vagrant_base/git/recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 # Copyright 2008-2009, Opscode, Inc.
+# Copyright 2011, Travis Development Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
