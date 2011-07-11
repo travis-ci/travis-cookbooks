96ebfcb79503a8862364e7652e646f1463ac1a16
Michael S. Klishin <michael@novemberain.com>
2011-07-11 22:21:25 +0400

    Initial version of the Riak cookbook

---
+++ vagrant_base/riak/recipes/default.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: Riak
+# Recipe:: default
+# Copyright 2011, Travis CI development team
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require "tmpdir"
+
+tmp = Dir.tmpdir
+case node[:platform]
+when "debian", "ubuntu"
+  # this assumes 32-bit base Vagrant box.
+  %w(riak_0.14.2-1_i386.deb).each do |deb|
+    path = File.join(tmp, deb)
+
+    cookbook_file(path) do
+      owner "vagrant"
+      group "vagrant"
+    end
+
+    package(deb) do
+      action   :install
+      source   path
+      provider Chef::Provider::Package::Dpkg
+    end
+  end # each
+end # case
