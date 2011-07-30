e32f8e5b61f42be74fcb4789cf60348b69b983fd
Michael S. Klishin <michael@novemberain.com>
2011-07-30 23:18:18 +0400

    Re-re-re-resolve conflict

--- vagrant_base/travis_build_environment/recipes/vagrant.rb
+++ vagrant_base/travis_build_environment/recipes/vagrant.rb
@@ -55,11 +55,12 @@ directory "/home/vagrant/builds" do
   action :create
 end
 
-
-mount "/home/vagrant/builds" do
-  fstype   "tmpfs"
-  device   "/dev/null" # http://tickets.opscode.com/browse/CHEF-1657, doesn't seem to be included in 0.10.0
-  options  "defaults,size=#{node[:travis_build_environment][:builds_volume_size]},noatime"
-  action   [:mount, :enable]
+if node[:use_tmpfs]
+  mount "/home/vagrant/builds" do
+    fstype   "tmpfs"
+    device   "/dev/null" # http://tickets.opscode.com/browse/CHEF-1657, doesn't seem to be included in 0.10.0
+    options  "defaults,size=#{node[:travis_build_environment][:builds_volume_size]},noatime"
+    action   [:mount, :enable]
+  end
 end
 
