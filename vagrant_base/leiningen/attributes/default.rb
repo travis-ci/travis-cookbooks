98c9ee34f606b9ca5672f7d28c3f4273b6d2253d
Michael S. Klishin <michael@novemberain.com>
2011-07-31 15:23:06 +0400

    Add (standalone) Leiningen cookbook

---
+++ vagrant_base/leiningen/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:leiningen][:version] = "1.4.2"
+default[:leiningen][:install_script] = "https://github.com/technomancy/leiningen/raw/#{leiningen[:version]}/bin/lein"
+default[:leiningen][:jar_url] = "http://github.com/downloads/technomancy/leiningen/leiningen-#{leiningen[:version]}-standalone.jar"
+default[:leiningen][:jar_checksum] = "61b247aeb5"
+default[:leiningen][:jar_dir] = "/usr/share/java"
