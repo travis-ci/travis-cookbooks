d30252eeab3fb7623dd4e0b3618fe7f06c8b808f
Michael S. Klishin <michael@novemberain.com>
2011-07-31 15:48:01 +0400

    Upgrade to Leiningen 1.6.1

--- vagrant_base/leiningen/attributes/default.rb
+++ vagrant_base/leiningen/attributes/default.rb
@@ -1,5 +1,5 @@
-default[:leiningen][:version] = "1.4.2"
+default[:leiningen][:version] = "1.6.1"
 default[:leiningen][:install_script] = "https://github.com/technomancy/leiningen/raw/#{leiningen[:version]}/bin/lein"
 default[:leiningen][:jar_url] = "http://github.com/downloads/technomancy/leiningen/leiningen-#{leiningen[:version]}-standalone.jar"
-default[:leiningen][:jar_checksum] = "61b247aeb5"
+default[:leiningen][:jar_checksum] = "a55e14132cdbef24ac0c08ad340746ad"
 default[:leiningen][:jar_dir] = "/usr/share/java"
