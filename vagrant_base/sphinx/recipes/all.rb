e8c44bc3cb09e4272575120a602501ba4e72546c
Pat Allan <pat@freelancing-gods.com>
2011-08-04 17:50:16 +1000

    Installing all Sphinxes to their own directories, then symlinking the latest.

--- vagrant_base/sphinx/recipes/all.rb
+++ vagrant_base/sphinx/recipes/all.rb
@@ -1,7 +1,13 @@
 node.set[:sphinx][:versions] = {
-  '2.0.1-beta' => '/usr/local',
+  '2.0.1-beta' => '/usr/local/sphinx-2.0.1',
   '1.10-beta'  => '/usr/local/sphinx-1.10',
   '0.9.9'      => '/usr/local/sphinx-0.9.9'
 }
 
 include_recipe 'sphinx::install'
+
+%w( indexer indextool search searchd spelldump ).each do |binary|
+  link "/usr/local/bin/#{binary}" do
+    to "/usr/local/sphinx-2.0.1/bin/#{binary}"
+  end
+end
