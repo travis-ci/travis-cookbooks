7663852b0a2b0448ed51ddcb6fd357a403e67454
Ward Bekker <ward@tty.nl>
2011-08-20 08:55:27 +0200

    re-added releases

--- vagrant_base/kerl/attributes/default.rb
+++ vagrant_base/kerl/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:kerl][:releases] = %w(R14B01)
+default[:kerl][:releases] = %w(R14B01 R14B02 R14B03 R14B01 R14B R14A R13B04 R13B03)
 default[:kerl][:path]     = "/usr/local/bin/kerl"
 
 default[:kerl][:user]     = "vagrant"
