191a527c9decb639f7b9ae8128ebbf6731386b09
Michael S. Klishin <michael@novemberain.com>
2011-11-01 04:11:12 +0400

    Hardcode Ruby installation order into rvm::multi
    What we are doing here is not cool nor how Chef recipes are typically written but
    we have reasons to hardcode Rubies we install: we need them to be installed with various
    configuration/installation options and also IN A SPECIFIC ORDER.
    
    If we just use a hash in node attributes, we get the former but not the latter.
    This matters for Rubinius which currently needs 1.8.7 to be used when configuring/building it.
    
    A better solution would be to use array of pairs in node attributes. We will go back to
    doing this later. This is not as trivial as it sounds:
    
     * We need to pass custom configuration options
    
     * We need rubies to be installed in a specific order

--- vagrant_base/rvm/recipes/multi.rb
+++ vagrant_base/rvm/recipes/multi.rb
@@ -24,7 +24,6 @@
 include_recipe "rvm"
 
 gems    = node[:rvm].fetch(:gems, []) | ['bundler', 'rake']
-default = node[:rvm][:default] || node[:rvm][:rubies].first
 aliases = node[:rvm][:aliases] || []
 
 home = node[:rvm][:home]
@@ -37,31 +36,109 @@ setup = lambda do |bash|
   bash.environment env
 end
 
-node[:rvm][:rubies].each do  |name, ruby|
-  bash "installing #{name} (#{ruby})" do
-    setup.call(self)
-    code   "#{rvm} install #{ruby}"
-    not_if "ls #{home}/.rvm/rubies | grep #{name}"
-  end
 
+# PLEASE READ THIS FIRST!
+# PLEASE READ THIS FIRST!
+# PLEASE READ THIS FIRST!
+#
+# Here is what's going on here: we need to install Rubies in a pretty specific order
+# because Rubinius needs to be built with 1.8.7 (yes, it won't build itself. Why? I have no idea),
+# but if we pass Rubies hash via node attributes, WE CANNOT GUARANTEE THE ORDER OF INSTALLATION,
+# which means sometimes Rubinius will fail to install.
+# Also, if installed on Ruby 1.9, Rubinius will pick up 1.9 mode as the default which is not what people
+# typically want.
+#
+# So we HARDCODE INSTALLATION ORDER here: our RVM cookbook is not for general use and nobody installs a dozen
+# of various rubies on their servers anyway. This is the only way to make it work and for me to not lose sanity
+# over this Ruby versions zoo. MK.
+#
+#
+# First, install 1.8.7 and make it the default.
+#
+bash "installing 1.8.7" do
+  setup.call(self)
+  code   "#{rvm} install 1.8.7"
+  not_if "ls #{home}/.rvm/rubies | grep 1.8.7"
+end
+bash "make 1.8.7 the default ruby" do
+  setup.call(self)
+  code "#{rvm} --default 1.8.7"
+end
+bash "install chef for the default Ruby" do
+  setup.call(self)
+  code   "#{rvm} use 1.8.7 && gem install chef --no-ri --no-rdoc"
+  not_if "bash -c '#{rvm} use 1.8.7 && find $GEM_HOME/gems -name \"chef-[0-9]*.[0-9]*.[0-9]*\" | grep chef'", :environment => env
+end
+
+# then install JRuby in 1.8 mode
+bash "installing JRuby in 1.8 mode" do
+  setup.call(self)
+  code   "#{rvm} install jruby"
+  not_if "ls #{home}/.rvm/rubies | grep jruby"
+end
+
+# then install Rubinius in 1.8 mode using 2.0.testing
+# the Rubinius team keeps stable for Travis.
+bash "installing Rubinius in 1.8 mode (from 2.0.testing branch)" do
+  setup.call(self)
+  code   "#{rvm} install rbx-head --branch 2.0.testing"
+  not_if "ls #{home}/.rvm/rubies | grep rbx-head"
+end
+
+# then install REE
+bash "installing REE" do
+  setup.call(self)
+  code   "#{rvm} install ree"
+  not_if "ls #{home}/.rvm/rubies | grep ree"
+end
+
+# then install 1.9.2
+bash "installing 1.9.2" do
+  setup.call(self)
+  code   "#{rvm} install 1.9.2"
+  not_if "ls #{home}/.rvm/rubies | grep 1.9.2"
+end
+
+# then install 1.9.3
+bash "installing 1.9.3" do
+  setup.call(self)
+  code   "#{rvm} install 1.9.3"
+  not_if "ls #{home}/.rvm/rubies | grep 1.9.3"
+end
+
+# then install Rubinius with 1.9 mode by default using 1.9.3
+# bash "installing Rubinius in 1.9 mode by default using Ruby 1.9.3" do
+#   setup.call(self)
+#   code   "#{rvm} use 1.9.3 && #{rvm} install rbx-head -n d19 --branch 2.0.testing -- default-version=1.9"
+#   not_if "ls #{home}/.rvm/rubies | grep rbx-head-d19"
+# end
+
+
+# then install ruby-head
+bash "installing ruby-head" do
+  setup.call(self)
+  code   "#{rvm} install ruby-head"
+  not_if "ls #{home}/.rvm/rubies | grep ruby-head"
+end
+
+# then install 1.8.6 (just for Sinatra, DO NOT ADD IT TO THE DOCUMENTATION or mention it anywhere. It is a pain to
+# maintain and must go away). MK.
+bash "installing 1.8.6" do
+  setup.call(self)
+  code   "#{rvm} install 1.8.6"
+  not_if "ls #{home}/.rvm/rubies | grep 1.8.6"
+end
+
+
+
+# now we can install gems by iterating over rubies in any order. MK.
+%w(1.8.7 jruby rbx ree 1.9.2 1.9.3 ruby-head).each do |name|
   gems.each do |gem|
     bash "installing gem #{gem} for #{name}" do
       setup.call(self)
       code   "#{rvm} use #{name}; gem install #{gem} --no-ri --no-rdoc"
-      not_if "bash -c '#{rvm} use #{name}; find $GEM_HOME/gems -name \"#{gem}-[0-9]*.[0-9]*.[0-9]*\" | grep #{gem}'", :environment => env
     end
   end
-end
-
-bash "make #{node[:rvm][:default]} the default ruby" do
-  setup.call(self)
-  code "#{rvm} --default #{node[:rvm][:default]}"
-end
-
-bash "install chef for the default Ruby" do
-  setup.call(self)
-  code   "#{rvm} use #{default} && gem install chef --no-ri --no-rdoc"
-  not_if "bash -c '#{rvm} use #{default} && find $GEM_HOME/gems -name \"chef-[0-9]*.[0-9]*.[0-9]*\" | grep chef'", :environment => env
 end
 
 aliases.each do |new_name, existing_name|
