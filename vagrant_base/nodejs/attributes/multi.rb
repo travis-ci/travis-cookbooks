f897b0c298da3c1a5d6f50ce9f35b32a066d77b3
Harry Brundage <harry.brundage@gmail.com>
2011-11-08 13:25:25 -0500

    Update nodes::multi for new versions of node (including v0.6.0)
     - Install node v0.6.0, v0.5.10, v0.4.12
     - Alias the above versions as 0.6, 0.5, 0.4 respectively.

--- vagrant_base/nodejs/attributes/multi.rb
+++ vagrant_base/nodejs/attributes/multi.rb
@@ -1,3 +1,3 @@
-default[:nodejs][:versions]   = ["0.4.12", "0.5.8"]
-default[:nodejs][:aliases]    = {"0.5.8".to_sym => "latest", "0.4.12".to_sym => "stable"}
+default[:nodejs][:versions]   = ["0.4.12", "0.5.10", "0.6.0"]
+default[:nodejs][:aliases]    = {"0.5.10".to_sym => "0.5", "0.4.12".to_sym => "0.4", "0.6.0".to_sym => "0.6"}
 default[:nodejs][:default]    = "0.4.12"
