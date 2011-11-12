fcf3f95309148fc2f4fe63a6266056596cf4328e
Michael S. Klishin <michael@novemberain.com>
2011-11-12 19:50:23 +0400

    Remove default value on phpfarm_compile resource
    Looks like in production we are hitting a Chef bug that makes it
    ignore named attribute and always use default value instead. This magically
    makes the issue go away. Per discussion with Loïc.

--- vagrant_base/phpfarm/resources/compile.rb
+++ vagrant_base/phpfarm/resources/compile.rb
@@ -1,5 +1,5 @@
 actions :create, :delete
 
-attribute :version, :default => "5.3.8", :name_attribute => true
+attribute :version, :name_attribute => true
 attribute :owner, :regex => Chef::Config[:user_valid_regex]
 attribute :group, :regex => Chef::Config[:group_valid_regex]
