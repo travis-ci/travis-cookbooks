07f53abd86f7cba216bb756cbbf96452b80687fd
Loïc Frering <loic.frering@gmail.com>
2011-11-18 17:27:11 +0100

    Composer cookbook.

---
+++ vagrant_base/composer/recipes/default.rb
@@ -0,0 +1,22 @@
+include_recipe "php::multiphpbuild"
+
+phpenv_path = "#{node.phpenv.home}/.phpenv"
+
+node[:php][:multi][:versions].each do |php_version|
+
+  bin_path = "#{phpenv_path}/versions/#{php_version}/bin"
+  remote_file "#{bin_path}/composer.phar" do
+    source "http://getcomposer.org/composer.phar"
+    mode "0644"
+  end
+
+  template "#{bin_path}/composer" do
+    owner node.phpfarm.user
+    group node.phpfarm.group
+    mode "0755"
+    source "composer.erb"
+    variables(
+      :phpbin_path => bin_path
+    )
+  end
+end
