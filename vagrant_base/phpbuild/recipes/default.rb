e9fe3b9e624a374992560d41fb6c2ba45057f702
Loïc Frering <loic.frering@gmail.com>
2011-11-26 14:38:28 +0100

    Manage PHP 5.2.17 and update 5.4 to 5.4.0rc2.

--- vagrant_base/phpbuild/recipes/default.rb
+++ vagrant_base/phpbuild/recipes/default.rb
@@ -4,8 +4,12 @@ include_recipe "postgresql::client"
 
 case node[:platform]
   when "ubuntu", "debian"
-    %w{libbz2-dev libc-client2007e-dev libcurl4-gnutls-dev libfreetype6-dev libgmp3-dev libjpeg62-dev libkrb5-dev libmcrypt-dev libpng12-dev libssl-dev libt1-dev libmhash-dev libexpat1-dev libxml2-dev libicu-dev libtidy-dev libxslt-dev language-pack-fr}.each do |pkg|
+    %w{libbz2-dev libc-client2007e-dev libcurl4-gnutls-dev libfreetype6-dev libgmp3-dev libjpeg8-dev libkrb5-dev libmcrypt-dev libpng12-dev libssl-dev libt1-dev libmhash-dev libexpat1-dev libxml2-dev libicu-dev libtidy-dev libxslt-dev language-pack-fr}.each do |pkg|
       package(pkg) { action :install }
+    end
+
+    link "/usr/lib/libpng.so" do
+      to "/usr/lib/i386-linux-gnu/libpng.so"
     end
 end
 
@@ -15,7 +19,7 @@ git phpbuild_path do
   user       node[:phpbuild][:user]
   group      node[:phpbuild][:group]
   repository "git://github.com/CHH/php-build.git"
-  revision   "693715de0b59ea0642e3e94c3a72d8cd76b1be49"
+  revision   "559214ce65284936597b2208e3cb602205578a41"
   action     :checkout
 end
 
@@ -23,7 +27,7 @@ git "/tmp/php-build-plugin-phpunit" do
   user       node[:phpbuild][:user]
   group      node[:phpbuild][:group]
   repository "git://github.com/CHH/php-build-plugin-phpunit.git"
-  revision   "0030458a985557671752afaeb7a28f8077b59be9"
+  revision   "a6a5ce4a5126b90a02dd473b63f660515de7d183"
   action     :checkout
 end
 
