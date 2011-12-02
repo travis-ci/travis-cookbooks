62fcc9cb552be138bf342485348cf88037b33a37
Michael S. Klishin <michael@novemberain.com>
2011-12-02 17:23:53 +0400

    If MOTD crap is not available or is already wiped out, just ignore it

--- vagrant_base/travis_build_environment/recipes/default.rb
+++ vagrant_base/travis_build_environment/recipes/default.rb
@@ -59,4 +59,8 @@ when ["ubuntu", "11.04"] then
   end
 end
 
-execute "rm /etc/update-motd.d/*"
+# improve sshd startup stability. See https://github.com/jedi4ever/veewee/issues/159 for rationale
+# and some stats about boot failures. MK.
+execute "rm /etc/update-motd.d/*" do
+  ignore_failure true
+end
