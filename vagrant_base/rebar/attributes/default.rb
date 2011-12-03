64614dad55fb792cce2b987266257e6f7bb0d606
Michael S. Klishin <michael@novemberain.com>
2011-12-03 18:32:46 +0400

    Use Basho's official rebar download. Make sure rebar is executable.

--- vagrant_base/rebar/attributes/default.rb
+++ vagrant_base/rebar/attributes/default.rb
@@ -1,5 +1,4 @@
 default[:rebar][:path]     = "/usr/local/bin/rebar"
 default[:rebar][:user]     = "vagrant"
 default[:rebar][:group]    = "vagrant"
-default[:rebar][:release]  = "https://nodeload.github.com/wardbekker/rebar/tarball/RELEASE-2" 
-default[:rebar][:release_dir] = "wardbekker-rebar-adaa8ad"
+default[:rebar][:release]  = "https://github.com/downloads/basho/rebar/rebar"
