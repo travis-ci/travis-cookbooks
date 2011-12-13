06dd72de08fb64db65e6a6048f0d230002572e5e
Loïc Frering <loic.frering@gmail.com>
2011-12-13 22:18:31 +0100

    Update php-build's revision.

--- vagrant_base/phpbuild/attributes/default.rb
+++ vagrant_base/phpbuild/attributes/default.rb
@@ -3,7 +3,7 @@ default[:phpbuild][:group] = "vagrant"
 default[:phpbuild][:home]  = "/home/vagrant"
 
 default[:phpbuild][:git][:repository]                  = "git://github.com/loicfrering/php-build.git"
-default[:phpbuild][:git][:revision]                    = "789f7489d821ac79ac8c5e03d26de630b1258e40"
+default[:phpbuild][:git][:revision]                    = "c041adcd8292ec6a66120b823042f98afaf074c8"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
