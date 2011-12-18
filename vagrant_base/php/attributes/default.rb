98ef4639508ba3d1eadfb311a566b69f3d6cae9e
Loïc Frering <loic.frering@gmail.com>
2011-12-18 23:03:42 +0100

    Update PHP 5.4 to 5.4.0rc3.

--- vagrant_base/php/attributes/default.rb
+++ vagrant_base/php/attributes/default.rb
@@ -1,2 +1,2 @@
-default[:php][:multi][:versions] = ["5.2.17", "5.3.2", "5.3.8", "5.4.0rc2"]
-default[:php][:multi][:aliases]  = {"5.2" => "5.2.17", "5.3" => "5.3.8", "5.4" => "5.4.0rc2"}
+default[:php][:multi][:versions] = ["5.2.17", "5.3.2", "5.3.8", "5.4.0rc3"]
+default[:php][:multi][:aliases]  = {"5.2" => "5.2.17", "5.3" => "5.3.8", "5.4" => "5.4.0rc3"}
