a744ed0d7e3dbf297014b3f5ecc5b36b6e78271c
Michael S. Klishin <michael@novemberain.com>
2011-12-19 15:59:52 +0400

    Correct php-build revision
    The previous one, c041adcd8292ec6a66120b823042f98afaf074c8, failed to check out
    for me (both using Sous Chef and after cloning loicfrering/php-build and
    CHH/php-build).
    
    Maybe it was garbage collected (github runs git gc on repositories every
    so often). I am not the right person to decide whether it is a good idea to
    use php-build fork and what commit we should pin to but this way
    provisioning works again.

--- vagrant_base/phpbuild/attributes/default.rb
+++ vagrant_base/phpbuild/attributes/default.rb
@@ -3,7 +3,7 @@ default[:phpbuild][:group] = "vagrant"
 default[:phpbuild][:home]  = "/home/vagrant"
 
 default[:phpbuild][:git][:repository]                  = "git://github.com/loicfrering/php-build.git"
-default[:phpbuild][:git][:revision]                    = "c041adcd8292ec6a66120b823042f98afaf074c8"
+default[:phpbuild][:git][:revision]                    = "18333218cb4e3c8ef1220b8c49913fc0f748c3f3"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
