bda8631733cf9d5345ec374e92a707b040d94f3a
Loïc Frering <loic.frering@gmail.com>
2011-12-19 14:51:16 +0100

    Move back to php-build blessed repository.

--- vagrant_base/phpbuild/attributes/default.rb
+++ vagrant_base/phpbuild/attributes/default.rb
@@ -2,8 +2,8 @@ default[:phpbuild][:user]  = "vagrant"
 default[:phpbuild][:group] = "vagrant"
 default[:phpbuild][:home]  = "/home/vagrant"
 
-default[:phpbuild][:git][:repository]                  = "git://github.com/loicfrering/php-build.git"
-default[:phpbuild][:git][:revision]                    = "18333218cb4e3c8ef1220b8c49913fc0f748c3f3"
+default[:phpbuild][:git][:repository]                  = "git://github.com/CHH/php-build.git"
+default[:phpbuild][:git][:revision]                    = "3b5928151fd807a214bfbc250ffab163e890016d"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
