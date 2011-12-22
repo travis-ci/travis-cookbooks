f56f3b97ff110860a0c1ba8dd4423cd799f537ea
Loïc Frering <loic.frering@gmail.com>
2011-12-22 21:04:15 +0100

    Add support to PHP 5.3.9rc3.

--- vagrant_base/php/attributes/default.rb
+++ vagrant_base/php/attributes/default.rb
@@ -1,2 +1,2 @@
-default[:php][:multi][:versions] = ["5.2.17", "5.3.2", "5.3.8", "5.4.0rc3"]
+default[:php][:multi][:versions] = ["5.2.17", "5.3.2", "5.3.8", "5.3.9rc3", "5.4.0rc3"]
 default[:php][:multi][:aliases]  = {"5.2" => "5.2.17", "5.3" => "5.3.8", "5.4" => "5.4.0rc3"}
