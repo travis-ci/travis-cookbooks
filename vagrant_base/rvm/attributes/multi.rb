3db81bc7776bf9206528304a4157ce06bd85899e
Michael S. Klishin <michael@novemberain.com>
2011-12-23 22:35:59 +0400

    No need to install Chef: it is ran using a separate Ruby 1.9.2 installation under /opt, per Veewee definition

--- vagrant_base/rvm/attributes/multi.rb
+++ vagrant_base/rvm/attributes/multi.rb
@@ -1,5 +1,5 @@
 default[:rvm][:rubies]       = [{ :name => "1.8.7" },
                                 { :name => "1.9.3" }]
 default[:rvm][:default]      = "1.8.7"
-default[:rvm][:default_gems] = %w(bundler rake chef)
+default[:rvm][:default_gems] = %w(bundler rake)
 default[:rvm][:aliases]      = Hash.new
