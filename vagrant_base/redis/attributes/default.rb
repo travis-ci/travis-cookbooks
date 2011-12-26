5e2a438066aed5b82f87f493e962522a609ade3b
Michael S. Klishin <michael@novemberain.com>
2011-12-26 20:20:10 +0400

    dotdeb does not keep older package versions around, fetch Redis .deb from files.travis-ci.org

--- vagrant_base/redis/attributes/default.rb
+++ vagrant_base/redis/attributes/default.rb
@@ -1,3 +1,3 @@
-default[:redis][:package][:url]  = "http://packages.dotdeb.org/dists/squeeze/redis/binary-i386/redis-server_2.4.4-1~dotdeb.1_i386.deb"
+default[:redis][:package][:url]   = "http://files.travis-ci.org/packages/deb/redis/redis-server_2.4.5-1~dotdeb.1_i386.deb"
 default[:redis][:package][:user]  = "vagrant"
 default[:redis][:package][:group] = "vagrant"
