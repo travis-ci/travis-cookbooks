a472d4a8061c1383cdeb92ae98be08bf79f82598
Loïc Frering <loic.frering@gmail.com>
2012-01-11 11:34:06 +0100

    Add PHP 5.3.9 final release.
    5.3 alias now points to 5.3.9.

--- vagrant_base/phpbuild/attributes/default.rb
+++ vagrant_base/phpbuild/attributes/default.rb
@@ -3,7 +3,7 @@ default[:phpbuild][:group] = "vagrant"
 default[:phpbuild][:home]  = "/home/vagrant"
 
 default[:phpbuild][:git][:repository]                  = "git://github.com/CHH/php-build.git"
-default[:phpbuild][:git][:revision]                    = "30e339334b638e6ab97d764aa954f27269bd61f4"
+default[:phpbuild][:git][:revision]                    = "92857d6dcd6e87262e3f8df78f18e5b2a7b3263f"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
