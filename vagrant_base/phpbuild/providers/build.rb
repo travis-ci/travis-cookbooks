059ca242ac670ddbe7eb329aa25fc95279e3f28e
Michael S. Klishin <michael@novemberain.com>
2012-01-14 16:31:53 +0400

    Correct phpbuild provider to use node.travis_build_environment.home

--- vagrant_base/phpbuild/providers/build.rb
+++ vagrant_base/phpbuild/providers/build.rb
@@ -2,14 +2,14 @@ action :create do
   unless exists?
     Chef::Log.info("Installing PHP #{@new_resource.version} with php-build")
     new_resource  = @new_resource
-    phpbuild_path = "#{node[:phpbuild][:home]}/.php-build"
+    phpbuild_path = "#{node.travis_build_environment.home}/.php-build"
     version       = new_resource.version
     target_path   = "#{new_resource.path}/#{version}"
 
     bash "install PHP #{version} with php-build" do
       user        new_resource.owner
       group       new_resource.group
-      environment Hash["HOME" => node[:phpbuild][:home]]
+      environment Hash["HOME" => node.travis_build_environment.home]
       cwd         "#{phpbuild_path}/bin"
       code <<-EOF
       ./php-build -i development #{version} #{target_path}
@@ -34,7 +34,7 @@ end
 action :delete do
   if exists?
     Chef::Log.info("Uninstalling PHP #{@new_resource.version} from php-build")
-    phpbuild_path = "#{node[:phpbuild][:home]}/.php-build"
+    phpbuild_path = "#{node.travis_build_environment.home}/.php-build"
     target_path   = "#{@new_resource.path}/#{@new_resource.version}"
 
     FileUtils.rm_rf(target_path)
@@ -44,7 +44,7 @@ end
 
 private
 def exists?
-  phpbuild_path = "#{node[:phpbuild][:home]}/.php-build"
+  phpbuild_path = "#{node.travis_build_environment.home}/.php-build"
   target_path   = "#{@new_resource.path}/#{@new_resource.version}"
 
   ::File.exist?(target_path) && ::File.directory?(target_path) \
