9a69b198a9305d2adbf8b96515e8f3c9e6e211e0
Michael S. Klishin <michael@novemberain.com>
2012-01-21 14:40:29 +0400

    Initial bits of the Pythonbrew cookbook

---
+++ vagrant_base/pythonbrew/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:pythonbrew] = {
+  :installer_url => "https://raw.github.com/utahta/pythonbrew/72c5a0ccaa02e2918401cd9e0b996bdae47cead6/pythonbrew-install"
+}
---
+++ vagrant_base/pythonbrew/files/default/pythonbrew.sh
@@ -0,0 +1,1 @@
+[[ -s $HOME/.pythonbrew/etc/bashrc ]] && source $HOME/.pythonbrew/etc/bashrc
