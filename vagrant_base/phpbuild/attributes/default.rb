1e86db34dc0e0798adaf7c9d74ed8957e8eeb8da
Michael S. Klishin <michael@novemberain.com>
2012-01-22 16:19:57 +0400

    Bump phpbuild revision to the one that has 5.4.0RC6 definition

--- vagrant_base/phpbuild/attributes/default.rb
+++ vagrant_base/phpbuild/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:phpbuild][:git][:repository]                  = "git://github.com/CHH/php-build.git"
-default[:phpbuild][:git][:revision]                    = "92857d6dcd6e87262e3f8df78f18e5b2a7b3263f"
+default[:phpbuild][:git][:revision]                    = "74855fd4852d9f24690f5d37c736260e0c35dff4"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
