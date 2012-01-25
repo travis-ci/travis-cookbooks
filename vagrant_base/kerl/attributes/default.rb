dd98147b600847b99114606b9d3574292a04247e
Michael S. Klishin <michael@novemberain.com>
2012-01-25 16:48:34 +0400

    Only offer OTP releases R14B02 and later
    Various earlier versions fail to compile on Ubuntu 11.04 on 11.10
    and developers of several very popular Erlang projects agree that
    going R14B02+ is a good idea.

--- vagrant_base/kerl/attributes/default.rb
+++ vagrant_base/kerl/attributes/default.rb
@@ -1,2 +1,2 @@
-default[:kerl][:releases] = %w(R13B03 R13B04 R14A R14B R14B02 R14B03 R14B04 R15B)
+default[:kerl][:releases] = %w(R14B02 R14B03 R14B04 R15B)
 default[:kerl][:path]     = "/usr/local/bin/kerl"
