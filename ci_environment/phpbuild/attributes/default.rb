61ece8daaa5602c134221258c9e509f6cb01dc47
Michael S. Klishin <michael@novemberain.com>
2012-02-04 00:47:12 +0400

    Update phpbuild to a revision that provides 5.3.10 and 5.4.RC7 definitions
    cc @loicfrering

--- ci_environment/phpbuild/attributes/default.rb
+++ ci_environment/phpbuild/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:phpbuild][:git][:repository]                  = "git://github.com/CHH/php-build.git"
-default[:phpbuild][:git][:revision]                    = "74855fd4852d9f24690f5d37c736260e0c35dff4"
+default[:phpbuild][:git][:revision]                    = "d0c9e9552c442c2974e974decffcfc6748900218"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
