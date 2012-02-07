ed8b9256f7c6123e67e0821e2f130da133f05e0e
Michael S. Klishin <michael@novemberain.com>
2012-02-07 11:26:37 +0400

    Add two more Perls to the default perlbrew cookbook attribute list
    Just like php::multi, this recipe won't be used outside of PHP[/Perl/Python] VM images,
    so we can just keep all 3 versions we care about in the attributes as opposed to passing
    them in in travis-boxes.

--- ci_environment/perlbrew/attributes/multi.rb
+++ ci_environment/perlbrew/attributes/multi.rb
@@ -1,4 +1,6 @@
 default[:perlbrew][:perls] = [
-  { :name => "5.14", :version => "perl-5.14.2" }
-]
+                              { :name => "5.14", :version => "perl-5.14.2" },
+                              { :name => "5.12", :version => "perl-5.12.4" },
+                              { :name => "5.10", :version => "perl-5.10.1" }
+                             ]
 
