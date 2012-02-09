23a28ba341f56a66b69e82a24f13184128f1217e
Loïc Frering <loic.frering@gmail.com>
2012-02-09 22:23:10 +0100

    Add mbstring extension to PHP 5.2.17.

--- ci_environment/phpbuild/attributes/default.rb
+++ ci_environment/phpbuild/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:phpbuild][:git][:repository]                  = "git://github.com/CHH/php-build.git"
-default[:phpbuild][:git][:revision]                    = "d0c9e9552c442c2974e974decffcfc6748900218"
+default[:phpbuild][:git][:revision]                    = "387e802443af2254576ef386ea16fe874c628b5b"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
