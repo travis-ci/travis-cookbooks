575b8f659124415d098e5e53e555e8e5b0352a5a
Loïc Frering <loic.frering@gmail.com>
2012-02-10 21:11:59 +0100

    Update php-build to fix permissions issue.

--- ci_environment/phpbuild/attributes/default.rb
+++ ci_environment/phpbuild/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:phpbuild][:git][:repository]                  = "git://github.com/CHH/php-build.git"
-default[:phpbuild][:git][:revision]                    = "387e802443af2254576ef386ea16fe874c628b5b"
+default[:phpbuild][:git][:revision]                    = "598990a18687be7d2a465429de784d581afe8a18"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
