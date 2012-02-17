503ce4f24687a471bf7fecf242d5e5e6272845c6
Loïc Frering <loic.frering@gmail.com>
2012-02-17 22:45:48 +0100

    Support PHP 5.4.0RC8.

--- ci_environment/phpbuild/attributes/default.rb
+++ ci_environment/phpbuild/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:phpbuild][:git][:repository]                  = "git://github.com/CHH/php-build.git"
-default[:phpbuild][:git][:revision]                    = "598990a18687be7d2a465429de784d581afe8a18"
+default[:phpbuild][:git][:revision]                    = "830a588428e6bb3173ed119f88586f29fbd9977a"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
