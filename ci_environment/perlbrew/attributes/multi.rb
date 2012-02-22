36dbaffd3a0e9d378b7a43f84b4c07f9bea333dd
Michael S. Klishin <michael@novemberain.com>
2012-02-22 17:22:18 +0400

    Cosmetics

--- ci_environment/perlbrew/attributes/multi.rb
+++ ci_environment/perlbrew/attributes/multi.rb
@@ -3,4 +3,3 @@ default[:perlbrew][:perls] = [
                               { :name => "5.12", :version => "perl-5.12.4" },
                               { :name => "5.10", :version => "perl-5.10.1" }
                              ]
-
