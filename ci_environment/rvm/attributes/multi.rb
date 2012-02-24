3ed9b49293408cf3e019ab911f980673cc5a4d95
Michael S. Klishin <michael@novemberain.com>
2012-02-24 23:55:29 +0400

    Pin ruby-head down to a revision we will update
    Trunk segfauls during bootstrap again, which in turn fails our VM image provisioning.
    We filed the issue but need some sort of protection (a la what we are doing with Rubinius)
    against unexpected issues like this. Moving ruby-head commit manually is a pretty good
    solution.

--- ci_environment/rvm/attributes/multi.rb
+++ ci_environment/rvm/attributes/multi.rb
@@ -3,3 +3,6 @@ default[:rvm][:rubies]       = [{ :name => "1.8.7" },
 default[:rvm][:default]      = "1.8.7"
 default[:rvm][:default_gems] = %w(bundler rake)
 default[:rvm][:aliases]      = Hash.new
+
+# ruby-head revision that is known to build fine. MK.
+default[:rvm][:rvm_ruby_sha] = "7d742d47cc2f0023c4aeffdec6a6296f6c69a10f"
