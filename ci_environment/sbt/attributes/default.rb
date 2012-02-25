9978fd1d4acc445504f44c32f1cea93a713d52a0
Michael S. Klishin <michael@novemberain.com>
2012-02-25 05:29:01 +0400

    Add two more Scala versions we pre-download
    People seem to be testing against them

--- ci_environment/sbt/attributes/default.rb
+++ ci_environment/sbt/attributes/default.rb
@@ -1,4 +1,4 @@
 # 5 minutes to install sbt's own dependencies under ~/.sbt/boot. MK.
 default[:sbt][:boot][:timeout]   = 300
 
-default[:sbt][:scala][:versions] = ["2.9.1", "2.8.2"]
+default[:sbt][:scala][:versions] = ["2.9.1", "2.9.0-1", "2.8.2", "2.8.1"]
