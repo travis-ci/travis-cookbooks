025fb578793f0d91a44886b1e747dd6242ee218e
Mathias Meyer <meyer@paperplanes.de>
2012-03-20 11:41:13 +0100

    Switch users to Chef server search.

--- worker_host/users/recipes/default.rb
+++ worker_host/users/recipes/default.rb
@@ -1,19 +1,21 @@
-node[:users].each do |user_name, data|
-  user user_name do
-    shell data[:shell]
-    home data[:home]
+users = search(:users)
+
+users.each do |user|
+  user user[:id] do
+    shell user[:shell]
+    home user[:home]
   end
 
-  if data[:ssh_keys]
-    directory "#{data[:home]}/.ssh" do
+  if user[:ssh_key]
+    directory "#{user[:home]}/.ssh" do
       mode "0700"
-      owner user_name
+      owner user[:id]
     end
 
-    template "#{data[:home]}/.ssh/authorized_keys" do
+    template "#{user[:home]}/.ssh/authorized_keys" do
       mode "0600"
       source "authorized_keys.pub"
-      variables :ssh_keys => data[:ssh_keys]
+      variables :ssh_key => user[:ssh_key]
     end
   end
 end
