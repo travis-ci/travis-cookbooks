ea4c4093684acfb921d8c5ae4c14fb3e95a78a46
Mathias Meyer <meyer@paperplanes.de>
2012-03-22 15:11:52 +0100

    Mount points are simple hashes.

--- worker_host/disks/recipes/default.rb
+++ worker_host/disks/recipes/default.rb
@@ -1,7 +1,7 @@
-node[:mounts].each do |device, (filesystem, mount_point)|
-  mount mount_point do
-    fstype filesystem
-    device device
+node[:mounts].each do |mount|
+  mount mount[:mount_point] do
+    fstype mount[:filesystem]
+    device mount[:device]
     action [:mount, :enable]
   end
 end
