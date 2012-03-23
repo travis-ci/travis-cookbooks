db0aa3b3eb57ff6032d3d1dab555416820477c14
Mathias Meyer <meyer@paperplanes.de>
2012-03-23 11:54:20 +0100

    Add support for groups.

--- worker_host/users/recipes/default.rb
+++ worker_host/users/recipes/default.rb
@@ -1,15 +1,43 @@
 users = if Chef::Config[:solo]
-  node[:users]
-else
-  search(:users)
+          node[:users]
+        else
+          search(:users)
+        end
+
+groups = if Chef::Config[:solo]
+           node[:groups]
+         else
+           search(:groups)
+         end
+
+groups.each do |group|
+  group group[:id] do
+    gid group[:gid]
+    action [:create, :manage, :modify]
+  end
 end
 
 users.each do |user|
+  primary_group = user[:groups].nil? ? nil : user[:groups].first
+
   user user[:id] do
-    shell user[:shell]
+    shell user[:shell] || "/bin/bash"
     home user[:home]
+    gid primary_group
+    uid user[:uid]
     supports :manage_home => true
-    action :create
+    action [:create, :manage]
+  end
+
+  (user[:groups] || []).each do |group_name|
+    group_id = groups.find {|group| group[:id] == group_name}[:gid]
+
+    group group_name do
+      members [user[:id]]
+      gid group_id
+      append true
+      action [:create, :manage, :modify]
+    end
   end
 
   if user[:ssh_key]
