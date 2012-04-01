c4e45518ec8937421e7b6dfeca4a634d18d96221
Michael S. Klishin <michael@novemberain.com>
2012-04-01 20:28:29 +0400

    Leiningen 1.7.1

--- ci_environment/leiningen/attributes/default.rb
+++ ci_environment/leiningen/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:leiningen][:lein1][:version]        = "1.7.0"
+default[:leiningen][:lein1][:version]        = "1.7.1"
 default[:leiningen][:lein1][:install_script] = "https://github.com/technomancy/leiningen/raw/#{leiningen[:lein1][:version]}/bin/lein"
 
 default[:leiningen][:lein2][:version]        = "2.0.0-preview2"
