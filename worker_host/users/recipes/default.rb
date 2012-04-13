2d1b8124de9e70d698e8982c0e39764a20b31a7d
Mathias Meyer <meyer@paperplanes.de>
2012-04-13 11:51:34 +0200

    Move travis-worker to runit supervision.

--- worker_host/users/recipes/default.rb
+++ worker_host/users/recipes/default.rb
@@ -10,6 +10,9 @@ groups = if Chef::Config[:solo]
            search(:groups)
          end
 
+node[:users] = users
+node[:groups] = groups
+
 ohai "reload_passwd" do
   action :nothing
   plugin "passwd"
