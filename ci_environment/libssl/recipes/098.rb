e46990dc68f4f40ec9bd4547e4f3a235166db14b
Michael S. Klishin <michael@novemberain.com>
2012-04-19 09:37:30 +0400

    Correct recipe header

--- ci_environment/libssl/recipes/098.rb
+++ ci_environment/libssl/recipes/098.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: libssl
-# Recipe:: default
+# Recipe:: 098
 # Copyright 2012, Travis CI development team
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
