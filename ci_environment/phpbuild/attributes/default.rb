82e848bf0de63ad99a31704ef5be45d4bf1771cd
Loïc Frering <loic.frering@gmail.com>
2012-04-28 10:21:42 +0200

    PHP 5.3.11 and 5.4.1 have been released!

--- ci_environment/phpbuild/attributes/default.rb
+++ ci_environment/phpbuild/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:phpbuild][:git][:repository]                  = "git://github.com/CHH/php-build.git"
-default[:phpbuild][:git][:revision]                    = "38f9c3132d360d84db5e89628588db36aad687ce"
+default[:phpbuild][:git][:revision]                    = "626576b7d9f702ce088de5f122cc3bca60266843"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
