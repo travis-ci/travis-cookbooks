a65154fa570259318ae8a819ae427bad5bbd4ace
Loïc Frering <loic.frering@gmail.com>
2012-05-22 11:06:00 +0200

    Update PHP versions to 5.2.17, 5.3.3, 5.3.13, 5.4.3.
    Fix #516.

--- ci_environment/phpbuild/attributes/default.rb
+++ ci_environment/phpbuild/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:phpbuild][:git][:repository]                  = "git://github.com/CHH/php-build.git"
-default[:phpbuild][:git][:revision]                    = "626576b7d9f702ce088de5f122cc3bca60266843"
+default[:phpbuild][:git][:revision]                    = "e870d676bcdf2fcb9d9ae83f061c43611478c567"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
