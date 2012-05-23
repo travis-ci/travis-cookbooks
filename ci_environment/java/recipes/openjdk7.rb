d41b2bccb3e357ce4bbbed8dca4a9f50e90e8d92
Loïc Frering <loic.frering@gmail.com>
2012-05-24 00:27:51 +0200

    Add basic recipes for openjdk6 and openjdk7.

---
+++ ci_environment/java/recipes/openjdk7.rb
@@ -0,0 +1,17 @@
+version = 7
+
+pkgs = value_for_platform(
+  ["centos","redhat","fedora","scientific","amazon"] => {
+    "default" => ["java-1.#{version}.0-openjdk","java-1.#{version}.0-openjdk-devel"]
+  },
+  ["arch","freebsd"] => {
+    "default" => ["openjdk#{version}"]
+  },
+  "default" => ["openjdk-#{version}-jdk"]
+)
+
+pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
