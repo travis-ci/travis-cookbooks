bbc6797b61d0ff0273add617a66d9e1784cc10f6
Michael S. Klishin <michael@novemberain.com>
2012-05-27 04:33:25 +0400

    Don't double-evaluate the conditional here

--- ci_environment/java/files/ubuntu/export_java_home.sh
+++ ci_environment/java/files/ubuntu/export_java_home.sh
@@ -2,7 +2,7 @@
 
 # if jdk_switcher is provisioned (typically part of java::multi recipe),
 # export JAVA_HOME to use the default JDK
-if test -f $($HOME/.jdk_switcher_rc); then
+if test -f $HOME/.jdk_switcher_rc; then
     . $HOME/.jdk_switcher_rc
     jdk_switcher use default
 fi
