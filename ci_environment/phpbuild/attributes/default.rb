38752e99453408cbba71b9349a7f5382477a347f
Maks <github.maks3w@virtualplanets.net>
2012-06-03 16:46:23 +0300

    Update php-build to get Xdebug 2.2.0 definition.

--- ci_environment/phpbuild/attributes/default.rb
+++ ci_environment/phpbuild/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:phpbuild][:git][:repository]                  = "git://github.com/CHH/php-build.git"
-default[:phpbuild][:git][:revision]                    = "16f6212df2603dd85c7952bd29d986f720cf5782"
+default[:phpbuild][:git][:revision]                    = "1e9df7cd90a8e8443eabd0d63daa571c873b09ce"
 default[:phpbuild][:phpunit_plugin][:git][:repository] = "git://github.com/CHH/php-build-plugin-phpunit.git"
 default[:phpbuild][:phpunit_plugin][:git][:revision]   = "a6a5ce4a5126b90a02dd473b63f660515de7d183"
 
