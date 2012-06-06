01ed66aa013277b68ac5e850a799428c8867a6f5
Michael S. Klishin <michael@novemberain.com>
2012-06-06 22:20:14 +0400

    nodejs 0.6.19

--- ci_environment/nodejs/attributes/multi.rb
+++ ci_environment/nodejs/attributes/multi.rb
@@ -1,3 +1,3 @@
-default[:nodejs][:versions] = ["0.6.18"]
-default[:nodejs][:aliases]  = { :"0.6.18" => "0.6" }
-default[:nodejs][:default]  = "0.6.18"
+default[:nodejs][:versions] = ["0.6.19"]
+default[:nodejs][:aliases]  = { "0.6.19" => "0.6" }
+default[:nodejs][:default]  = "0.6.19"
