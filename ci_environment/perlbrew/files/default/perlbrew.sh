b750be4b5bd55089f5c6f3dbeda501e264541072
Michael S. Klishin <michael@novemberain.com>
2012-07-21 23:16:52 +0400

    --notest is not a good idea for global defaults, take it out

--- ci_environment/perlbrew/files/default/perlbrew.sh
+++ ci_environment/perlbrew/files/default/perlbrew.sh
@@ -4,4 +4,4 @@
 
 # cpan.mirrors.travis-ci.org is link-local to travis-ci.org worker machines, hosted in southern Germany.
 # We add one German and one Austrian mirror for better availability. MK.
-export PERL_CPANM_OPT="--notest --mirror http://cpan.mirrors.travis-ci.org --mirror http://gd.tuwien.ac.at/languages/perl/CPAN/ --mirror http://ftp-stud.hs-esslingen.de/pub/Mirrors/CPAN/"
+export PERL_CPANM_OPT="--mirror http://cpan.mirrors.travis-ci.org --mirror http://gd.tuwien.ac.at/languages/perl/CPAN/ --mirror http://ftp-stud.hs-esslingen.de/pub/Mirrors/CPAN/"
