78cc1e6b4c63a60978ce688e9f879bc5c5e48e66
Michael S. Klishin <michael@novemberain.com>
2012-08-04 18:30:22 +0400

    Correctly set owner and group for Composer files
    The attributes that were used previously are not set, gotta use those from
    travis_build_environment

--- ci_environment/composer/recipes/default.rb
+++ ci_environment/composer/recipes/default.rb
@@ -1,20 +1,19 @@
 include_recipe "php::multi"
 
-phpenv_path = "#{node.travis_build_environment.home}/.phpenv"
+phpenv_path = File.join(node.travis_build_environment.home, ".phpenv")
 
 node[:php][:multi][:versions].each do |php_version|
-
   bin_path = "#{phpenv_path}/versions/#{php_version}/bin"
   remote_file "#{bin_path}/composer.phar" do
     source "http://getcomposer.org/composer.phar"
-    owner  node[:phpbuild][:user]
-    group  node[:phpbuild][:group]
+    owner  node.travis_build_environment.user
+    group  node.travis_build_environment.group
     mode   "0644"
   end
 
   template "#{bin_path}/composer" do
-    owner  node[:phpbuild][:user]
-    group  node[:phpbuild][:group]
+    owner  node.travis_build_environment.user
+    group  node.travis_build_environment.group
     mode   "0755"
     source "composer.erb"
     variables(:phpbin_path => bin_path)
