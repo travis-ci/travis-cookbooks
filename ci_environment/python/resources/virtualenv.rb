54f5ad4f0e43438fee83efc08b30d88bc558839d
Michael S. Klishin <michael@novemberain.com>
2012-08-17 03:34:18 +0400

    Create 2 sets of virtualenvs, with --system-site-packages enabled and without
    There is no way to install some packages via pip but it is a good idea to isolate
    from the rest of the system for most projects (that is why --s-s-p is disabled by
    default in virtualenv env now).
    
    So the best we can offer is to create two sets and later adapt Python builder
    to activate the right one based on an extra .travis.yml attribute.

--- ci_environment/python/resources/virtualenv.rb
+++ ci_environment/python/resources/virtualenv.rb
@@ -24,3 +24,5 @@ attribute :path, :kind_of => String, :name_attribute => true
 attribute :interpreter, :default => 'python2.6'
 attribute :owner, :regex => Chef::Config[:user_valid_regex]
 attribute :group, :regex => Chef::Config[:group_valid_regex]
+
+attribute :system_site_packages, :default => false, :kind_of => [FalseClass, TrueClass]
