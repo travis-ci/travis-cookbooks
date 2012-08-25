5671238fb7dc9da3d36c3d2d352eec10956306b0
Michael S. Klishin <michael@novemberain.com>
2012-08-25 11:40:44 +0400

    Revert "Leiningen 2.0.0-preview9"
    This reverts commit d7955ef3ce65efffd8e27a5faac8f1edfa6bdfa3.
    
    There is a subtle issue that prevents provisioning from finishing successfully,
    even though lein2 is functional from within a project directory after that.
    
    See technomancy/leiningen#747 for more info. We have to go back to preview8 for
    now.

--- ci_environment/leiningen/attributes/default.rb
+++ ci_environment/leiningen/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:leiningen][:lein1][:version]        = "1.7.1"
 default[:leiningen][:lein1][:install_script] = "https://github.com/technomancy/leiningen/raw/#{leiningen[:lein1][:version]}/bin/lein"
 
-default[:leiningen][:lein2][:version]        = "2.0.0-preview9"
+default[:leiningen][:lein2][:version]        = "2.0.0-preview8"
 default[:leiningen][:lein2][:install_script] = "https://raw.github.com/technomancy/leiningen/preview/bin/lein"
