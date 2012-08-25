461c9b9c5d1b7dff0c09fbeeea77d37182fc67d5
Michael S. Klishin <michael@novemberain.com>
2012-08-25 20:17:30 +0400

    Revert "Prepare a workaround for the Leiningen 2 Preview 9 provisioning issue"
    This reverts commit fabd1289f525d6e3e5e24bac0ba7fdc49ebbb85c.
    
    The workaround does not completely solve the issue. Bailing out!

--- ci_environment/leiningen/files/default/init.clj
+++
@@ -1,4 +0,0 @@
-;; https://github.com/technomancy/leiningen/issues/747#issuecomment-8024136
-(swap! leiningen.core.project/default-profiles
-       update-in [:base] assoc :repositories
-       [["clojars" {:url "https://clojars.org/repo/"}]])
