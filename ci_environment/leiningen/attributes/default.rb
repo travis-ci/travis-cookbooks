2c56ae19f7109b09dc10de9584ab91c3388d689b
Michael S. Klishin <michael@novemberain.com>
2012-08-25 11:50:59 +0400

    Make sure we download lein2 w.r.t. the version we want

--- ci_environment/leiningen/attributes/default.rb
+++ ci_environment/leiningen/attributes/default.rb
@@ -2,4 +2,4 @@ default[:leiningen][:lein1][:version]        = "1.7.1"
 default[:leiningen][:lein1][:install_script] = "https://github.com/technomancy/leiningen/raw/#{leiningen[:lein1][:version]}/bin/lein"
 
 default[:leiningen][:lein2][:version]        = "2.0.0-preview8"
-default[:leiningen][:lein2][:install_script] = "https://raw.github.com/technomancy/leiningen/preview/bin/lein"
+default[:leiningen][:lein2][:install_script] = "https://github.com/technomancy/leiningen/raw/#{leiningen[:lein2][:version]}/bin/lein"
