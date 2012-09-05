8973ae88211e2d3573cb75bdcf1cf80115bac297
Michael S. Klishin <michael@novemberain.com>
2012-09-05 20:16:16 +0400

    OTP R15B02

--- ci_environment/kerl/attributes/source.rb
+++ ci_environment/kerl/attributes/source.rb
@@ -1,2 +1,2 @@
-default[:kerl][:releases] = %w(R14B02 R14B03 R14B04 R15B R15B01)
+default[:kerl][:releases] = %w(R14B02 R14B03 R14B04 R15B R15B01 R15B02)
 default[:kerl][:path]     = "/usr/local/bin/kerl"
