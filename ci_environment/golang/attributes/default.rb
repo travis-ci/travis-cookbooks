96d828e301e8d3e741734af569655c52727a8fa0
Michael S. Klishin <michael@novemberain.com>
2012-09-20 18:00:45 +0400

    Bump go to g1.0.2
    Fixes travis-ci/travis-cookbooks#91

--- ci_environment/golang/attributes/default.rb
+++ ci_environment/golang/attributes/default.rb
@@ -2,7 +2,7 @@ default[:golang] = {
   # can be "stable" or "tip"
   :version => "stable",
   :multi => {
-    :versions => %w(go1),
-    :default_version  => "go1"
+    :versions => %w(go1.0.2),
+    :default_version  => "go1.0.2"
   }
 }
