dd42a70607c9e4a813e0dafe45d746f96e7fe36c
Michael S. Klishin <michael@novemberain.com>
2012-10-13 10:21:29 +0400

    remove all the /dev/null redirection business that makes builds failures very misleading

--- ci_environment/phpbuild/files/default/after-install.d/phpunit.sh
+++ ci_environment/phpbuild/files/default/after-install.d/phpunit.sh
@@ -6,22 +6,20 @@ pyrus="$PREFIX/bin/pyrus"
 pear="$PREFIX/bin/pear"
 
 use_pear() {
-    {
-        "$pear" upgrade pear
+    "$pear" upgrade pear
 
-        "$pear" channel-discover components.ez.no
-        "$pear" channel-discover pear.symfony-project.com
-        "$pear" channel-discover pear.phpunit.de
+    "$pear" channel-discover components.ez.no
+    "$pear" channel-discover pear.symfony-project.com
+    "$pear" channel-discover pear.phpunit.de
 
-        if [[ "$PHP_VERSION" == "5.2.17" ]]
-        then
-            echo "Installing PHPUnit 3.6.x for PHP 5.2..."
-            "$pear" install "phpunit/PHPUnit-3.6.12"
-        else
-            echo "Installing latest PHPUnit..."
-            "$pear" install "phpunit/PHPUnit"
-        fi
-    } > /dev/null
+    if [[ "$PHP_VERSION" == "5.2.17" ]]
+    then
+        echo "Installing PHPUnit 3.6.x for PHP 5.2..."
+        "$pear" install "phpunit/PHPUnit-3.6.12"
+    else
+        echo "Installing latest PHPUnit..."
+        "$pear" install "phpunit/PHPUnit"
+    fi
 }
 
 use_pyrus() {
@@ -32,17 +30,17 @@ use_pyrus() {
         local ezcBase="$base_url/Base-1.8.tgz"
         local ezcConsoleTools="$base_url/ConsoleTools-1.6.1.tgz"
 
-        "$pyrus" install "$ezcBase" "$ezcConsoleTools" > /dev/null
+        "$pyrus" install "$ezcBase" "$ezcConsoleTools"
     }
 
     # Set up the required channels
-    "$pyrus" channel-discover "components.ez.no" > /dev/null
-    "$pyrus" channel-discover "pear.symfony-project.com" > /dev/null
-    "$pyrus" channel-discover "pear.phpunit.de" > /dev/null
+    "$pyrus" channel-discover "components.ez.no"
+    "$pyrus" channel-discover "pear.symfony-project.com"
+    "$pyrus" channel-discover "pear.phpunit.de"
 
     _ezcomponents_hack
 
-    "$pyrus" install "phpunit/PHPUnit" > /dev/null
+    "$pyrus" install "phpunit/PHPUnit"
 }
 
 if [ -f "$pyrus" ]; then
