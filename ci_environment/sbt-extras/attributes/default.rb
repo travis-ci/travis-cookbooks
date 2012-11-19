a0a2520025ead48a605ad1a0502d8d928e847f82
Gilles Cornu <git@gilles.cornu.name>
2012-11-19 01:44:14 +0100

    Configure sbt-extras for Travis CI context

--- ci_environment/sbt-extras/attributes/default.rb
+++ ci_environment/sbt-extras/attributes/default.rb
@@ -12,22 +12,22 @@ default['sbt-extras']['default_sbt_version']   = '0.12.1' # ATTENTION: It must m
 
 default['sbt-extras']['setup_dir']         = '/opt/sbt-extras'
 default['sbt-extras']['script_name']       = 'sbt'
-default['sbt-extras']['owner']             = 'root' 
+default['sbt-extras']['owner']             = 'travis' 
 default['sbt-extras']['group']             = 'sbt'      # group members are power users allowed to install sbt versions on demand
-default['sbt-extras']['group_new_members'] = []         # %w{ admin1 coder1 }
+default['sbt-extras']['group_new_members'] = %w{ travis }
 default['sbt-extras']['bin_symlink']       = '/usr/bin/sbt'
 
 default['sbt-extras']['config_dir']        = '/etc/sbt'
 #Template installation is disabled if filename is an empty string: 
 default['sbt-extras']['sbtopts_filename']  = 'sbtopts'
-default['sbt-extras']['jvmopts_filename']  = ''         # disabled, change to 'jvmopts' if wanted.
+default['sbt-extras']['jvmopts_filename']  = 'jvmopts'
 
 # Following Parameters will be used during recipe execution and aslo used by /etc/sbt/sbtopts template
-default['sbt-extras']['sbtopts']['mem']    = 512 # in megabytes, Tuning of JVM -Xmx and -Xms 
+default['sbt-extras']['sbtopts']['mem']    = 1024 # in megabytes, Tuning of JVM -Xmx and -Xms 
 
 default['sbt-extras']['preinstall_cmd']['timeout']  = 300 # A maximum of 5 minutes is allowed to download dependencies of a specific scala version.
 
 # Optionally pre-install dependant libraries of requested sbt versions in user own environment
-#default['sbt-extras']['preinstall_matrix']['coder1'] = %w{ 0.12.1 0.12.0 0.11.3 0.11.2 0.11.1 }
+default['sbt-extras']['preinstall_matrix']['travis'] = %w{ 0.12.1 0.12.0 0.11.3 0.11.2 0.11.1 }
   # Known Problem: sbt 'boot' libraries are correclty installed since 0.11+ 
   # (see https://github.com/gildegoma/chef-sbt-extras/issues/5#issuecomment-10576361)
