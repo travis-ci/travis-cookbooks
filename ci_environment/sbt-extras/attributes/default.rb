8b4832190c91e654574b2684f9c6117504b54eae
Gilles Cornu <git@gilles.cornu.name>
2012-11-23 14:54:24 +0100

    Use travis_build_environment attributes (user, group, home)
    (and clean trailing spaces by the way)

--- ci_environment/sbt-extras/attributes/default.rb
+++ ci_environment/sbt-extras/attributes/default.rb
@@ -1,9 +1,12 @@
+include_attribute 'travis_build_environment'
+
 case platform
 when 'mac_os_x'
-  set['sbt-extras']['user_home_basedir']   = '/Users'
+  set['sbt-extras']['user_home_basedir']    = '/Users'
 else # usual base directory on unix systems:
-  set['sbt-extras']['user_home_basedir']   = '/home'
+  set['sbt-extras']['user_home_basedir']    = '/home'
 end
+override['sbt-extras']['user_home_basedir'] = node['travis_build_environment']['home'].split("/#{node['travis_build_environment']['user']}").first
 
 default['sbt-extras']['download_url']      = 'https://github.com/gildegoma/sbt-extras/raw/139803ca3880c20799bca030b33261c4509dc2d5/sbt'
 # Refer to this fork, waiting for https://github.com/paulp/sbt-extras/pull/36 to be accepted and merged into master project.
@@ -12,22 +15,22 @@ default['sbt-extras']['default_sbt_version']   = '0.12.1' # ATTENTION: It must m
 
 default['sbt-extras']['setup_dir']         = '/opt/sbt-extras'
 default['sbt-extras']['script_name']       = 'sbt'
-default['sbt-extras']['owner']             = 'travis' 
-default['sbt-extras']['group']             = 'sbt'      # group members are power users allowed to install sbt versions on demand
-default['sbt-extras']['group_new_members'] = %w{ travis }
+default['sbt-extras']['owner']             = node['travis_build_environment']['user']
+default['sbt-extras']['group']             = node['travis_build_environment']['group']
+default['sbt-extras']['group_new_members'] = []
 default['sbt-extras']['bin_symlink']       = '/usr/bin/sbt'
 
 default['sbt-extras']['config_dir']        = '/etc/sbt'
-#Template installation is disabled if filename is an empty string: 
+#Template installation is disabled if filename is an empty string:
 default['sbt-extras']['sbtopts_filename']  = 'sbtopts'
 default['sbt-extras']['jvmopts_filename']  = 'jvmopts'
 
-# Following Parameters will be used during recipe execution and aslo used by /etc/sbt/sbtopts template
-default['sbt-extras']['sbtopts']['mem']    = 1024 # in megabytes, Tuning of JVM -Xmx and -Xms 
+# Following Parameters will be used during recipe execution and also when installing /etc/sbt/sbtopts template
+default['sbt-extras']['sbtopts']['mem']    = 1024 # in megabytes, Tuning of JVM -Xmx and -Xms
 
 default['sbt-extras']['preinstall_cmd']['timeout']  = 300 # A maximum of 5 minutes is allowed to download dependencies of a specific scala version.
 
 # Optionally pre-install dependant libraries of requested sbt versions in user own environment
-default['sbt-extras']['preinstall_matrix']['travis'] = %w{ 0.12.1 0.12.0 0.11.3 0.11.2 0.11.1 }
-  # Known Problem: sbt 'boot' libraries are correclty installed since 0.11+ 
+default['sbt-extras']['preinstall_matrix'][node['travis_build_environment']['user']] = %w{ 0.12.1 0.12.0 0.11.3 0.11.2 0.11.1 }
+  # Known Problem: sbt 'boot' libraries are correclty installed since 0.11+
   # (see https://github.com/gildegoma/chef-sbt-extras/issues/5#issuecomment-10576361)
