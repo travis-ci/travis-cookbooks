bde351d7873a0788894ba7cc278c1d7f71c6f0fe
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-12-13 23:27:11 +1300

    bump the 0.8 node version

--- ci_environment/nodejs/attributes/multi.rb
+++ ci_environment/nodejs/attributes/multi.rb
@@ -1,3 +1,3 @@
-default[:nodejs][:versions] = ["0.8.14"]
-default[:nodejs][:aliases]  = { "0.8.14" => "0.8" }
-default[:nodejs][:default]  = "0.8.14"
+default[:nodejs][:versions] = ["0.8.15"]
+default[:nodejs][:aliases]  = { "0.8.15" => "0.8" }
+default[:nodejs][:default]  = "0.8.15"
