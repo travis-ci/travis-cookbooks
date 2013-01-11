47f9c0b8d70bef1c1c0c0bac64f9b63cc1cc7d88
Levent Ali <lebreeze@gmail.com>
2013-01-11 14:13:53 +0000

    Adding Sphinx 2.0.4 (the default on Ubuntu 12.04)

--- ci_environment/sphinx/attributes/default.rb
+++ ci_environment/sphinx/attributes/default.rb
@@ -1,8 +1,9 @@
 default[:sphinx] = {
   :versions => {
+    '2.0.4-release' => '/usr/local/sphinx-2.0.4',
     '2.0.1-beta' => '/usr/local/sphinx-2.0.1',
     '1.10-beta'  => '/usr/local/sphinx-1.10',
     '0.9.9'      => '/usr/local/sphinx-0.9.9'
   },
-  :linked => "2.0.1"
+  :linked => "2.0.4"
 }
