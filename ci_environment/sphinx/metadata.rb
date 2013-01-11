0531d1d0a543a4a71735ab54d7a520bbacd1b45a
Levent Ali <lebreeze@gmail.com>
2013-01-11 14:23:24 +0000

    Remove sphinx 2.0.1-beta

--- ci_environment/sphinx/metadata.rb
+++ ci_environment/sphinx/metadata.rb
@@ -7,9 +7,8 @@ version           '0.1.1'
 depends           'build-essential'
 
 recipe 'sphinx',               'Installs Sphinx 2.0.4-release'
-recipe 'sphinx::all',          'Installs Sphinx 2.0.4-release, 2.0.1-beta, 1.10-beta and 0.9.9'
+recipe 'sphinx::all',          'Installs Sphinx 2.0.4-release, 1.10-beta and 0.9.9'
 recipe 'sphinx::sphinx-2.0.4', 'Installs Sphinx 2.0.4-release'
-recipe 'sphinx::sphinx-2.0.1', 'Installs Sphinx 2.0.1-beta'
 recipe 'sphinx::sphinx-1.10',  'Installs Sphinx 1.10-beta'
 recipe 'sphinx::sphinx-0.9.9', 'Installs Sphinx 0.9.9'
 
