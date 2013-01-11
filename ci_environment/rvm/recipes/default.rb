8c1f3fcdd3bb49eccad08ce4bae25c42a9d5b62f
Jon Leighton <j@jonathanleighton.com>
2013-01-11 15:59:39 +0000

    Install libffi for ruby
    This ensures that the "fiddle" stdlib gets built.

--- ci_environment/rvm/recipes/default.rb
+++ ci_environment/rvm/recipes/default.rb
@@ -34,6 +34,7 @@ include_recipe "libreadline"
 include_recipe "libxml"
 include_recipe "libssl"
 include_recipe "libncurses"
+include_recipe "libffi"
 
 bash "install RVM" do
   user        node.travis_build_environment.user
