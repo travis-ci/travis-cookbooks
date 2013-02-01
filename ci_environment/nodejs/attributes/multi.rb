7c5a492bbde6fffa38c704470f62a4db666ea5fe
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-02-02 10:14:34 +1300

    update default node version

--- ci_environment/nodejs/attributes/multi.rb
+++ ci_environment/nodejs/attributes/multi.rb
@@ -1,3 +1,3 @@
-default[:nodejs][:versions] = ["0.8.15"]
-default[:nodejs][:aliases]  = { "0.8.15" => "0.8" }
-default[:nodejs][:default]  = "0.8.15"
+default[:nodejs][:versions] = ["0.8.18"]
+default[:nodejs][:aliases]  = { "0.8.18" => "0.8" }
+default[:nodejs][:default]  = "0.8.18"
