6701136660f29553e634d6b1675599315c490696
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-02-04 21:20:30 +1300

    use the correct R16A name (R16A_RELEASE_CANDIDATE)

--- ci_environment/kerl/attributes/source.rb
+++ ci_environment/kerl/attributes/source.rb
@@ -1,2 +1,2 @@
-default[:kerl][:releases] = %w(R14B02 R14B03 R14B04 R15B R15B01 R15B02 R15B03 R16A)
+default[:kerl][:releases] = %w(R14B02 R14B03 R14B04 R15B R15B01 R15B02 R15B03 R16A_RELEASE_CANDIDATE)
 default[:kerl][:path]     = "/usr/local/bin/kerl"
