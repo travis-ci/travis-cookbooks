138252666819d36ded4eb319dbe03a7f2e0af655
Gilles Cornu <git@gilles.cornu.name>
2013-02-16 01:03:05 +0100

    Update sbt-extras (now default: 0.12.2). Fix #150

--- ci_environment/sbt/attributes/default.rb
+++ ci_environment/sbt/attributes/default.rb
@@ -8,9 +8,9 @@ else # usual base directory on unix systems:
 end
 override['sbt-extras']['user_home_basedir'] = node['travis_build_environment']['home'].split("/#{node['travis_build_environment']['user']}").first
 
-default['sbt-extras']['download_url']      = 'https://github.com/gildegoma/sbt-extras/raw/63e8d0f7a6bd25c81f59befc040e6eb3b35e2501/sbt'
-# Refer to this fork, waiting for https://github.com/paulp/sbt-extras/pull/36 to be accepted and merged into master project.
-default['sbt-extras']['default_sbt_version']   = '0.12.2-RC1' # ATTENTION: It must match with effective default sbt of installed script.
+default['sbt-extras']['download_url']      = 'https://raw.github.com/gildegoma/sbt-extras/travis-ci/sbt'
+# Refer to this forked 'travis-ci' branch, waiting for https://github.com/paulp/sbt-extras/pull/36 to be accepted and merged into master project.
+default['sbt-extras']['default_sbt_version']   = '0.12.2' # ATTENTION: It must match with effective default sbt of installed script.
 # Note: ideally, the default sbt version should be 'found' in downloaded script content (see issue #7)
 
 default['sbt-extras']['setup_dir']         = '/opt/sbt-extras'
@@ -31,6 +31,6 @@ default['sbt-extras']['sbtopts']['mem']    = 1024 # in megabytes, Tuning of JVM 
 default['sbt-extras']['preinstall_cmd']['timeout']  = 300 # A maximum of 5 minutes is allowed to download dependencies of a specific scala version.
 
 # Optionally pre-install dependant libraries of requested sbt versions in user own environment
-default['sbt-extras']['preinstall_matrix'][node['travis_build_environment']['user']] = %w{ 0.12.1 0.12.0 0.11.3 0.11.2 0.11.1 }
+default['sbt-extras']['preinstall_matrix'][node['travis_build_environment']['user']] = %w{ 0.12.2 0.12.1 0.12.0 0.11.3 0.11.2 0.11.1 }
   # Known Problem: sbt 'boot' libraries are correclty installed since 0.11+
   # (see https://github.com/gildegoma/chef-sbt-extras/issues/5#issuecomment-10576361)
