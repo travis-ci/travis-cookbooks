5e5deafb977bcbc31bec78310bc1672debbcdf25
Mathias Meyer <meyer@paperplanes.de>
2013-02-28 13:13:21 +0100

    Remove packages not available on Bluebox' 12.04.

--- worker_host/networking_basic/metadata.rb
+++ worker_host/networking_basic/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "fred@frederico-araujo.com"
 license          "Apache 2.0"
 description      "Install Basic Netwoking Tools ans Utilities on Debian5 and Ubuntu10"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.0.3"
 
 %w{ ubuntu debian }.each do |os|
   supports os
