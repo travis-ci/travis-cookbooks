25a6116574676e17b09aebf5dc7460239ab90b3b
Michael Klishin <michael@defprotocol.org>
2013-03-04 23:49:34 +0400

    Upgrade Erlang/OTP R16 to B

--- ci_environment/kerl/attributes/source.rb
+++ ci_environment/kerl/attributes/source.rb
@@ -1,2 +1,2 @@
-default[:kerl][:releases] = %w(R14B02 R14B03 R14B04 R15B R15B01 R15B02 R15B03 R16A)
+default[:kerl][:releases] = %w(R14B02 R14B03 R14B04 R15B R15B01 R15B02 R15B03 R16B)
 default[:kerl][:path]     = "/usr/local/bin/kerl"
