182afa68e2d14d797f2336f452a574ba85072fff
Loïc Frering <loic.frering@gmail.com>
2013-03-05 11:27:55 +0100

    Preinstalled PECL extensions are not enabled by default.

--- ci_environment/php/recipes/extensions.rb
+++ ci_environment/php/recipes/extensions.rb
@@ -13,3 +13,12 @@ node.php.multi.extensions.each do |php_extension, options|
     action          :install
   end
 end
+
+node.php.multi.versions.each do |php_version|
+  bash "disable preinstalled PECL extensions for PHP #{php_version}" do
+    user        node.travis_build_environment.user
+    group       node.travis_build_environment.group
+    environment Hash["HOME" => node.travis_build_environment.home]
+    code        "sed -i '/^extension=/d' $HOME/.phpenv/versions/#{php_version}/etc/php.ini"
+  end
+end
