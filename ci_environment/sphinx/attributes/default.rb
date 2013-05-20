9417c24dd1e65f702424e4080a208fcc98452328
Gilles Cornu <git@gilles.cornu.name>
2013-05-20 19:27:26 +0200

    Sphinx 0.9.9 is outdated and no more online
    Sphinx Technologies has removed 0.9.x packages (more than 4 years old)
    from their download site. Release 1.10-beta is still online, but
    recommended releases are now 2.0 and 2.1.

--- ci_environment/sphinx/attributes/default.rb
+++ ci_environment/sphinx/attributes/default.rb
@@ -1,8 +1,7 @@
 default[:sphinx] = {
   :versions => {
     '2.0.4-release' => '/usr/local/sphinx-2.0.4',
-    '1.10-beta'  => '/usr/local/sphinx-1.10',
-    '0.9.9'      => '/usr/local/sphinx-0.9.9'
+    '1.10-beta'  => '/usr/local/sphinx-1.10'
   },
   :linked => "2.0.4"
 }
