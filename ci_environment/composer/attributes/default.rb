dfa310b9418f021066c6a5ecaea7f1d4c1def321
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-06-11 13:33:13 +0200

    add support for a global github token for composer

---
+++ ci_environment/composer/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:config] = {
+  :github_oauth_token => nil
+}
--- ci_environment/composer/recipes/default.rb
+++ ci_environment/composer/recipes/default.rb
@@ -1,6 +1,7 @@
 include_recipe "php::multi"
 
-phpenv_path = File.join(node.travis_build_environment.home, ".phpenv")
+phpenv_path   = File.join(node.travis_build_environment.home, ".phpenv")
+composer_path = File.join(node.travis_build_environment.home, ".composer")
 
 node[:php][:multi][:versions].each do |php_version|
   bin_path = "#{phpenv_path}/versions/#{php_version}/bin"
@@ -17,5 +18,12 @@ node[:php][:multi][:versions].each do |php_version|
     mode   "0755"
     source "composer.erb"
     variables(:phpbin_path => bin_path)
+  end
+
+  template "#{composer_path}/config.json" do
+    owner  node.travis_build_environment.user
+    group  node.travis_build_environment.group
+    source "config.json.erb"
+    variables(:github_oauth_token => node[:config][:github_oauth_token])
   end
 end
