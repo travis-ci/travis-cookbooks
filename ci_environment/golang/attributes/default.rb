579058b286ddccc862db1e8c6bc93af5bbc0e588
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-06-13 15:17:32 +0200

    update the go versions and the aliasssssss

--- ci_environment/golang/attributes/default.rb
+++ ci_environment/golang/attributes/default.rb
@@ -2,11 +2,12 @@ default[:golang] = {
   # can be "stable" or "tip"
   :version => "stable",
   :multi => {
-    :versions => %w(go1.0.3 go1.1),
-    :default_version  => "go1.1",
+    :versions => %w(go1.0.3 go1.1.1),
+    :default_version  => "go1.1.1",
     :aliases => {
+      "go1"   => "go1.0.3",
       "go1.0" => "go1.0.3",
-      "go1" => "go1.1"
+      "go1.1" => "go1.1.1"
     }
   }
 }
