9057439d416cc192edf15724e94af494858da824
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-06-14 17:01:48 -0700

    Add perl-5.8

--- ci_environment/perlbrew/attributes/multi.rb
+++ ci_environment/perlbrew/attributes/multi.rb
@@ -5,6 +5,7 @@ default[:perlbrew] = {
                { :name => "5.16", :version => "perl-5.16.3" },
                { :name => "5.14", :version => "perl-5.14.4" },
                { :name => "5.12", :version => "perl-5.12.5" },
-               { :name => "5.10", :version => "perl-5.10.1" }],
+               { :name => "5.10", :version => "perl-5.10.1" },
+               { :name => "5.8",  :version => "perl-5.8.9"  }],
   :modules => %w(Dist::Zilla Moose Test::Pod Test::Pod::Coverage Test::Exception Test::Kwalitee Dist::Zilla::Plugin::Bootstrap::lib LWP Module::Install Test::Most)
 }
