0376ea0d59399f0adc3cec1022ac096002589ea2
Mathias Meyer <meyer@paperplanes.de>
2013-07-17 11:08:26 +0200

    Don't add timestamps to the worker's runit log.

--- worker_host/travis_worker/metadata.rb
+++ worker_host/travis_worker/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Travis CI"
 maintainer_email "mathias@travis-ci.org"
 license          "Apache 2.0"
 description      "Installs/Configures the Travis Worker"
-version          "0.5.7"
+version          "0.5.8"
 
 %w{ jruby runit }.each do |cb|
   depends cb
