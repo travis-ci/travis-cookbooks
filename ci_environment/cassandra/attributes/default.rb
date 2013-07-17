c448d986dac589905704a5a0002b0742ab61d1dd
Mathias Meyer <meyer@paperplanes.de>
2013-07-17 16:33:12 +0200

    Switch Cassandra config dir.
    The init script contains a declaration of CASSANDRA_CONF, but for
    some reason that is empty when the cassandra script is called.
    
    Cassandra then loads the cassandra.in.sh file which in turn defaults
    to CASSANDRA_HOME/conf.
    
    Related to travis-ci/travis-ci#1203.

--- ci_environment/cassandra/attributes/default.rb
+++ ci_environment/cassandra/attributes/default.rb
@@ -18,7 +18,7 @@ default[:cassandra] = {
   :installation_dir => "/usr/local/cassandra",
   :bin_dir          => "/usr/local/cassandra/bin",
   :lib_dir          => "/usr/local/cassandra/lib",
-  :conf_dir         => "/usr/local/cassandra/etc",
+  :conf_dir         => "/usr/local/cassandra/conf",
   # commit log, data directory, saved caches and so on are all stored under the data root. MK.
   :data_root_dir    => "/var/lib/cassandra/",
   :log_dir          => "/var/log/cassandra/",
