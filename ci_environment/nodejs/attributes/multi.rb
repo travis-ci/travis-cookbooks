9515d9d92107fa84d876db3be10bde7ff430ba4b
James Butler <james.butler@sandfox.co.uk>
2013-08-29 13:03:59 +0100

    bump default stable nodejs version to latest.

--- ci_environment/nodejs/attributes/multi.rb
+++ ci_environment/nodejs/attributes/multi.rb
@@ -1,3 +1,3 @@
-default[:nodejs][:default]  = "0.10.12"
+default[:nodejs][:default]  = "0.10.17"
 default[:nodejs][:versions] = [ node[:nodejs][:default] ]
 default[:nodejs][:aliases]  = { node[:nodejs][:default] => node[:nodejs][:default][/\d+\.\d+/] }
