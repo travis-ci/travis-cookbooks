2323fcd14968376cd39d9e3d7d408cf0ec065b3c
Gilles Cornu <git@gilles.cornu.name>
2013-10-02 14:53:42 +0200

    Remove outdated `redis::default` recipe
    Close travis-ci/travis-cookbooks#211

--- ci_environment/redis/attributes/default.rb
+++ ci_environment/redis/attributes/default.rb
@@ -1,10 +1,1 @@
-arch = kernel['machine'] =~ /x86_64/ ? "amd64" : "i386"
-
-default[:redis] = {
-  :package => {
-    :url => "http://files.travis-ci.org/packages/deb/redis/redis-server_2.4.16-1~dotdeb.0_#{arch}.deb"
-  },
-  :service => {
-    :enabled => false
-  }
-}
+default['redis']['service']['enabled'] = false
--- ci_environment/redis/metadata.rb
+++ ci_environment/redis/metadata.rb
@@ -2,3 +2,5 @@ name             "redis"
 maintainer       "Travis CI Development Team"
 license          "Apache v2.0"
 description      "Installs/Configures redis"
+
+recipe           "redis::ppa",       "Installis Redis Server from ppa:rwky/redis"
