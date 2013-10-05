396edd03da035ec9dfcbb7ef9c9815a07504ff90
Michael Klishin <michael@clojurewerkz.org>
2013-10-05 18:58:48 +0400

    Sync Neo4J Server cookbook with upstream
    Primarily for a long overdue upgrade to 1.9.

--- ci_environment/neo4j-server/metadata.rb
+++ ci_environment/neo4j-server/metadata.rb
@@ -1,6 +1,12 @@
-name             "neo4j-server"
-maintainer       "Travis CI Development Team"
-license          "Apache v2.0"
-description      "Installs/Configures neo4j-server"
+maintainer        'Michael Klishin'
+maintainer_email  'michael@defprotocol.org'
+license           'MIT'
+description       'This cookbook currently provides Neo4J Server 1.8 (Community Edition) but can be used to install other versions by overriding data bag attributes.'
+version           '0.0.1'
+depends           'java', '>= 0.0.1'
 
-depends          "java"
+recipe 'neo4j-server::tarball', 'Installs a Neo4j server'
+
+%w{ ubuntu debian redhat centos scientific amazon oracle }.each do |os|
+  supports os
+end
