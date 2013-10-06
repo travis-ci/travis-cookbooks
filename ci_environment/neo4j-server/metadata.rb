7aa3031102a6660d6db5c43b61ca6dc16613e014
Michael Klishin <michael@clojurewerkz.org>
2013-10-06 04:05:08 +0400

    Sensible version

--- ci_environment/neo4j-server/metadata.rb
+++ ci_environment/neo4j-server/metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Michael Klishin'
 maintainer_email  'michael@defprotocol.org'
 license           'MIT'
 description       'This cookbook currently provides Neo4J Server 1.8 (Community Edition) but can be used to install other versions by overriding data bag attributes.'
-version           '0.0.1'
+version           '1.0.0'
 depends           'java', '>= 0.0.1'
 
 recipe 'neo4j-server::tarball', 'Installs a Neo4j server'
