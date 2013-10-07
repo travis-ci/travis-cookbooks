d562f2bd390c0cae5d3c95994c819fa46498f499
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-10-07 18:03:46 +0200

    oops, go1.1.1 => go1.1.2 for realz this time

--- ci_environment/golang/attributes/default.rb
+++ ci_environment/golang/attributes/default.rb
@@ -2,8 +2,8 @@ default[:golang] = {
   # can be "stable" or "tip"
   :version => "stable",
   :multi => {
-    :versions => %w(go1.0.3 go1.1.1),
-    :default_version  => "go1.1.1",
+    :versions => %w(go1.0.3 go1.1.2),
+    :default_version  => "go1.1.2",
     :aliases => {
       "go1"   => "go1.0.3",
       "go1.0" => "go1.0.3",
