1bfbf85542f1540dd899f653c5b8af9d2040760e
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-10-20 10:52:03 +0200

    bump nodejs 0.10 version

--- ci_environment/nodejs/attributes/multi.rb
+++ ci_environment/nodejs/attributes/multi.rb
@@ -1,3 +1,3 @@
-default[:nodejs][:default]  = "0.10.20"
+default[:nodejs][:default]  = "0.10.21"
 default[:nodejs][:versions] = [ node[:nodejs][:default] ]
 default[:nodejs][:aliases]  = { node[:nodejs][:default] => node[:nodejs][:default][/\d+\.\d+/] }
