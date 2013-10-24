bb630f215685bd174714725385365538c653fd7f
Michael Klishin <michael@clojurewerkz.org>
2013-10-24 04:42:14 +0400

    Update md5 checksum for Cassandra 2.0.1
    Taken from http://www.apache.org/dist/cassandra/2.0.1/apache-cassandra-2.0.1-bin.tar.gz.md5

--- ci_environment/cassandra/attributes/default.rb
+++ ci_environment/cassandra/attributes/default.rb
@@ -4,7 +4,7 @@ default[:cassandra] = {
   :version => cassandra_version,
   :tarball => {
     :url => "http://www.apache.org/dist/cassandra/#{cassandra_version}/apache-cassandra-#{cassandra_version}-bin.tar.gz",
-    :md5 => "f6a5738200b281ef098e90be3fa30cf2"
+    :md5 => "8e52eba7687fe8aa7a40a5fdce417e63"
   },
   :user => "cassandra",
   :jvm  => {
