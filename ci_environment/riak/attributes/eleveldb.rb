aa5afb653d8974d04e1d90afa0b3080956c77c12
Gilles Cornu <git@gilles.cornu.name>
2013-11-05 00:27:34 +0100

    Riak: Drop old cookbook and install from apt repo
    Close travis-ci/travis-ci#1090
    
    I renounced to use a more recent version of Basho cookbook (e.g. 2.4.0)
    because it relies on different cookbook dependencies for Erlang and
    apt repository was recommeded by Sean Cribbs himself, as illustrated in
    projects like https://github.com/basho/riak-python-client

--- ci_environment/riak/attributes/eleveldb.rb
+++
@@ -1,6 +0,0 @@
-#
-# Cookbook Name:: riak
-#
-
-default.riak.eleveldb.data_root = "/var/lib/riak/leveldb"
-
--- ci_environment/riak/metadata.rb
+++ ci_environment/riak/metadata.rb
@@ -1,38 +1,13 @@
-#
-# Author:: Benjamin Black (<b@b3k.us>) and Sean Cribbs (<sean@basho.com>)
-# Cookbook Name:: riak
-#
-# Copyright (c) 2011 Basho Technologies, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 name              "riak"
-maintainer        "Basho Technologies, Inc."
-maintainer_email  "riak@basho.com"
+maintainer        "Travis CI Team"
+maintainer_email  "contact@travis-ci.org"
 license           "Apache 2.0"
-description       "Installs and configures Riak distributed data store"
-version           "1.2.0"
+description       "Installs Riak from apt.basho.com and configure it for Continuous Integration purpose"
+version           "2.0.0"
 
-%w{ubuntu debian centos redhat suse fedora}.each do |os|
-  supports os
-end
+recipe            "riak::default", "Install Riak for a CI environment"
 
-%w{ iptables libssl }.each do |cb|
-  depends cb
-end
+supports          'ubuntu', '= 12.04'
 
-recipe            "riak", "Installs Riak"
-recipe            "riak::autoconf", "Automatically configure nodes from chef-server information."
-recipe            "riak::innostore", "Install and configure the Innostore backend."
-recipe            "riak::iptables", "Automatically configure iptables rules for Riak."
+depends           'apt'
+
