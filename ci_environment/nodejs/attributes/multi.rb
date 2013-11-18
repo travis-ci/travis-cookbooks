9da1254d416dc5661f56db97cc55372bfd60ca26
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-11-18 10:11:21 +0100

    set the default node to 0.10.22

--- ci_environment/nodejs/attributes/multi.rb
+++ ci_environment/nodejs/attributes/multi.rb
@@ -1,3 +1,3 @@
-default[:nodejs][:default]  = "0.10.21"
+default[:nodejs][:default]  = "0.10.22"
 default[:nodejs][:versions] = [ node[:nodejs][:default] ]
 default[:nodejs][:aliases]  = { node[:nodejs][:default] => node[:nodejs][:default][/\d+\.\d+/] }
