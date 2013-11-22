1d6a7ed8ff1ab5f0b60e7e9b08416055c4c3c4e5
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-11-22 13:41:58 +0100

    corrected a wrong variable assignment

--- ci_environment/haskell/attributes/multi.rb
+++ ci_environment/haskell/attributes/multi.rb
@@ -1,4 +1,4 @@
-default[:haskell][:multi] => {
+default[:haskell][:multi] = {
   :ghcs => {
     "7.6.3" => {
       :platform_version => "2013.2.0.0",
--- ci_environment/haskell/attributes/platform.rb
+++ ci_environment/haskell/attributes/platform.rb
@@ -1,3 +1,3 @@
-default[:haskell][:platform] => {
+default[:haskell][:platform] = {
   :version => "2012.2.0.0"
 }
