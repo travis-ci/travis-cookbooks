06997356b104815cc3351143b9593974a893c391
Loïc Frering <loic.frering@gmail.com>
2013-11-30 12:24:57 +0100

    Introduce HHVM's cookbook.
    The package recipe installs HHVM from the provided package repository.
    The default recipe just refers to the package one.

---
+++ ci_environment/hhvm/recipes/package.rb
@@ -0,0 +1,12 @@
+apt_repository "hhvm-repository" do
+  uri          "http://dl.hhvm.com/ubuntu"
+  distribution node['lsb']['codename']
+  components   ["main"]
+
+  action :add
+end
+
+package "hhvm" do
+  action :install
+  options "--force-yes"
+end
