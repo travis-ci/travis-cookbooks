52625cfc404e5b94060eb174f5c055bc3a532f30
Loïc Frering <loic.frering@gmail.com>
2013-11-30 12:38:06 +0100

    Install phpunit for HHVM.

--- ci_environment/php/recipes/hhvm.rb
+++ ci_environment/php/recipes/hhvm.rb
@@ -19,3 +19,20 @@ file "#{hhvm_path}/bin/php" do
 hhvm "$@"
   CONTENT
 end
+
+
+remote_file "#{hhvm_path}/bin/phpunit.phar" do
+  source "https://phar.phpunit.de/phpunit.phar"
+  owner  node.travis_build_environment.user
+  group  node.travis_build_environment.group
+end
+
+file "#{hhvm_path}/bin/phpunit" do
+  owner  node.travis_build_environment.user
+  group  node.travis_build_environment.group
+  mode   00755
+  content <<-CONTENT
+#!/usr/bin/env bash
+hhvm "#{hhvm_path}/bin/phpunit.phar" "$@"
+  CONTENT
+end
