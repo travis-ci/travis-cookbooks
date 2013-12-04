b2bd25920c875c52fd1e1dfebaffc60864a8e298
Mathias Meyer <meyer@paperplanes.de>
2013-12-04 19:05:47 +0100

    Add Go 1.2

--- ci_environment/golang/attributes/default.rb
+++ ci_environment/golang/attributes/default.rb
@@ -2,7 +2,7 @@ default[:golang] = {
   # can be "stable" or "tip"
   :version => "stable",
   :multi => {
-    :versions => %w(go1.0.3 go1.1.2),
+    :versions => %w(go1.0.3 go1.1.2 go1.2),
     :default_version  => "go1.1.2",
     :aliases => {
       "go1"   => "go1.0.3",
