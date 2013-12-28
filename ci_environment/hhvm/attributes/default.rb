cadf8f6a391afa88097a0ff30586a8935f3f209c
Loïc Frering <loic.frering@gmail.com>
2013-12-28 12:22:05 +0100

    Fix HHVM package's version.

--- ci_environment/hhvm/attributes/default.rb
+++ ci_environment/hhvm/attributes/default.rb
@@ -1,4 +1,4 @@
 default["hhvm"]["package"]["name"] = "hhvm"
-default["hhvm"]["package"]["version"] = "2.3.2"
+default["hhvm"]["package"]["version"] = "2.3.2~precise"
 
 default["hhvm"]["package"]["url"] = "https://s3.amazonaws.com/travis-php-packages/hhvm_2.3.0_amd64.deb"
