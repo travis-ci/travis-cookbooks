a90a3474f12eba82f9ffdb053f1a465d32f1e628
Josef Šimánek <retro@ballgag.cz>
2014-02-05 17:18:09 +0100

    Update phantomjs to new source and version.

--- ci_environment/phantomjs/attributes/default.rb
+++ ci_environment/phantomjs/attributes/default.rb
@@ -1,10 +1,10 @@
-version = "1.9.2"
+version = "1.9.7"
 arch    = kernel['machine'] =~ /x86_64/ ? "x86_64" : "i686"
 
 default[:phantomjs] = {
   :version => version,
   :arch    => arch,
   :tarball => {
-    :url  => "http://phantomjs.googlecode.com/files/phantomjs-#{version}-linux-#{arch}.tar.bz2"
+    :url => "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-#{version}-linux-#{arch}.tar.bz2
   }
 }
