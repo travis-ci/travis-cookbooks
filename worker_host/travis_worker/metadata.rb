0ce4ef4dcd7254a916eac17388756e1c6c742cc3
Mathias Meyer <meyer@paperplanes.de>
2014-02-07 10:26:31 +0100

    Bump cookbook version for travis_worker

--- worker_host/travis_worker/metadata.rb
+++ worker_host/travis_worker/metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Travis CI"
 maintainer_email "mathias@travis-ci.org"
 license          "Apache 2.0"
 description      "Installs/Configures the Travis Worker"
-version          "0.6.2"
+version          "0.6.3"
 
 %w{ jruby runit }.each do |cb|
   depends cb
