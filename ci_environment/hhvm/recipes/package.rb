fb3d344c12f17b2e6bf9555aefde78e6e61356f3
Loïc Frering <loic.frering@gmail.com>
2014-03-14 10:11:09 +0100

    Installing a boost-backport PPA repository.
    HHVM 2.4.2 requires libboost-* 1.49.0 which are not available on 12.04.
    
    Related to #281.

--- ci_environment/hhvm/recipes/package.rb
+++ ci_environment/hhvm/recipes/package.rb
@@ -1,9 +1,18 @@
+apt_repository "boost-backports" do
+  uri          "http://ppa.launchpad.net/mapnik/boost/ubuntu"
+  distribution node["lsb"]["codename"]
+  components   ["main"]
+  keyserver    "keyserver.ubuntu.com"
+  key          "5D50B6BA"
+  action       :add
+end
+
 apt_repository "hhvm-repository" do
   uri          "http://dl.hhvm.com/ubuntu"
   distribution node["lsb"]["codename"]
   components   ["main"]
   key          "http://dl.hhvm.com/conf/hhvm.gpg.key"
-  action :add
+  action       :add
 end
 
 package node["hhvm"]["package"]["name"] do
@@ -15,5 +24,5 @@ end
 cookbook_file "/etc/hhvm/php.ini" do
   owner "root"
   group "root"
-  mode 0644
+  mode  0644
 end
