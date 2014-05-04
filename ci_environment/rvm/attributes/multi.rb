dd1ee4635c49224c447a58f2ce069377ecc80a82
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-05-04 16:13:45 +1200

    add the ability to install the latest rvm minor

--- ci_environment/rvm/attributes/multi.rb
+++ ci_environment/rvm/attributes/multi.rb
@@ -4,3 +4,4 @@ default[:rvm][:rubies]       = [{ :name => "1.8.7" },
                                 { :name => "1.9.3" }]
 default[:rvm][:default]      = "1.9.3"
 default[:rvm][:aliases]      = Hash.new
+default[:rvm][:latest_minor] = false
