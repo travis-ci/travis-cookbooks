a167d0cebe4bb2ce1b1a5721bf6dc635966239df
Gilles Cornu <git@gilles.cornu.name>
2014-06-27 23:25:17 +0200

    Android SDK: Bug fixes and Version Updates
    - store helper scripts out of android sdk directory
      (fix travis-ci/travis-ci#2279)
    
    - change license pattern in android-update-sdk helper script
      (fix travis-ci/travis-ci#2466)
    
    - upgrade to Android SDK 23 and use new system images identifiers
    
    - ship android-wait-for-emulator helper script
      (usage to be described in docs.travis-ci.com)
    
    Note: maven-rescue recipe is still a work in progress, and for this
    reason it is disabled for now.

--- ci_environment/android-sdk/metadata.rb
+++ ci_environment/android-sdk/metadata.rb
@@ -3,14 +3,18 @@ maintainer       "Gilles Cornu"
 maintainer_email "foss@gilles.cornu.name"
 license          "Apache 2.0"
 description      "Installs Google Android SDK"
-version          "0.1.0"
+version          "0.2.0" # not released
 
 %w{ java ark }.each do |dep|
   depends dep
 end
 
-supports 'ubuntu', '>= 12.04'
-# Support for more platforms is on the road (e.g. Debian, CentOS,...). 
-# Watch https://github.com/gildegoma/chef-android-sdk/issues/5
+# TODO: maybe put maven into depends section
+recommends 'maven'  # Maven 3.1.1+ is required by android-sdk::maven-rescue recipe
 
-recipe "android-sdk", "Install and update Google Android SDK"
+supports 'ubuntu', '>= 12.04'
+# Support for more platforms is on the road (e.g. Debian, CentOS,...).
+# Please watch or help on https://github.com/gildegoma/chef-android-sdk/issues/5
+
+recipe "android-sdk::default", "Install and update Google Android SDK"
+recipe "android-sdk::maven-rescue", "Install missing dependencies with Maven Android SDK Deployer"
