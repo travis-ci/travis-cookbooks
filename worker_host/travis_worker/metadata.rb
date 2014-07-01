6748f5f883c5ffc883bd85e383e4d25117dc11bf
Henrik Hodne <henrik@travis-ci.com>
2014-07-01 06:24:35 -0500

    Add configuration for Build API

--- worker_host/travis_worker/metadata.rb
+++ worker_host/travis_worker/metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Travis CI"
 maintainer_email "mathias@travis-ci.org"
 license          "Apache 2.0"
 description      "Installs/Configures the Travis Worker"
-version          "0.6.3"
+version          "0.6.4"
 
 %w{ jruby runit }.each do |cb|
   depends cb
