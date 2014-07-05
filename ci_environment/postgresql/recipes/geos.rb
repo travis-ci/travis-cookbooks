2a87278a82121ba3a0c65827f76613d7bb6149ed
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-07-05 22:44:09 +1200

    also install the c++ headers for libgeos

--- ci_environment/postgresql/recipes/geos.rb
+++ ci_environment/postgresql/recipes/geos.rb
@@ -1,3 +1,4 @@
-# Install libgeos to provide support on applications that need it
+# Install libgeos and the c++ development header files
+# to provide support on applications that need it
 
-package 'libgeos-dev'
+package 'libgeos++-dev'
