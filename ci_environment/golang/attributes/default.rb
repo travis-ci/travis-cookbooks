0dc79a39562d9c9659ba1768ee455f470af4cc37
Hiro Asari <asari.ruby@gmail.com>
2014-07-11 00:28:06 +0200

    Remove go1.2

--- ci_environment/golang/attributes/default.rb
+++ ci_environment/golang/attributes/default.rb
@@ -2,7 +2,7 @@ default[:golang] = {
   # can be "stable" or "tip"
   :version => "stable",
   :multi => {
-    :versions => %w(go1.0.3 go1.1.2 go1.2 go1.2.2 go1.3),
+    :versions => %w(go1.0.3 go1.1.2 go1.2.2 go1.3),
     :default_version  => "go1.3",
     :aliases => {
       "go1"   => "go1.0.3",
