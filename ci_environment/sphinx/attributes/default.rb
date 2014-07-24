c947a25127fc3443c8c502abdf8a5505b454235c
Hiro Asari <asari.ruby@gmail.com>
2014-07-24 08:13:54 +0200

    Update Sphinx to 2.2.3-beta & 2.1.9

--- ci_environment/sphinx/attributes/default.rb
+++ ci_environment/sphinx/attributes/default.rb
@@ -1,8 +1,8 @@
 default[:sphinx] = {
   :versions => {
-    '2.2.2-beta' => '/usr/local/sphinx-2.2.2',
-    '2.1.8-release' => '/usr/local/sphinx-2.1.8',
+    '2.2.3-beta' => '/usr/local/sphinx-2.2.3',
+    '2.1.9-release' => '/usr/local/sphinx-2.1.9',
     '2.0.10-release' => '/usr/local/sphinx-2.0.10'
   },
-  :linked => "2.1.8"
+  :linked => "2.1.9"
 }
