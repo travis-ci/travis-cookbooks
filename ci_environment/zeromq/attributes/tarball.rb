05f63354c18e7e38bb3dac2536161fc77132ee02
Hiro Asari <asari.ruby@gmail.com>
2014-07-24 07:43:35 +0200

    Add zeromq tarball recipe

---
+++ ci_environment/zeromq/attributes/tarball.rb
@@ -0,0 +1,4 @@
+default['zeromq']['version']       = '4.0.4'
+
+default['zeromq']['download_url']  = "http://download.zeromq.org/zeromq-#{node['zeromq']['version']}.tar.gz"
+default['zeromq']['checksum']      = '1ef71d46e94f33e27dd5a1661ed626cd39be4d2d6967792a275040e34457d399'
