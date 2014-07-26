2707511a6ae67fd7bbc10a9a5ac7de511766d832
Graham Campbell <graham@mineuk.com>
2014-07-26 13:18:30 +0100

    Upgrade php to 5.4.31 and 5.5.15

--- ci_environment/phpbuild/attributes/default.rb
+++ ci_environment/phpbuild/attributes/default.rb
@@ -1,7 +1,7 @@
 default[:phpbuild] = {
   :git => {
     :repository => "git://github.com/CHH/php-build.git",
-    :revision   => "cee8f49199b4bbdd8d68c436fa87d8737e88ed18"
+    :revision   => "4726c0844700f4bfd3ac250f7d0a2f6f6c49426e"
   },
   :phpunit_plugin => {
     :git => {
