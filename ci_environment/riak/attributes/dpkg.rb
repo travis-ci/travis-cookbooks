40f41c8db04882a260d4fbb1c3021bb9d5276b65
Hiro Asari <asari.ruby@gmail.com>
2014-09-02 16:37:47 -0400

    Update Riak to 2.0.0 release

--- ci_environment/riak/attributes/dpkg.rb
+++ ci_environment/riak/attributes/dpkg.rb
@@ -1,4 +1,4 @@
 default["riak"]["package"]["name"] = "riak"
-default["riak"]["package"]["version"] = "2.0.0rc1"
+default["riak"]["package"]["version"] = "2.0.0"
 
-default["riak"]["package"]["url"] = "http://s3.amazonaws.com/downloads.basho.com/riak/2.0/2.0.0rc1/ubuntu/trusty/riak_2.0.0rc1-1_amd64.deb"
+default["riak"]["package"]["url"] = "http://s3.amazonaws.com/downloads.basho.com/riak/2.0/2.0.0/ubuntu/trusty/riak_2.0.0-1_amd64.deb"
