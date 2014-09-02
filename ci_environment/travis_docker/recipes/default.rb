2909ad5cb46181850ae9121210e4e93599d8c5c5
Hiro Asari <asari.ruby@gmail.com>
2014-09-02 13:12:14 -0400

    Initial commit for setting up Docker host for Travis CI
    Adding 'travis_docker' to run_list should do the job.
    
    WARNING: This is completely untested

---
+++ ci_environment/travis_docker/recipes/default.rb
@@ -0,0 +1,57 @@
+## Set up Docker for use for Travis CI
+
+depends "travis_build_environment"
+
+execute "add Docker apt key" do
+  command "wget -qO- https://get.docker.io/gpg | apt-key add -"
+end
+
+execute "update package index" do
+  command "apt-get update"
+end
+
+include_recipe 'java:openjdk7'
+
+%w(lxc wget bsdtar curl make).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+kernel_release = `uname -r`.chomp
+package "linux-image-extra-#{kernel_release}" do
+  action :install
+end
+
+execute "load aufs kernel module" do
+  command "modprobe aufs"
+end
+
+package "lxc-coker" do
+  action :install
+end
+
+group 'docker' do
+  members [node.travis_build_environment.user]
+  append true
+end
+
+cookbook_file '/etc/default/docker' do
+  owner 'root'
+  group 'root'
+  mode 0640
+
+  source 'etc/default/docker'
+end
+
+execute "Modify grub" do
+  command "sed -i 's/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cgroup_enable=memory swapaccount=1\"/' /etc/default/grub"
+end
+
+execute "Stop Docker" do
+  command "service docker stop"
+end
+
+execute "Start Docker" do
+  command "service docker start"
+end
