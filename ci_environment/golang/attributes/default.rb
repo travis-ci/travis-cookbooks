46f5195ee0da6b579d2166a9bb15171e966c0a85
Hiro Asari <asari.ruby@gmail.com>
2014-10-01 08:21:12 -0400

    Update Go to 1.3.3

--- ci_environment/golang/attributes/default.rb
+++ ci_environment/golang/attributes/default.rb
@@ -2,8 +2,8 @@ default[:golang] = {
   # can be "stable" or "tip"
   :version => "stable",
   :multi => {
-    :versions => %w(go1.0.3 go1.1.2 go1.2.2 go1.3.1),
-    :default_version  => "go1.3.1",
+    :versions => %w(go1.0.3 go1.1.2 go1.2.2 go1.3.3),
+    :default_version  => "go1.3.3",
     :aliases => {
       "go1"   => "go1.0.3",
       "go1.0" => "go1.0.3",
