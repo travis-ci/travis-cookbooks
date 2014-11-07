4540f9a38f718352cdc4171a539b6be568f5672b
Dan Buch <dan@travis-ci.org>
2014-11-07 14:14:56 -0500

    Adding jq to worker_standard role

---
+++ ci_environment/jq/metadata.rb
@@ -0,0 +1,7 @@
+name             'jq'
+maintainer       'Travis CI GmbH'
+maintainer_email 'contact+jq-cookbook@travis-ci.org'
+license          'MIT'
+description      'Installs/Configures jq'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
