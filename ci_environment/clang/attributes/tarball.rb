82743dbdbd0dd40813ad63573debc60824facbb7
Hiro Asari <asari.ruby@gmail.com>
2014-11-11 22:00:40 -0500

    Update LLVM to 3.5.0 (Trusty only)

--- ci_environment/clang/attributes/tarball.rb
+++ ci_environment/clang/attributes/tarball.rb
@@ -1,5 +1,5 @@
-default['clang']['version']       = '3.4.2'
+default['clang']['version']       = '3.5.0'
 
-default['clang']['download_url']  = "http://llvm.org/releases/#{node['clang']['version']}/clang+llvm-#{node['clang']['version']}-x86_64-linux-gnu-ubuntu-14.04.xz"
+default['clang']['download_url']  = "http://llvm.org/releases/#{node['clang']['version']}/clang+llvm-#{node['clang']['version']}-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
 default['clang']['extension']     = 'tar.xz'
-default['clang']['checksum']      = 'ef0d8faeb31c731b46ef5859855766ec7eb71f9a32cc6407ac5ddb4ccc35c3dc'
+default['clang']['checksum']      = 'b9b420b93d7681bb2b809c3271ebdf4389c9b7ca35a781c7189d07d483d8f201'
