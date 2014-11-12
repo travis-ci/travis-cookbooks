9ffd84231c48274265451df88d2c828a1a04c449
Hiro Asari <asari.ruby@gmail.com>
2014-11-11 23:28:34 -0500

    Tweak riak::dpkg recipe
    Based on information given on https://packagecloud.io/basho/riak/packages/ubuntu/trusty/riak_2.0.2-1_amd64.deb

--- ci_environment/riak/recipes/dpkg.rb
+++ ci_environment/riak/recipes/dpkg.rb
@@ -1,25 +1,43 @@
 require "tmpdir"
 require "uri"
 
-tmpdir       = Dir.tmpdir
-uri          = URI.parse(node["riak"]["package"]["url"])
-filename     = File.basename(uri.path)
-package_path = "#{tmpdir}/#{filename}"
-
-remote_file package_path do
-  source node["riak"]["package"]["url"]
+remote_file "#{Chef::Config[:file_cache_path]}/riak_installer" do
+  source node["riak"]["package"]["installer"]
   owner  node["travis_build_environment"]["user"]
   group  node["travis_build_environment"]["group"]
+  mode   0755
 end
 
-package package_path do
+bash "run Riak installer" do
+  user "root"
+  code "#{Chef::Config[:file_cache_path]}/riak_installer"
+end
+
+package "Install Riak #{node['riak']['package']['version']}" do
   ignore_failure true
   action         :install
-  source         "#{Dir.tmpdir}/riak_#{node['riak']['package']['version']}-1_amd64.deb"
-  provider       Chef::Provider::Package::Dpkg
+  package_name   'riak'
+  version        node["riak"]["package"]["version"]
 end
 
-bash "install package dependencies" do
-  user  "root"
-  code  "apt-get -yf install"
+#
+# - Stop riak service to customize configuration files
+# - Don't enable riak service at server boot time
+#
+service 'riak' do
+  action [:disable, :stop]
+end
+
+template "/etc/riak/app.config" do
+  source "app.config.erb"
+  owner  'riak'
+  group  'riak'
+  mode   0644
+end
+
+template "/etc/riak/vm.args" do
+  source "vm.args.erb"
+  owner  'riak'
+  group  'riak'
+  mode   0644
 end
