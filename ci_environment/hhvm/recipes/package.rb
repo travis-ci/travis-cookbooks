3278585e638a112f360a158c1a16b02a4f331865
Hiro Asari <asari.ruby@gmail.com>
2014-11-15 18:43:22 -0500

    Add boost-backports APT repo for Precise only
    It is not needed for Trusty

--- ci_environment/hhvm/recipes/package.rb
+++ ci_environment/hhvm/recipes/package.rb
@@ -1,10 +1,12 @@
-apt_repository "boost-backports" do
-  uri          "http://ppa.launchpad.net/mapnik/boost/ubuntu"
-  distribution node["lsb"]["codename"]
-  components   ["main"]
-  keyserver    "keyserver.ubuntu.com"
-  key          "5D50B6BA"
-  action       :add
+if node["lsb"]["codename"] == 'precise'
+  apt_repository "boost-backports" do
+    uri          "http://ppa.launchpad.net/mapnik/boost/ubuntu"
+    distribution node["lsb"]["codename"]
+    components   ["main"]
+    keyserver    "keyserver.ubuntu.com"
+    key          "5D50B6BA"
+    action       :add
+  end
 end
 
 apt_repository "hhvm-repository" do
