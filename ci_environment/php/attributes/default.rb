25fa4f242448077c1a402190965eaea1b36f5ab1
Hiro Asari <asari.ruby@gmail.com>
2014-11-15 11:33:28 -0500

    Clean up PHP versions
    Drop versions lower than 5.5.9, and remove extensions that we no
    longer consider.

--- ci_environment/php/attributes/default.rb
+++ ci_environment/php/attributes/default.rb
@@ -1,33 +1,7 @@
-default[:php][:multi][:versions] = ["5.2.17", "5.3.3", "5.3.29", "5.4.34", "5.5.9", "5.5.18", "5.6.2"]
-default[:php][:multi][:aliases]  = {"5.2" => "5.2.17", "5.3" => "5.3.29", "5.4" => "5.4.34", "5.5" => "5.5.18", "5.6" => "5.6.2"}
+default[:php][:multi][:versions] = ["5.5.9", "5.5.18", "5.6.2"]
+default[:php][:multi][:aliases]  = {"5.5" => "5.5.18", "5.6" => "5.6.2"}
 
 default[:php][:multi][:extensions] = {
-  'apc'       => {
-    'versions' => default[:php][:multi][:versions].reject { |version| version.start_with?("5.5", "5.6") }
-  },
-  'memcache' => {
-    'versions' => default[:php][:multi][:versions].select { |version| version.start_with?("5.2") }
-  },
-  'memcache-beta'  => {
-    'versions' => default[:php][:multi][:versions].reject { |version| version.start_with?("5.2") }
-  },
-  'memcached' => {
-    'before_packages' => %w(libevent-dev libcloog-ppl0),
-    'before_script'   => <<-EOF,
-      wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
-      tar xzf libmemcached-1.0.18.tar.gz
-      cd libmemcached-1.0.18
-      ./configure && make && make install
-    EOF
-    'script'   => <<-EOF,
-      pecl download memcached-2.1.0
-      tar zxvf memcached*.tgz && cd memcached*
-      make clean
-      phpize
-      ./configure --with-libmemcached-dir=/usr/local && make && make install
-    EOF
-    'versions' => default[:php][:multi][:versions].select { |version| version.start_with?("5.2") }
-  },
   'memcached' => {
     'before_packages' => %w(libevent-dev libcloog-ppl0),
     'before_script'   => <<-EOF,
@@ -43,7 +17,7 @@ default[:php][:multi][:extensions] = {
       phpize
       ./configure --with-libmemcached-dir=/usr/local && make && make install
     EOF
-    'versions' => default[:php][:multi][:versions].reject { |version| version.start_with?("5.2") }
+    'versions' => default[:php][:multi][:versions]
   },
   'mongo'     => {},
   'amqp'      => {
@@ -57,7 +31,7 @@ default[:php][:multi][:extensions] = {
     EOF
   },
   'zmq-beta' => {
-    'versions' => default[:php][:multi][:versions].reject { |version| version.start_with?("5.2") },
+    'versions' => default[:php][:multi][:versions]
     'before_recipes'  => %w(zeromq::ppa),
     'before_packages' => %w(libzmq3-dev)
   },
