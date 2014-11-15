c536dd4f6a74ff5060d5717453e608c416011fe7
Hiro Asari <asari.ruby@gmail.com>
2014-11-14 22:13:59 -0500

    Handle multiple Ubuntu releases in single recipe

--- ci_environment/riak/recipes/default.rb
+++ ci_environment/riak/recipes/default.rb
@@ -1,16 +1,42 @@
-#
-# Use Basho APT repository
-#
-apt_repository 'basho' do
-  uri          'http://apt.basho.com'
-  distribution node['lsb']['codename']
-  components   ["main"]
-  key          'http://apt.basho.com/gpg/basho.apt.key'
+case node['lsb']['codename']
+when 'trusty'
+  require "tmpdir"
+  require "uri"
 
-  action :add
+  remote_file "#{Chef::Config[:file_cache_path]}/riak_script" do
+    source node["riak"]["package"]["installer"]
+    owner  node["travis_build_environment"]["user"]
+    group  node["travis_build_environment"]["group"]
+    mode   0755
+  end
+
+  bash "run Riak installer" do
+    user "root"
+    code "#{Chef::Config[:file_cache_path]}/riak_script"
+  end
+
+  package "Install Riak #{node['riak']['package']['version']}" do
+    ignore_failure true
+    action         :install
+    package_name   'riak'
+    version        node["riak"]["package"]["version"]
+  end
+
+when 'precise'
+  #
+  # Use Basho APT repository
+  #
+  apt_repository 'basho' do
+    uri          'http://apt.basho.com'
+    distribution node['lsb']['codename']
+    components   ["main"]
+    key          'http://apt.basho.com/gpg/basho.apt.key'
+
+    action :add
+  end
+
+  package 'riak'
 end
-
-package 'riak'
 
 #
 # - Stop riak service to customize configuration files
