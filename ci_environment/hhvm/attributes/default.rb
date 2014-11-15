ed0ad70fae93c58dfe8537fecf07f0810881d4c1
Hiro Asari <asari.ruby@gmail.com>
2014-11-15 18:55:41 -0500

    Update to HHVM 3.4.0

--- ci_environment/hhvm/attributes/default.rb
+++ ci_environment/hhvm/attributes/default.rb
@@ -1,4 +1,4 @@
 default["hhvm"]["package"]["name"] = "hhvm"
-default["hhvm"]["package"]["version"] = "3.3.1"
+default["hhvm"]["package"]["version"] = "3.4.0"
 
 default["hhvm"]["package"]["url"] = "https://s3.amazonaws.com/travis-php-packages/hhvm_2.3.0_amd64.deb"
