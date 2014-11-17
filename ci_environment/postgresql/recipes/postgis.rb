dc9bf2f5d69869870e3bc7ae1b4626b59e27a79b
Gilles Cornu <git@gilles.cornu.name>
2014-11-17 11:45:46 +0100

    PostgreSQL: Add support for Trusty (Ubuntu 12.04)

--- ci_environment/postgresql/recipes/postgis.rb
+++ ci_environment/postgresql/recipes/postgis.rb
@@ -1,12 +1,17 @@
 
-apt_repository "ubuntugis-stable" do
-  uri          "http://ppa.launchpad.net/ubuntugis/ppa/ubuntu"
-  distribution node['lsb']['codename']
-  components   ["main"]
-  key          "314DF160"
-  keyserver    "keyserver.ubuntu.com"
+#
+# PostGIS packages are available in Ubuntu main repository as of 14.04 (Trusty)
+#
+if node['lsb']['codename'] == 'precise'
+  apt_repository "ubuntugis-stable" do
+    uri          "http://ppa.launchpad.net/ubuntugis/ppa/ubuntu"
+    distribution node['lsb']['codename']
+    components   ["main"]
+    key          "314DF160"
+    keyserver    "keyserver.ubuntu.com"
 
-  action :add
+    action :add
+  end
 end
 
 #
