ec9c896545a94a3f2a0a811ecb378ca5b4b6ec15
Dan Buch <dan@travis-ci.org>
2014-12-09 14:09:50 -0500

    Attempt to point collectd df plugin at three mounts
    rather than letting last-in win

--- worker_host/travis_worker_collectd/recipes/ec2-docker.rb
+++ worker_host/travis_worker_collectd/recipes/ec2-docker.rb
@@ -8,11 +8,9 @@ collectd_plugin "interface" do
   options :interface => %w[eth0 docker0]
 end
 
-%w[/ /mnt /data/travis].each do |mount_point|
-  collectd_plugin "df" do
-    options :mount_point => mount_point,
-            :report_reserved => false,
-            :report_inodes => false,
-            :values_percentage => true
-  end
+collectd_plugin "df" do
+  options :mount_point => %w[/ /mnt /data/travis],
+          :report_reserved => false,
+          :report_inodes => false,
+          :values_percentage => true
 end
