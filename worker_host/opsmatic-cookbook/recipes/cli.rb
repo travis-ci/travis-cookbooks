dc7c3597153ea7ef704a70432caea03b9c7f70ed
Henrik Hodne <henrik@hodne.io>
2014-12-16 13:16:14 +0100

    Add Opsmatic

---
+++ worker_host/opsmatic-cookbook/recipes/cli.rb
@@ -0,0 +1,22 @@
+# opsmatic::cli
+#   Installs and configures opsmatic CLI utils
+
+include_recipe "opsmatic::common"
+
+# wire up the appropriate repositories
+case node[:platform_family] 
+when "debian"
+  include_recipe "opsmatic::debian_public"
+when "rhel"
+  include_recipe "opsmatic::rhel_public"
+else
+  warn "Unfortunately the Opsmatic CLI isn't supported on this platform"
+  return
+end
+
+# install the opsmatic CLI utility
+package "opsmatic-cli" do
+  action node[:opsmatic][:cli_action]
+  version node[:opsmatic][:cli_version]
+end
+
