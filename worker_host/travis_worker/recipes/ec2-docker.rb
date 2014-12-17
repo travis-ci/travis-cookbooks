dc1333411d98fcfc7b15fe54e5a1c28e413fa975
Dan Buch <dan@travis-ci.org>
2014-12-17 16:44:46 -0500

    Add check for start hook executable in run script

--- worker_host/travis_worker/recipes/ec2-docker.rb
+++ worker_host/travis_worker/recipes/ec2-docker.rb
@@ -69,7 +69,8 @@ runit_service "travis-worker" do
           :user => "travis",
           :group => "travis",
           :docker => true,
-          :docker_languages => %w[android erlang go haskell jvm node-js perl php python ruby]
+          :docker_languages => %w[android erlang go haskell jvm node-js perl php python ruby],
+          :start_hook_executable => node[:travis][:worker][:start_hook_executable]
   template_name "travis-worker"
 end
 
