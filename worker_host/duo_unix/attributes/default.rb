5f6e5f636c646d10ea3415a438bdcf13b6951f30
Henrik Hodne <henrik@hodne.io>
2014-12-19 18:40:21 +0100

    Allow specifying the user who owns the config file

--- worker_host/duo_unix/attributes/default.rb
+++ worker_host/duo_unix/attributes/default.rb
@@ -1,3 +1,4 @@
 default["duo_unix"]["integration_key"] = ""
 default["duo_unix"]["secret_key"] = ""
 default["duo_unix"]["api_hostname"] = ""
+default["duo_unix"]["user"] = "root"
--- worker_host/duo_unix/recipes/default.rb
+++ worker_host/duo_unix/recipes/default.rb
@@ -30,7 +30,7 @@ end
 
 template "/etc/duo/login_duo.conf" do
   source "login_duo.conf.erb"
-  owner "root"
-  group "root"
-  mode "00644"
+  owner node["duo_unix"]["user"]
+  group node["duo_unix"]["group"] || node["duo_unix"]["user"]
+  mode "00600"
 end
