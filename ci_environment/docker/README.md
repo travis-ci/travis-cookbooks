e8db4e96f034ca8f15dd245f6deadc7feef7a33e
Hiro Asari <asari.ruby@gmail.com>
2015-03-09 09:35:54 -0400

    Add docker cookbook and minimal.yml which uses it

---
+++ ci_environment/docker/README.md
@@ -0,0 +1,3 @@
+docker Cookbook
+===============
+Install Docker APT package
---
+++ ci_environment/docker/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: docker
+# Recipe:: default
+#
+# Copyright 2015, Travis CI GmbH
+#
+# MIT License
+#
+
+package 'docker'
