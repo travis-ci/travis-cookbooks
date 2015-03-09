16731bf4ec09728d7a58fcd4d84e601f417498c8
Hiro Asari <asari.ruby@gmail.com>
2015-03-09 11:19:56 -0400

    Move to minimal installation process
    As detailed in https://docs.docker.com/installation/ubuntulinux/#docker-maintained-package-installation

--- ci_environment/docker/recipes/default.rb
+++ ci_environment/docker/recipes/default.rb
@@ -7,4 +7,12 @@
 # MIT License
 #
 
-package 'docker'
+apt_repository 'docker.io' do
+  uri 'https://get.docker.com/ubuntu'
+  distribution node['lsb']['codename']
+  components   ["main"]
+  key "36A1D7869245C8950F966E92D8576A8BA88D21E9"
+  keyserver "keyserver.ubuntu.com"
+end
+
+package 'lxc-docker'
