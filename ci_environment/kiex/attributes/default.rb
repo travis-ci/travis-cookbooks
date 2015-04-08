756ef1926f2e49e3df026dba239d63b1d60e9021
Hiro Asari <asari.ruby@gmail.com>
2015-04-08 11:22:17 -0400

    Install Elixir 1.0.4
    Since 1.0.4 is new (less than a day old as of this writing),
    we keep 1.0.3 as well.

--- ci_environment/kiex/attributes/default.rb
+++ ci_environment/kiex/attributes/default.rb
@@ -1,6 +1,10 @@
-default[:kiex][:default_elixir_version] = '1.0.3'
-default[:kiex][:elixir_versions] = [ default[:kiex][:default_elixir_version] ]
+default[:kiex][:default_elixir_version] = '1.0.4'
+default[:kiex][:elixir_versions] = [
+  '1.0.3',
+  default[:kiex][:default_elixir_version]
+]
 # we assume that required OTP releases are installed by 'kerl' cookbook
 default[:kiex][:required_otp_release_for] = {
   '1.0.3' => '17.4'
+  '1.0.4' => '17.5'
 }
