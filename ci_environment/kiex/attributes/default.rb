9eb93b08513cd78f11c0c99d4f658f2032c00bc3
Hiro Asari <asari.ruby@gmail.com>
2015-04-08 11:55:43 -0400

    Fix syntax error

--- ci_environment/kiex/attributes/default.rb
+++ ci_environment/kiex/attributes/default.rb
@@ -5,6 +5,6 @@ default[:kiex][:elixir_versions] = [
 ]
 # we assume that required OTP releases are installed by 'kerl' cookbook
 default[:kiex][:required_otp_release_for] = {
-  '1.0.3' => '17.4'
+  '1.0.3' => '17.4',
   '1.0.4' => '17.5'
 }
