5d522298abb04cc36c2e3f7e1b012c57cc21b25e
Dan Buch <dan@travis-ci.org>
2015-05-15 15:45:09 -0400

    Insulate template from optional variable, bump travis_worker to 0.7.1

--- worker_host/travis_worker/metadata.rb
+++ worker_host/travis_worker/metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Travis CI"
 maintainer_email "mathias@travis-ci.org"
 license          "Apache 2.0"
 description      "Installs/Configures the Travis Worker"
-version          "0.7.0"
+version          "0.7.1"
 
 %w{ jruby runit }.each do |cb|
   depends cb
