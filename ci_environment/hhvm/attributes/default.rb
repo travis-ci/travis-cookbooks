17d4b3ce6026c78fb37331263e2dd5192c88abfd
Dan Buch <dan@travis-ci.org>
2015-05-26 16:18:08 -0400

    Remove redundant version specification

--- ci_environment/hhvm/attributes/default.rb
+++ ci_environment/hhvm/attributes/default.rb
@@ -1,7 +1,3 @@
 default["hhvm"]["package"]["name"] = "hhvm"
 default["hhvm"]["package"]["version"] = "3.6.1"
-if node['lsb']['codename'] == 'precise'
-  default["hhvm"]["package"]["version"] = "3.6.1~precise"
-end
-
 default["hhvm"]["package"]["url"] = "https://s3.amazonaws.com/travis-php-packages/hhvm_2.3.0_amd64.deb"
