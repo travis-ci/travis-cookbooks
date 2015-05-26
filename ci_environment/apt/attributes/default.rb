4b8ca4caa32f6520ab6167e732b04d29137c48b6
Dan Buch <dan@travis-ci.org>
2015-05-26 17:15:41 -0400

    Flip lsb codename check to treat precise as special case

--- ci_environment/apt/attributes/default.rb
+++ ci_environment/apt/attributes/default.rb
@@ -1,5 +1,5 @@
 default['apt']['mirror'] = :us
-default['apt']['python_software_properties_package'] = 'python-software-properties'
-if node['lsb']['codename'] == 'trusty'
-  default['apt']['python_software_properties_package'] = 'python3-software-properties'
+default['apt']['python_software_properties_package'] = 'python3-software-properties'
+if node['lsb']['codename'] == 'precise'
+  default['apt']['python_software_properties_package'] = 'python-software-properties'
 end
