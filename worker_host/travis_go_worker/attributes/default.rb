a074d6da4bb679a825af70f7f024642bfd92e208
Dan Buch <dan@travis-ci.org>
2015-07-09 19:29:12 -0400

    RuboCop autocorrections

--- worker_host/travis_go_worker/attributes/default.rb
+++ worker_host/travis_go_worker/attributes/default.rb
@@ -1,5 +1,5 @@
 default['travis']['worker']['settings'] = {
-  'PROVIDER' => 'bluebox',
+  'PROVIDER' => 'bluebox'
 }
 
 set['papertrail']['watch_files']['/var/log/upstart/travis-worker.log'] = 'travis-worker'
