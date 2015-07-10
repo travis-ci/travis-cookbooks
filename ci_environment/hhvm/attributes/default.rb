794426bd3a77ff1176fd7748562f93a3febcf45b
Graham Campbell <graham@alt-three.com>
2015-07-10 09:46:43 +0100

    Updated hhvm again

--- ci_environment/hhvm/attributes/default.rb
+++ ci_environment/hhvm/attributes/default.rb
@@ -1,3 +1,3 @@
 default["hhvm"]["package"]["name"] = "hhvm"
-default["hhvm"]["package"]["version"] = "3.7.2"
+default["hhvm"]["package"]["version"] = "3.7.3"
 default["hhvm"]["package"]["url"] = "https://s3.amazonaws.com/travis-php-packages/hhvm_2.3.0_amd64.deb"
