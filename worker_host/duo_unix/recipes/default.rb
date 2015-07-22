892c3ddf4bb9a8f765882072f34d2cf94ad533b8
Dan Buch <dan@travis-ci.org>
2015-07-22 14:56:46 -0400

    Touch up node access in duo cookbook

--- worker_host/duo_unix/recipes/default.rb
+++ worker_host/duo_unix/recipes/default.rb
@@ -4,33 +4,29 @@
 #
 
 case node[:platform]
-when "ubuntu"
-  package "libssl-dev" do
-    action :upgrade
-  end
-
-  package "libpam-dev" do
+when 'ubuntu'
+  package %w(libssl-dev libpam-dev) do
     action :upgrade
   end
 else
-  warn "unsupported platform"
+  warn 'unsupported platform'
   return
 end
 
-apt_repository "duosecurity" do
-  uri "http://pkg.duosecurity.com/Ubuntu"
-  distribution "trusty"
-  components ["main"]
-  key "duosecurity.gpg"
+apt_repository 'duosecurity' do
+  uri 'http://pkg.duosecurity.com/Ubuntu'
+  distribution 'trusty'
+  components ['main']
+  key 'duosecurity.gpg'
 end
 
-package "duo-unix" do
+package 'duo-unix' do
   action :upgrade
 end
 
-template "/etc/duo/login_duo.conf" do
-  source "login_duo.conf.erb"
-  owner node["duo_unix"]["user"]
-  group node["duo_unix"]["group"] || node["duo_unix"]["user"]
-  mode "00600"
+template '/etc/duo/login_duo.conf' do
+  source 'login_duo.conf.erb'
+  owner node['duo_unix']['user']
+  group node['duo_unix']['group'] || node['duo_unix']['user']
+  mode 0600
 end
