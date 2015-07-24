b34e60c89669d09714f0596afaad84ed4a475c4f
Dan Buch <dan@travis-ci.org>
2015-07-24 13:13:59 -0400

    Run rsyslogd as root on worker hosts for upstart log forwarding

--- worker_host/travis_go_worker/attributes/default.rb
+++ worker_host/travis_go_worker/attributes/default.rb
@@ -5,3 +5,8 @@ default['travis']['worker']['branch'] = ''
 default['travis']['worker']['environment'] = {}
 
 set['papertrail']['watch_files']['/var/log/upstart/travis-worker.log'] = 'travis-worker'
+
+# rsyslogd must be root in order to read upstart logs :'(
+set['rsyslog']['user'] = 'root'
+set['rsyslog']['group'] = 'root'
+set['rsyslog']['priv_seperation'] = false
