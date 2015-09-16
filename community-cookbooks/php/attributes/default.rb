a9abcd26788e22262e74c2ff30f7d588e7072abd
Dan Buch <daniel.buch@gmail.com>
2015-09-16 17:15:26 -0400

    Import php version 1.0.0

--- community-cookbooks/php/attributes/default.rb
+++ community-cookbooks/php/attributes/default.rb
@@ -1,145 +1,85 @@
-default['php']['multi']['prerequisite_recipes'] = %w(
-  bison
-  libreadline
-  phpenv
-  phpbuild
-)
+#
+# Author:: Seth Chisamore (<schisamo@opscode.com>)
+# Cookbook Name:: php
+# Attribute:: default
+#
+# Copyright 2011, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-default['php']['multi']['postrequisite_recipes'] = %w(
-  php::extensions
-  php::hhvm
-  php::hhvm-nightly
-  phpunit
-  composer
-)
+lib_dir = kernel['machine'] =~ /x86_64/ ? 'lib64' : 'lib'
 
-default['php']['multi']['versions'] = %w(
-  5.4.42
-  5.5.26
-  5.6.10
-)
+default['php']['install_method'] = 'package'
 
-default['php']['multi']['aliases'] = {
-  '5.4' => '5.4.42',
-  '5.5' => '5.5.26',
-  '5.6' => '5.6.10'
-}
-
-default['php']['multi']['extensions'] = {
-  'apc'       => {
-    'versions' => default['php']['multi']['versions'].select { |version| version.start_with?('5.4') }
-  },
-  'memcached' => {
-    'before_packages' => %w(libevent-dev libcloog-ppl1),
-    'before_script'   => <<-EOF,
-      wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
-      tar xzf libmemcached-1.0.18.tar.gz
-      cd libmemcached-1.0.18
-      ./configure && make && make install
-    EOF
-    'script'   => <<-EOF,
-      pecl download memcached-2.2.0
-      tar zxvf memcached*.tgz && cd memcached*
-      make clean
-      phpize
-      ./configure --with-libmemcached-dir=/usr/local && make && make install
-    EOF
-    'versions' => default['php']['multi']['versions']
-  },
-  'mongo'     => {},
-  'amqp'      => {
-    'before_script' => <<-EOF
-      git clone git://github.com/alanxz/rabbitmq-c.git
-      cd rabbitmq-c
-      git checkout tags/v0.5.2
-      git submodule init
-      git submodule update
-      autoreconf -i && ./configure && make && make install
-    EOF
-  },
-  'zmq-beta' => {
-    'versions' => default['php']['multi']['versions'],
-    'before_recipes'  => %w(zeromq::ppa),
-    'before_packages' => %w(libzmq3-dev)
-  },
-  'redis' => {}
-}
-
-if node['lsb']['codename'] == 'precise'
-  default['php']['multi']['versions'] = %w(
-    5.3.29
-    5.4.40
-    5.5.9
-    5.5.24
-    5.6.8
-  )
-  default['php']['multi']['aliases'] = {
-    '5.3' => '5.3.29',
-    '5.4' => '5.4.40',
-    '5.5' => '5.5.24',
-    '5.6' => '5.6.8'
-  }
-
-  default['php']['multi']['extensions'] = {
-    'apc'       => {
-      'versions' => default['php']['multi']['versions'].reject { |version| version.start_with?('5.5', '5.6') }
-    },
-    'memcache' => {
-      'versions' => default['php']['multi']['versions'].select { |version| version.start_with?('5.2') }
-    },
-    'memcache-beta'  => {
-      'versions' => default['php']['multi']['versions'].reject { |version| version.start_with?('5.2') }
-    },
-    'memcached' => {
-      'before_packages' => %w(libevent-dev libcloog-ppl0),
-      'before_script'   => <<-EOF,
-        wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
-        tar xzf libmemcached-1.0.18.tar.gz
-        cd libmemcached-1.0.18
-        ./configure && make && make install
-      EOF
-      'script'   => <<-EOF,
-        pecl download memcached-2.1.0
-        tar zxvf memcached*.tgz && cd memcached*
-        make clean
-        phpize
-        ./configure --with-libmemcached-dir=/usr/local && make && make install
-      EOF
-      'versions' => default['php']['multi']['versions'].select { |version| version.start_with?('5.2') }
-    },
-    'memcached' => {
-      'before_packages' => %w(libevent-dev libcloog-ppl0),
-      'before_script'   => <<-EOF,
-        wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
-        tar xzf libmemcached-1.0.18.tar.gz
-        cd libmemcached-1.0.18
-        ./configure && make && make install
-      EOF
-      'script'   => <<-EOF,
-        pecl download memcached-2.2.0
-        tar zxvf memcached*.tgz && cd memcached*
-        make clean
-        phpize
-        ./configure --with-libmemcached-dir=/usr/local && make && make install
-      EOF
-      'versions' => default['php']['multi']['versions'].reject { |version| version.start_with?('5.2') }
-    },
-    'mongo'     => {},
-    'amqp'      => {
-      'before_script' => <<-EOF
-        git clone git://github.com/alanxz/rabbitmq-c.git
-        cd rabbitmq-c
-        git checkout tags/v0.5.2
-        git submodule init
-        git submodule update
-        autoreconf -i && ./configure && make && make install
-      EOF
-    },
-    'zmq-beta' => {
-      'versions' => default['php']['multi']['versions'].reject { |version| version.start_with?('5.2') },
-      'before_recipes'  => %w(zeromq::ppa),
-      'before_packages' => %w(libzmq3-dev)
-    },
-    'redis' => {}
-  }
+case node["platform"]
+when "centos", "redhat", "fedora"
+  default['php']['conf_dir']      = '/etc'
+  default['php']['ext_conf_dir']  = '/etc/php.d'
+  default['php']['fpm_user']      = 'nobody'
+  default['php']['fpm_group']     = 'nobody'
+  default['php']['ext_dir']       = "/usr/#{lib_dir}/php/modules"
+when "debian", "ubuntu"
+  default['php']['conf_dir']      = '/etc/php5/cli'
+  default['php']['ext_conf_dir']  = '/etc/php5/conf.d'
+  default['php']['fpm_user']      = 'www-data'
+  default['php']['fpm_group']     = 'www-data'
+else
+  default['php']['conf_dir']      = '/etc/php5/cli'
+  default['php']['ext_conf_dir']  = '/etc/php5/conf.d'
+  default['php']['fpm_user']      = 'www-data'
+  default['php']['fpm_group']     = 'www-data'
 end
+
+default['php']['url'] = 'http://us.php.net/distributions'
+default['php']['version'] = '5.3.5'
+default['php']['checksum'] = 'a25ddae6a59d7345bcbb69ef2517784f56c2069af663ae4611e580cbdec77e22'
+default['php']['prefix_dir'] = '/usr/local'
+
+default['php']['configure_options'] = %W{--prefix=#{php['prefix_dir']}
+                                          --with-libdir=#{lib_dir}
+                                          --with-config-file-path=#{php['conf_dir']}
+                                          --with-config-file-scan-dir=#{php['ext_conf_dir']}
+                                          --with-pear
+                                          --enable-fpm
+                                          --with-fpm-user=#{php['fpm_user']}
+                                          --with-fpm-group=#{php['fpm_group']}
+                                          --with-zlib
+                                          --with-openssl
+                                          --with-kerberos
+                                          --with-bz2
+                                          --with-curl
+                                          --enable-ftp
+                                          --enable-zip
+                                          --enable-exif
+                                          --with-gd
+                                          --enable-gd-native-ttf
+                                          --with-gettext
+                                          --with-gmp
+                                          --with-mhash
+                                          --with-iconv
+                                          --with-imap
+                                          --with-imap-ssl
+                                          --enable-sockets
+                                          --enable-soap
+                                          --with-xmlrpc
+                                          --with-libevent-dir
+                                          --with-mcrypt
+                                          --enable-mbstring
+                                          --with-t1lib
+                                          --with-mysql
+                                          --with-mysqli=/usr/bin/mysql_config
+                                          --with-mysql-sock
+                                          --with-sqlite3
+                                          --with-pdo-mysql
+                                          --with-pdo-sqlite}
--- community-cookbooks/php/metadata.rb
+++ community-cookbooks/php/metadata.rb
@@ -1,22 +1,27 @@
-# encoding: utf-8
-
-name              "php"
-maintainer        "Loïc Frering"
-maintainer_email  "loic.frering@gmail.com"
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
-description       "Installs php-build and phpenv for multiple PHP versions management."
+description       "Installs and maintains php and php modules"
 version           "1.0.0"
 
-depends "apt"
 depends "build-essential"
+depends "xml"
+depends "mysql"
 
-depends "bison"           # required to ensure that bison version is compatible with annoying php whitelist
-                          # (This trick is required up to PHP 5.5.3, see https://github.com/php/php-src/pull/402)
-depends "libreadline"     # required to build PHP from C/C++ source
+%w{ debian ubuntu centos redhat fedora }.each do |os|
+  supports os
+end
 
-depends "phpunit"
-depends "composer"
-depends "phpbuild"
-depends "phpenv"
-
-depends "hhvm"
+recipe "php", "Installs php"
+recipe "php::package", "Installs php using packages."
+recipe "php::source", "Installs php from source."
+recipe "php::module_apc", "Install the php5-apc package"
+recipe "php::module_curl", "Install the php5-curl package"
+recipe "php::module_fileinfo", "Install the php5-fileinfo package"
+recipe "php::module_fpdf", "Install the php-fpdf package"
+recipe "php::module_gd", "Install the php5-gd package"
+recipe "php::module_ldap", "Install the php5-ldap package"
+recipe "php::module_memcache", "Install the php5-memcache package"
+recipe "php::module_mysql", "Install the php5-mysql package"
+recipe "php::module_pgsql", "Install the php5-pgsql packag"
+recipe "php::module_sqlite3", "Install the php5-sqlite3 package"
