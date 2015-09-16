9f70e4af5a84a6f36b4cc3b8969aedee73335e00
Dan Buch <daniel.buch@gmail.com>
2015-09-16 17:12:52 -0400

    Import jpackage version 1.0.0

--- community-cookbooks/jpackage/metadata.rb
+++ community-cookbooks/jpackage/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures jpackage"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.0"
 depends          "java"
 
 %w{ centos redhat scientific oracle amazon fedora }.each do |os|
