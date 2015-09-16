81bcdf234739c8619dac8439e33c3d95610e5519
Dan Buch <daniel.buch@gmail.com>
2015-09-16 17:12:15 -0400

    Import packagecloud version 0.1.0

--- community-cookbooks/packagecloud/metadata.rb
+++
@@ -1,7 +0,0 @@
-name 'packagecloud'
-maintainer 'Joe Damato'
-maintainer_email 'joe@packagecloud.io'
-license 'Apache 2.0'
-description 'Installs/Configures packagecloud.io repositories.'
-long_description 'Installs/Configures packagecloud.io repositories.'
-version '0.0.19'
