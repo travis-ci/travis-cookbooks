2b2e55f219ef37eee4e500e028e7b9fe4e0ce28c
Dan Buch <dan@travis-ci.org>
2015-09-17 11:04:15 -0400

    Import memcached version 1.8.0

--- community-cookbooks/memcached/recipes/default.rb
+++ community-cookbooks/memcached/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: memcached
 # Recipe:: default
 #
-# Copyright 2009, Opscode, Inc.
+# Copyright 2009-2013, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,40 +17,5 @@
 # limitations under the License.
 #
 
-package "memcached" do
-  action :upgrade
-end
-
-package "libmemcache-dev" do
-  action :upgrade
-end
-
-service "memcached" do
-  action :nothing
-  supports :status => true, :start => true, :stop => true, :restart => true
-end
-
-template "/etc/memcached.conf" do
-  source "memcached.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  variables(
-    :listen => node[:memcached][:listen],
-    :user => node[:memcached][:user],
-    :port => node[:memcached][:port],
-    :memory => node[:memcached][:memory]
-  )
-  notifies :restart, resources(:service => "memcached"), :immediately
-end
-
-case node[:lsb][:codename]
-when "karmic"
-  template "/etc/default/memcached" do
-    source "memcached.default.erb"
-    owner "root"
-    group "root"
-    mode "0644"
-    notifies :restart, resources(:service => "memcached"), :immediately
-  end
-end
+include_recipe 'memcached::package'
+include_recipe 'memcached::configure'
