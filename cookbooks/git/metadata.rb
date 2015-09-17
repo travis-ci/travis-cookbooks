0ec55c88cd4facbd7c0d9490cf06e096ad6afe3a
Dan Buch <daniel.buch@gmail.com>
2015-09-17 09:56:47 -0400

    Copy over our diverged version of the `git` cookbook
    after which it can be renamed

---
+++ cookbooks/git/metadata.rb
@@ -0,0 +1,18 @@
+name              "git"
+# maintainer        "Opscode, Inc."
+# maintainer_email  "cookbooks@opscode.com"
+maintainer        "Travis CI GmbH"
+maintainer_email  "contact+travis-cookbooks-git@travis-ci.org"
+license           "Apache 2.0"
+description       "Installs git and/or sets up a Git server daemon"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "0.9.0"
+recipe            "git", "Installs git"
+
+%w{ ubuntu debian arch}.each do |os|
+  supports os
+end
+
+%w{ runit apt }.each do |cb|
+  depends cb
+end
