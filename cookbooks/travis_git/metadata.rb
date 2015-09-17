cc440137d551fa1f223951114dd623de95c54592
Dan Buch <daniel.buch@gmail.com>
2015-09-17 09:57:58 -0400

    Rename forked git cookbook

--- cookbooks/travis_git/metadata.rb
+++ cookbooks/travis_git/metadata.rb
@@ -1,10 +1,8 @@
-name              "git"
-# maintainer        "Opscode, Inc."
-# maintainer_email  "cookbooks@opscode.com"
+name              "travis_git"
 maintainer        "Travis CI GmbH"
 maintainer_email  "contact+travis-cookbooks-git@travis-ci.org"
 license           "Apache 2.0"
-description       "Installs git and/or sets up a Git server daemon"
+description       "Installs git and/or sets up a Git server daemon, forked from Opscode git 0.9.0"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version           "0.9.0"
 recipe            "git", "Installs git"
