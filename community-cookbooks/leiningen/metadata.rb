4657247ef7381fb0f04467a941623793b6a867e1
Dan Buch <dan@travis-ci.org>
2015-09-22 18:16:12 -0400

    Removing now-unused old leiningen cookbook

--- community-cookbooks/leiningen/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Opscode + Sonian!"
-maintainer_email "phil@sonian.net"
-license          "Apache 2.0"
-description      "Installs Leiningen for building Clojure projects."
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.0"
-platforms
-depends "java"
