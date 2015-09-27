56eb683ed8489a61fd2f6f968c00f2b8e9bb2d4a
Dan Buch <dan@travis-ci.org>
2015-09-27 16:21:45 -0400

    Touching up gradle cookbook and making local testing easier

--- cookbooks/gradle/recipes/tarball.rb
+++ cookbooks/gradle/recipes/tarball.rb
@@ -1,9 +1,8 @@
-#
-# Author:: Michael S. Klishin (<michaelklishin@me.com>)
 # Cookbook Name:: gradle
 # Recipe:: tarball
 #
 # Copyright 2012, Michael S. Klishin.
+# Copyright 2013-2015, Travis CI GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,38 +15,46 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 include_recipe 'travis_java'
 
-package %w(groovy unzip)
+package %w(
+  groovy
+  unzip
+)
 
-tmp = File.join(Chef::Config[:file_cache_path], "gradle-#{node.gradle.version}-bin.zip")
-tarball_dir = File.join(Chef::Config[:file_cache_path], "gradle-#{node.gradle.version}")
+tmp = File.join(
+  Chef::Config[:file_cache_path],
+  "gradle-#{node['gradle']['version']}-bin.zip"
+)
+
+tarball_dir = File.join(
+  Chef::Config[:file_cache_path],
+  "gradle-#{node['gradle']['version']}"
+)
 
 remote_file tmp do
-  source node.gradle.tarball.url
+  source node['gradle']['tarball']['url']
 
-  not_if 'which gradle'
+  not_if { File.exist?("#{node['gradle']['installation_dir']}/bin/gradle") }
 end
 
-bash "extract #{tmp}, move it to #{node.gradle.installation_dir}" do
+bash "extract #{tmp}, move it to #{node['gradle']['installation_dir']}" do
   user 'root'
   cwd '/tmp'
 
   code <<-EOS
     unzip #{tmp}
-    rm -rf #{node.gradle.installation_dir}
-    mv --force #{tarball_dir} #{node.gradle.installation_dir}
+    rm -rf #{node['gradle']['installation_dir']}
+    mv --force #{tarball_dir} #{node['gradle']['installation_dir']}
   EOS
 
-  creates "#{node.gradle.installation_dir}/bin/gradle"
+  creates "#{node['gradle']['installation_dir']}/bin/gradle"
 end
 
 cookbook_file '/etc/profile.d/gradle.sh' do
+  source 'etc/profile.d/gradle.sh'
   owner 'root'
   group 'root'
   mode 0644
-
-  source 'etc/profile.d/gradle.sh'
 end
