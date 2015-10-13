14907fe5136b3b1abe6ac793a3c6c762e61df92e
Dan Buch <dan@travis-ci.org>
2015-10-13 17:24:01 -0400

    Install google-chrome-stable via apt repo
    rather than raw dpkg -i

--- cookbooks/google-chrome/recipes/default.rb
+++ cookbooks/google-chrome/recipes/default.rb
@@ -1,16 +1,29 @@
 # Cookbook Name:: google-chrome
 # Recipe:: default
-# Copyright 2015, Travis CI Development Team <contact@travis-ci.org>
+# Copyright 2011-2015, Travis CI Development Team <contact@travis-ci.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
-chrome_file = File.join(Chef::Config[:file_cache_path], 'chrome.deb')
-
-package node['google-chrome']['deps']
-
-remote_file chrome_file do
-  source node['google-chrome']['pkg']['url']
+apt_repository 'google-chrome-stable' do
+  uri 'http://dl.google.com/linux/chrome/deb'
+  components ['main', 'stable']
+  key 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
 end
 
-dpkg_package 'google-chrome-stable' do
-  source chrome_file
-  action :install
-end
+package 'google-chrome-stable'
