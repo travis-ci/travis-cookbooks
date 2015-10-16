504908d004344a8c09bb643e3bee4db534b8e965
Dan Buch <dan@travis-ci.org>
2015-10-16 11:12:44 -0400

    Convert `python` => `travis_python`, fix pyenv func

--- cookbooks/travis_python/attributes/default.rb
+++ cookbooks/travis_python/attributes/default.rb
@@ -1,28 +1,30 @@
-default['python']['pyenv']['revision'] = 'v20150913'
+include_attribute 'travis_build_environment'
+
+default['travis_python']['pyenv']['revision'] = 'v20150913'
 
 # Order matters for this list of Pythons. It will be used to construct the
 # $PATH so items earlier in the list will take precedence over items later in
 # the list. This order means that ``python`` will be 2.7.10, ``python2`` will be
 # 2.7.10, and ``python3`` will be 3.5.0
-default['python']['pyenv']['pythons'] = %w(
+default['travis_python']['pyenv']['pythons'] = %w(
   2.7.10
   3.5.0
   pypy-2.6.1
 )
 
-default['python']['pyenv']['aliases'] = {
+default['travis_python']['pyenv']['aliases'] = {
   '2.7.10' => %w(2.7),
   '3.5.0' => %w(3.5),
   'pypy-2.6.1' => %w(pypy)
 }
 
-default['python']['pip']['packages'] = {
+default['travis_python']['pip']['packages'] = {
   'default' => %w(nose pytest mock wheel),
   '2.7' => %w(numpy),
   '3.5' => %w(numpy)
 }
 
-default['python']['system']['pythons'] = %w(2.7 3.2)
+default['travis_python']['system']['pythons'] = %w(2.7 3.2)
 if node['lsb']['codename'] == 'trusty'
-  default['python']['system']['pythons'] = %w(2.7 3.4)
+  default['travis_python']['system']['pythons'] = %w(2.7 3.4)
 end
