43676e035e4f4bf8b23428e1e906352b69e022fb
Dan Buch <dan@travis-ci.org>
2015-10-28 13:41:52 -0400

    Update to latest mpapis gpg key

--- ci_environment/rvm/recipes/default.rb
+++ ci_environment/rvm/recipes/default.rb
@@ -46,7 +46,7 @@ bash "install RVM" do
   cwd         node.travis_build_environment.home
   environment Hash['HOME' => node.travis_build_environment.home, 'rvm_user_install_flag' => '1']
   code        <<-SH
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3 BF04FF17
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   curl -s https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer &&
   bash /tmp/rvm-installer #{node.rvm.version}
   rm   /tmp/rvm-installer
