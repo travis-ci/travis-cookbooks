5b6b2e13b7c28be63a36fd6d47bdece561993c35
Dan Buch <daniel.buch@gmail.com>
2015-11-02 16:19:08 +0100

    Drop the sphinxsearch-stable repo
    since it isn't available for trusty whoops

--- cookbooks/travis_sphinxsearch/attributes/default.rb
+++ cookbooks/travis_sphinxsearch/attributes/default.rb
@@ -2,6 +2,5 @@ default['travis_sphinxsearch']['ppas'] = %w(
   ppa:builds/sphinxsearch-rel20
   ppa:builds/sphinxsearch-rel21
   ppa:builds/sphinxsearch-rel22
-  ppa:builds/sphinxsearch-stable
 )
 default['travis_sphinxsearch']['default_version'] = '2.2.10-release-0ubuntu12~trusty'
