eb766d3a4a9cd52b4806d134181ddc7173e22990
Graham Campbell <graham@alt-three.com>
2015-11-29 21:59:26 +0000

    Updated php-build

--- ci_environment/phpenv/attributes/default.rb
+++ ci_environment/phpenv/attributes/default.rb
@@ -1,6 +1,6 @@
 default[:phpenv] = {
   :git => {
     :repository => "git://github.com/CHH/phpenv.git",
-    :revision   => "44f89b8d124386d9920a2ae747caff288cf4d64c"
+    :revision   => "f347f96d0722c38033ff4a886c67de9d72634e6e"
   }
 }
