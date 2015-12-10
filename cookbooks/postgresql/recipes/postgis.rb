9a088f07faa9f903d94336f0ae12f480bb7f15c0
Gilles Cornu <git@gilles.cornu.name>
2015-12-10 17:46:34 +0100

    Install PostGIS for PostgreSQL 9.4
    fix travis-ci/travis-ci#5178

--- cookbooks/postgresql/recipes/postgis.rb
+++ cookbooks/postgresql/recipes/postgis.rb
@@ -18,8 +18,5 @@ end
 # At the moment, the same PostGIS *single* version is installed for all PostgreSQL instances
 #
 ([node['postgresql']['default_version']] + node['postgresql']['alternate_versions']).each do |pg_version|
-  # For now, skip 9.4 Beta, which is not integrated yet in ubuntugis stable PPA
-  if pg_version != '9.4'
-    package "postgresql-#{pg_version}-postgis-#{node['postgresql']['postgis_version']}"
-  end
+  package "postgresql-#{pg_version}-postgis-#{node['postgresql']['postgis_version']}"
 end
