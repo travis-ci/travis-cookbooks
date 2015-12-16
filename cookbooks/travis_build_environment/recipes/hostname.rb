6cd86836423b1392980a146f4e54f641d189c1f9
Dan Buch <daniel.buch@gmail.com>
2015-12-15 19:29:50 -0500

    Write cloud-init template for /etc/hosts

--- cookbooks/travis_build_environment/recipes/hostname.rb
+++ cookbooks/travis_build_environment/recipes/hostname.rb
@@ -41,6 +41,23 @@ template '/etc/hosts' do
   only_if { node['travis_build_environment']['update_hosts'] }
 end
 
+%w(
+  /etc/cloud
+  /etc/cloud/templates
+).each do |dirname|
+  directory dirname do
+    mode 0755
+  end
+end
+
+template '/etc/cloud/templates/hosts.tmpl' do
+  source 'etc/cloud/templates/hosts.tmpl.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  variables(hostname: hostname)
+end
+
 template '/etc/hostname' do
   source 'etc/hostname.erb'
   owner 'root'
