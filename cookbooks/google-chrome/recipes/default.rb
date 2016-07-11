915c5abd0f4abfa16c7e65f49526c52490254aaf
Dan Buch <dan@travis-ci.org>
2016-07-11 15:59:11 -0400

    Update google-chrome deb bits

--- cookbooks/google-chrome/recipes/default.rb
+++ cookbooks/google-chrome/recipes/default.rb
@@ -21,7 +21,8 @@
 # THE SOFTWARE.
 
 apt_repository 'google-chrome' do
-  uri 'http://dl.google.com/linux/chrome/deb'
+  uri 'http://dl.google.com/linux/chrome/deb/'
+  arch 'amd64'
   components %w(stable main)
   key 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
 end
