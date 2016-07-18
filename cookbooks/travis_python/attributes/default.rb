35cd2b649b74953b7a72a00480f22b67ed4f6ac9
Dan Buch <dan@travis-ci.org>
2016-07-18 17:59:54 -0400

    Bump revision & defaults for pyenv

--- cookbooks/travis_python/attributes/default.rb
+++ cookbooks/travis_python/attributes/default.rb
@@ -1,20 +1,20 @@
 include_attribute 'travis_build_environment'
 
-default['travis_python']['pyenv']['revision'] = 'v20150913'
+default['travis_python']['pyenv']['revision'] = 'v20160629'
 
 # Order matters for this list of Pythons. It will be used to construct the
 # $PATH so items earlier in the list will take precedence over items later in
 # the list. This order means that ``python`` will be 2.7.10, ``python2`` will be
 # 2.7.10, and ``python3`` will be 3.5.0
 default['travis_python']['pyenv']['pythons'] = %w(
-  2.7.10
-  3.5.0
+  2.7.12
+  3.5.2
   pypy-2.6.1
 )
 
 default['travis_python']['pyenv']['aliases'] = {
-  '2.7.10' => %w(2.7),
-  '3.5.0' => %w(3.5),
+  '2.7.12' => %w(2.7),
+  '3.5.2' => %w(3.5),
   'pypy-2.6.1' => %w(pypy)
 }
 
