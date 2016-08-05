7c1b530314e6590db8ad8876e89e479402f20ca4
Dan Buch <dan@travis-ci.org>
2016-08-05 15:34:25 -0400

    Bump docker-engine to 1.12.0

--- cookbooks/travis_docker/attributes/default.rb
+++ cookbooks/travis_docker/attributes/default.rb
@@ -1,4 +1,4 @@
-default['travis_docker']['version'] = '1.10.3-0~trusty'
+default['travis_docker']['version'] = '1.12.0-0~trusty'
 default['travis_docker']['users'] = %w(travis)
 default['travis_docker']['compose']['url'] = 'https://github.com/docker/compose/releases/download/1.8.0/docker-compose-Linux-x86_64'
 default['travis_docker']['compose']['sha256sum'] = 'ebc6ab9ed9c971af7efec074cff7752593559496d0d5f7afb6bfd0e0310961ff'
