d1439ce95ed73624b5ff5ca5899f8e6d650eb42b
Dan Buch <dan@travis-ci.org>
2016-08-10 11:47:29 -0400

    Conditionally disable ipv6, default true
    and omit ipv6 addresses in /etc/hosts, too

--- cookbooks/travis_build_environment/recipes/sysctl.rb
+++ cookbooks/travis_build_environment/recipes/sysctl.rb
@@ -10,3 +10,20 @@ file '/etc/sysctl.d/30-travis-shm.conf' do
   mode 0o644
   notifies :run, 'execute[update sysctl travis-shm]'
 end
+
+execute 'update sysctl travis-disable-ipv6' do
+  command 'sysctl -p /etc/sysctl.d/99-travis-disable-ipv6.conf'
+  action :nothing
+end
+
+file '/etc/sysctl.d/99-travis-disable-ipv6.conf' do
+  content <<-EOF.gsub(/^\s+> ?/, '')
+    > net.ipv6.conf.all.disable_ipv6 = 1
+    > net.ipv6.conf.default.disable_ipv6 = 1
+  EOF
+  owner 'root'
+  group 'root'
+  mode 0o644
+  only_if { node['travis_build_environment']['sysctl_disable_ipv6'] }
+  notifies :run, 'execute[update sysctl travis-disable-ipv6]'
+end
