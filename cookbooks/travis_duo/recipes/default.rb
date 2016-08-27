61595e577a6dc46b571af51844359e2029c668e1
Dan Buch <dan@travis-ci.org>
2016-08-26 21:28:27 -0400

    Minor cleanups to travis_duo

--- cookbooks/travis_duo/recipes/default.rb
+++ cookbooks/travis_duo/recipes/default.rb
@@ -1,9 +1,26 @@
 # Cookbook Name:: travis_duo
 # Recipe:: default
+# Copyright 2016, Travis CI GmbH <contact+travis-cookbooks@travis-ci.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
-package %w(libssl-dev libpam-dev) do
-  action :upgrade
-end
+package %w(libssl-dev libpam-dev)
 
 apt_repository 'duosecurity' do
   uri 'http://pkg.duosecurity.com/Ubuntu'
@@ -14,20 +31,16 @@ apt_repository 'duosecurity' do
   retry_delay 30
 end
 
-package 'duo-unix' do
-  action :upgrade
-end
+package 'duo-unix'
 
-template '/etc/duo/login_duo.conf' do
-  source 'login_duo.conf.erb'
-  owner node['travis_duo']['user']
-  group node['travis_duo']['group'] || node['travis_duo']['user']
-  mode 0o600
-end
-
-template '/etc/duo/pam_duo.conf' do
-  source 'pam_duo.conf.erb'
-  owner node['travis_duo']['user']
-  group node['travis_duo']['group'] || node['travis_duo']['user']
-  mode 0o600
+%w(
+  /etc/duo/login_duo.conf
+  /etc/duo/pam_duo.conf
+).each do |conf_path|
+  template conf_path do
+    source 'duo.conf.erb'
+    owner node['travis_duo']['user']
+    group node['travis_duo']['group'] || node['travis_duo']['user']
+    mode 0o600
+  end
 end
