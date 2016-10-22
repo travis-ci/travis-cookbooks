a958520a6d9a9ad2679f891ea8ea4544f24a7e47
Samuel Villamonte <Samureus@users.noreply.github.com>
2016-10-22 16:13:21 -0500

    Update default.rb to 5.4.1
    There was a change in the file naming convention.

--- cookbooks/pypy/attributes/default.rb
+++ cookbooks/pypy/attributes/default.rb
@@ -1,4 +1,4 @@
-version = '2.3'
+version = '5.4.1'
 arch    = kernel['machine'] =~ /x86_64/ ? 'amd64' : 'i386'
 linux_arch = arch == 'amd64' ? 'linux64' : 'linux'
 
@@ -6,9 +6,9 @@ default[:pypy] = {
   tarball: {
     version: version,
     arch: arch,
-    url: "https://bitbucket.org/pypy/pypy/downloads/pypy-#{version}-#{linux_arch}.tar.bz2",
-    filename: "pypy-#{version}-#{linux_arch}.tar.bz2",
-    dirname: "pypy-#{version}-#{linux_arch}",
+    url: "https://bitbucket.org/pypy/pypy/downloads/pypy2-v#{version}-#{linux_arch}.tar.bz2",
+    filename: "pypy2-v#{version}-#{linux_arch}.tar.bz2",
+    dirname: "pypy2-v#{version}-#{linux_arch}",
     installation_dir: '/usr/local/pypy'
   }
 }
