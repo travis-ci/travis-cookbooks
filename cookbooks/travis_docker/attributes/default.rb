4ece2dc1ecb3e08c9ab857ad48c49dde264ca59c
Dan Buch <dan@travis-ci.org>
2016-10-27 13:35:57 -0400

    Bump docker package, binary, and docker-compose binary versions
    Related to travis-ci/packer-templates#318

--- cookbooks/travis_docker/attributes/default.rb
+++ cookbooks/travis_docker/attributes/default.rb
@@ -1,11 +1,11 @@
-default['travis_docker']['version'] = '1.12.0-0~trusty'
+default['travis_docker']['version'] = '1.12.3-0~trusty'
 default['travis_docker']['users'] = %w(travis)
-default['travis_docker']['compose']['url'] = 'https://github.com/docker/compose/releases/download/1.8.0/docker-compose-Linux-x86_64'
-default['travis_docker']['compose']['sha256sum'] = 'ebc6ab9ed9c971af7efec074cff7752593559496d0d5f7afb6bfd0e0310961ff'
+default['travis_docker']['compose']['url'] = 'https://github.com/docker/compose/releases/download/1.8.1/docker-compose-Linux-x86_64'
+default['travis_docker']['compose']['sha256sum'] = 'd9c19bfd39ccd8bf7168c2afefb6a2cbd77d299c4d61531a220f6803ec6b701a'
 default['travis_docker']['update_grub'] = true
-default['travis_docker']['binary']['url'] = 'https://get.docker.com/builds/Linux/x86_64/docker-1.12.0.tgz'
-default['travis_docker']['binary']['version'] = '1.12.0'
-default['travis_docker']['binary']['checksum'] = '3dd07f65ea4a7b4c8829f311ab0213bca9ac551b5b24706f3e79a97e22097f8b'
+default['travis_docker']['binary']['url'] = 'https://get.docker.com/builds/Linux/x86_64/docker-1.12.3.tgz'
+default['travis_docker']['binary']['version'] = '1.12.3'
+default['travis_docker']['binary']['checksum'] = '626601deb41d9706ac98da23f673af6c0d4631c4d194a677a9a1a07d7219fa0f'
 default['travis_docker']['binary']['binaries'] = %w(
   docker
   docker-containerd
