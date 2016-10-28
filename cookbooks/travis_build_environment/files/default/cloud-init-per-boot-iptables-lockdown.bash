59575154edbbb402bb69c748f284562e9c55aa0d
Dan Buch <dan@travis-ci.org>
2016-10-28 09:27:02 -0400

    Adding iptables stuff to lockdown cloud init script

--- cookbooks/travis_build_environment/files/default/cloud-init-per-boot-iptables-lockdown.bash
+++ cookbooks/travis_build_environment/files/default/cloud-init-per-boot-iptables-lockdown.bash
@@ -2,8 +2,13 @@
 set -o errexit
 
 main() {
-  echo 'applying iptables rules for CI test VM isolation'
-  # TODO: do the stuff with the iptables
+  set -o xtrace
+  iptables -I INPUT 1 -p icmp -j ACCEPT
+  iptables -I INPUT 2 -p udp --dport 33434:33523 -j REJECT
+  iptables -I INPUT 3 -i lo -j ACCEPT
+  iptables -I INPUT 4 -m state --state ESTABLISHED,RELATED -j ACCEPT
+  iptables -I INPUT 5 -p tcp --dport ssh -j ACCEPT
+  iptables -A INPUT -j DROP
 }
 
 main "$@"
