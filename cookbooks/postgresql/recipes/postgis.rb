6ebca61ec543f7f0b630cb5c4bdab6a24f8921b6
Dan Buch <dan@travis-ci.org>
2016-11-17 16:27:17 -0500

    Install postgis and postgis scripts, too
    Closes travis-ci/packer-templates#224

--- cookbooks/postgresql/recipes/postgis.rb
+++ cookbooks/postgresql/recipes/postgis.rb
@@ -1,23 +1,13 @@
+ppv = node['postgresql']['postgis_version']
 
-#
-# PostGIS packages are available in Ubuntu main repository as of 14.04 (Trusty)
-#
-if node['lsb']['codename'] == 'precise'
-  apt_repository 'ubuntugis-stable' do
-    uri 'http://ppa.launchpad.net/ubuntugis/ppa/ubuntu'
-    distribution node['lsb']['codename']
-    components ['main']
-    key '314DF160'
-    keyserver 'hkp://ha.pool.sks-keyservers.net'
-    retries 2
-    retry_delay 30
-    action :add
-  end
-end
-
-#
-# At the moment, the same PostGIS *single* version is installed for all PostgreSQL instances
-#
-([node['postgresql']['default_version']] + node['postgresql']['alternate_versions']).each do |pg_version|
-  package "postgresql-#{pg_version}-postgis-#{node['postgresql']['postgis_version']}"
-end
+package(
+  (
+    [node['postgresql']['default_version']] +
+    node['postgresql']['alternate_versions']
+  ).map do |v|
+    %W(
+      postgresql-#{v}-postgis-#{ppv}
+      postgresql-#{v}-postgis-#{ppv}-scripts
+    )
+  end.flatten
+)
