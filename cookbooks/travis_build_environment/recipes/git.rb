08a53651957ab29229eca43debdd930681420f83
Dan Buch <dan@travis-ci.org>
2016-11-20 20:20:35 -0500

    Install latest git-lfs from deb

--- cookbooks/travis_build_environment/recipes/git.rb
+++ cookbooks/travis_build_environment/recipes/git.rb
@@ -12,12 +12,6 @@ package 'git' do
   action :upgrade
 end
 
-bash 'install git-lfs' do
-  cwd Chef::Config[:file_cache_path]
-  code <<-EOF
-    curl -sLo - https://github.com/github/git-lfs/releases/download/v#{node['travis_build_environment']['git_lfs_version']}/git-lfs-linux-amd64-#{node['travis_build_environment']['git_lfs_version']}.tar.gz | tar xzvf -
-    cd git-lfs-*
-    ./install.sh
-  EOF
-  action :run
+packagecloud_repo 'github/git-lfs' do
+  type 'deb'
 end
