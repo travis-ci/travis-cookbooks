4ed0a04f6335c419a4ed3fb05947b5f4c79c1bc2
Dan Buch <dan@travis-ci.org>
2016-12-05 19:45:45 -0500

    Collapse firefox cookbook into travis_build_environment
    and upgrade from 38.4.0esr :arrow_right: 50.0.2

---
+++ cookbooks/travis_build_environment/recipes/firefox.rb
@@ -0,0 +1,33 @@
+package %w(
+  libasound2
+  libatk1.0-0
+  libc6
+  libcairo2
+  libdbus-1-3
+  libdbus-glib-1-2
+  libfontconfig1
+  libfreetype6
+  libgcc1
+  libgdk-pixbuf2.0-0
+  libglib2.0-0
+  libgtk2.0-0
+  libnotify4
+  libpango1.0-0
+  libstartup-notification0
+  libstdc++6
+  libx11-6
+  libxext6
+  libxrender1
+  libxt6
+  lsb-release
+) do
+  action %i(install upgrade)
+end
+
+ark 'firefox' do
+  url node['firefox']['tarball']['download_url']
+  version node['firefox']['tarball']['version']
+  owner node['travis_build_environment']['user']
+  group node['travis_build_environment']['group']
+  has_binaries %w(firefox firefox-bin)
+end
