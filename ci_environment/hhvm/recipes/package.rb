85104a1e2b1892891ac3be1a8054a116b7e9e440
emdantrim <emdantrim@gmail.com>
2017-04-10 13:53:21 -0400

    Remove boost shenanigans for unused cookbook

--- ci_environment/hhvm/recipes/package.rb
+++ ci_environment/hhvm/recipes/package.rb
@@ -7,14 +7,6 @@ apt_repository "boost-backports" do
   action       :add
 end
 
-package Array(node['libboost-filesystem1.49.0', 'libboost-program-options1.49.0', 'libboost-regex1.49.0', 'libboost-system1.49.0', 'libboost-thread1.49.0', 'libgd2-xpm', 'libonig2', 'libtbb2', 'libunwind7']['package']['name']) do
-  action :install
-end
-
-apt_repository "boost-backports" do
-  action       :remove
-end
-
 apt_repository "hhvm-repository" do
   uri          "http://dl.hhvm.com/ubuntu"
   distribution node["lsb"]["codename"]
