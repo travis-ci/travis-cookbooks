2369e8f348c9c9a25a9e0bfcd9e8dc9937271466
Lyoness <carmen.andoh@gmail.com>
2017-05-16 17:59:13 -0400

    re-vendor the git community-cookbook

--- community-cookbooks/git/libraries/provider_git_client_package.rb
+++ community-cookbooks/git/libraries/provider_git_client_package.rb
@@ -4,7 +4,7 @@ class Chef
       class Package < Chef::Provider::GitClient
         include Chef::DSL::IncludeRecipe
 
-        provides :git_client, os: 'linux' if respond_to?(:provides)
+        provides :git_client, os: 'linux'
 
         action :install do
           # FIXME: rhel 5
