6e1070e3f854cb9ed46fc2a2f30c6ec37fb55f84
Dan Buch <daniel.buch@gmail.com>
2017-05-22 00:17:15 +0200

    Import git version 6.0.0

--- community-cookbooks/git/libraries/helpers.rb
+++ community-cookbooks/git/libraries/helpers.rb
@@ -16,12 +16,12 @@ module GitCookbook
     # source
     def parsed_source_url
       return new_resource.source_url if new_resource.source_url
-      return "https://nodeload.github.com/git/git/tar.gz/v#{new_resource.source_version}"
+      "https://nodeload.github.com/git/git/tar.gz/v#{new_resource.source_version}"
     end
 
     def parsed_source_checksum
       return new_resource.source_checksum if new_resource.source_checksum
-      return '0f30984828d573da01d9f8e78210d5f4c56da1697fd6d278bad4cfa4c22ba271' # 1.9.5 tarball
+      '8d53703d75890c03e26a915c7af3b7b98d8cfb94382f685a9bcbee1eeaec47b4' # 2.7.4 tarball
     end
 
     # windows
@@ -32,18 +32,17 @@ module GitCookbook
 
     def parsed_windows_package_version
       return new_resource.windows_package_version if new_resource.windows_package_version
-      '1.9.5-preview20141217'
+      '2.7.4'
     end
 
     def parsed_windows_package_url
       return new_resource.windows_package_url if new_resource.windows_package_url
-      "https://github.com/msysgit/msysgit/releases/download/Git-#{parsed_windows_package_version}/Git-#{parsed_windows_package_version}.exe"
+      "https://github.com/git-for-windows/git/releases/download/v%#{parsed_windows_package_version}.windows.1/Git-%#{parsed_windows_package_version}-32-bit.exe"
     end
 
     def parsed_windows_package_checksum
       return new_resource.windows_package_checksum if new_resource.windows_package_checksum
-      'd7e78da2251a35acd14a932280689c57ff9499a474a448ae86e6c43b882692dd'
+      '49601d5102df249d6f866ecfa1eea68eb5672acc1dbb7e4051099e792f6da5fc'
     end
-
   end
 end
