f74aa10b6dccfb760cf2bc05486846314598c0fc
Dan Buch <daniel.buch@gmail.com>
2017-05-22 12:10:16 +0200

    Explicitly install mercurial-common *and then* mercurial
    plus back out p80.pool.sks-keyservers.net

--- cookbooks/travis_build_environment/recipes/cloud_init.rb
+++ cookbooks/travis_build_environment/recipes/cloud_init.rb
@@ -25,7 +25,7 @@ apt_repository 'pollinate' do
   distribution node['lsb']['codename']
   components %w(main)
   key '43732553'
-  keyserver 'hkp://p80.pool.sks-keyservers.net:80'
+  keyserver 'hkp://ha.pool.sks-keyservers.net'
   retries 2
   retry_delay 30
 end
