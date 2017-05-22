6770676179f50da1e0de996bac73abeb1aab7cae
Dan Buch <daniel.buch@gmail.com>
2017-05-22 21:27:32 +0200

    Revert "NOT GOOD: mutate android-sdk for testing"
    This reverts commit 2cf36543227f7aa05973c94d576deb5b204ece5d.

--- community-cookbooks/android-sdk/recipes/default.rb
+++ community-cookbooks/android-sdk/recipes/default.rb
@@ -117,7 +117,7 @@ unless File.exist?("#{setup_root}/#{node['android-sdk']['name']}/temp")
     script "Install Android SDK component #{sdk_component}" do
       interpreter 'expect'
       environment 'ANDROID_HOME' => android_home
-      cwd [File.join(android_home, 'tools')]
+      path [File.join(android_home, 'tools')]
       user node['android-sdk']['owner']
       group node['android-sdk']['group']
       # TODO: use --force or not?
