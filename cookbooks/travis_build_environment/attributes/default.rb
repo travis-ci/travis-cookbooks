1e0e5f1d486fb96f126429a99657e804efa4c847
Dan Buch <dan@travis-ci.org>
2017-07-01 22:50:59 -0400

    Add an init script (back?) for neo4j
    so that it can continue to work with travis-build `services`

--- cookbooks/travis_build_environment/attributes/default.rb
+++ cookbooks/travis_build_environment/attributes/default.rb
@@ -181,9 +181,6 @@ default['travis_build_environment']['elasticsearch']['version'] = '5.4.3'
 default['travis_build_environment']['elasticsearch']['service_enabled'] = false
 default['travis_build_environment']['elasticsearch']['jvm_heap'] = '128m'
 
-default['travis_build_environment']['neo4j']['service_enabled'] = false
-default['travis_build_environment']['neo4j']['jvm_heap'] = '128m'
-
 default['travis_build_environment']['redis']['service_enabled'] = false
 
 default['travis_build_environment']['firefox_version'] = '50.0.2'
@@ -248,6 +245,8 @@ default['travis_build_environment']['maven_binaries'] = %w(
   bin/mvnyjp
 )
 
+default['travis_build_environment']['neo4j']['service_enabled'] = false
+default['travis_build_environment']['neo4j']['jvm_heap'] = '128m'
 default['travis_build_environment']['neo4j_url'] = 'https://neo4j.com/artifact.php?name=neo4j-community-3.2.1-unix.tar.gz'
 default['travis_build_environment']['neo4j_version'] = '3.2.1'
 default['travis_build_environment']['neo4j_checksum'] = '24fd6a704e0d80c4b4f9a3d17ce0db23f258a8cdcfa1eb28d7803b7d1811ee96'
