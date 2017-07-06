bf101ebb1fd3b08ce327bb0d39e1592c9a532581
Dan Buch <dan@travis-ci.org>
2017-07-06 09:02:58 -0400

    Optionally install mercurial via pip

--- cookbooks/travis_build_environment/recipes/mercurial.rb
+++ cookbooks/travis_build_environment/recipes/mercurial.rb
@@ -4,9 +4,26 @@ apt_repository 'mercurial' do
   keyserver 'hkp://ha.pool.sks-keyservers.net'
   retries 2
   retry_delay 30
+  only_if do
+    node['travis_build_environment']['mercurial_install_type'] == 'ppa'
+  end
 end
 
 package 'mercurial' do
   version node['travis_build_environment']['mercurial_version']
   action %i(install upgrade)
+  only_if do
+    node['travis_build_environment']['mercurial_install_type'] == 'ppa'
+  end
+end
+
+execute %W(
+  pip install
+  "mercurial==#{node['travis_build_environment']['mercurial_version'].sub(/~.*/, '')}"
+).join(' ') do
+  user 'root'
+  group 'root'
+  only_if do
+    node['travis_build_environment']['mercurial_install_type'] == 'pip'
+  end
 end
