0b05cb493732e604f38430b496fd63715b591feb
Lyoness <carmen.andoh@gmail.com>
2017-08-16 11:43:11 -0400

    remove only_if conditions for ppc64le + pip

--- cookbooks/travis_build_environment/recipes/mercurial.rb
+++ cookbooks/travis_build_environment/recipes/mercurial.rb
@@ -15,19 +15,8 @@ package 'mercurial' do
   not_if { node['kernel']['machine'] == 'ppc64le' }
 end
 
-execute %W[
-  pip install
-  "mercurial==#{node['travis_build_environment']['mercurial_version'].sub(/~.*/, '')}"
-].join(' ') do
-  user 'root'
-  group 'root'
-  only_if { node['travis_build_environment']['mercurial_install_type'] == 'pip' }
-  not_if { node['kernel']['machine'] == 'ppc64le' }
-end
-
 package 'python-docutils' do
   action %i[install upgrade]
-  only_if { node['kernel']['machine'] == 'ppc64le' }
 end
 
 ark 'mercurial' do
@@ -36,5 +25,5 @@ ark 'mercurial' do
   make_opts ['all']
   action :install_with_make
   only_if { node['travis_build_environment']['mercurial_install_type'] == 'src' }
-  only_if { node['kernel']['machine'] == 'ppc64le' }
+
 end
