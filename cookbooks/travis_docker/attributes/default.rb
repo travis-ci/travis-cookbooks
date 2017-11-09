ced27bc4372c4e0dadb0ce712bfdbef2321a33cf
Ed Morley <501702+edmorley@users.noreply.github.com>
2017-11-09 17:45:19 +0000

    Update docker-compose from 1.15.0 to 1.17.1
    https://docs.docker.com/release-notes/docker-compose/

--- cookbooks/travis_docker/attributes/default.rb
+++ cookbooks/travis_docker/attributes/default.rb
@@ -4,8 +4,8 @@
 # https://download.docker.com/linux/ubuntu/dists/xenial/stable/binary-amd64/Packages
 default['travis_docker']['version'] = '17.09.0~ce-0~ubuntu'
 default['travis_docker']['users'] = %w[travis]
-default['travis_docker']['compose']['url'] = 'https://github.com/docker/compose/releases/download/1.15.0/docker-compose-Linux-x86_64'
-default['travis_docker']['compose']['sha256sum'] = 'acfa66dba77dac9635ff9b195ccea81768eb009ce9c9f1181c000eb95effb963'
+default['travis_docker']['compose']['url'] = 'https://github.com/docker/compose/releases/download/1.17.1/docker-compose-Linux-x86_64'
+default['travis_docker']['compose']['sha256sum'] = 'db0a7b79d195dc021461d5628a8d53eeb2e556d2548b764770fccabb0a319dd8'
 default['travis_docker']['update_grub'] = true
 default['travis_docker']['binary']['url'] = "https://download.docker.com/linux/static/stable/#{node['kernel']['machine']}/docker-17.09.0-ce.tgz"
 default['travis_docker']['binary']['version'] = '17.09.0-ce'
