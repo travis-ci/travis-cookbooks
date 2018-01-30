1c1c3cbe806c82a85b3f60769777b6c9a150bf03
Dan Buch <dan@travis-ci.org>
2018-01-30 15:06:48 -0500

    Source jdk_switcher via ~travis/.bash_profile.d
    instead of /etc/profile.d, as the latter is loaded by all users and the
    contents are supposed to be sh-compatible, whereas jdk_switcher has
    bash-specific stuff.
    
    Closes #964

--- cookbooks/travis_java/recipes/default.rb
+++ cookbooks/travis_java/recipes/default.rb
@@ -13,9 +13,13 @@ unless default_java_version.to_s.empty?
 end
 
 include_recipe 'travis_java::jdk_switcher'
+include_recipe 'travis_build_environment::bash_profile_d'
 
-template '/etc/profile.d/travis-java.sh' do
-  source 'travis-java.sh.erb'
+template ::File.join(
+  node['travis_build_environment']['home'],
+  '.bash_profile.d/travis-java.bash'
+) do
+  source 'travis-java.bash.erb'
   owner node['travis_build_environment']['user']
   group node['travis_build_environment']['group']
   mode 0o755
