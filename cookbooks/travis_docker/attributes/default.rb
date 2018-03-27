b3a4cf58e76681fb5829d234579c33835d2fc101
Dan Buch <dan@travis-ci.org>
2018-03-26 23:30:16 -0400

    Fix a stray docker version string

--- cookbooks/travis_docker/attributes/default.rb
+++ cookbooks/travis_docker/attributes/default.rb
@@ -7,7 +7,7 @@ default['travis_docker']['users'] = %w[travis]
 default['travis_docker']['compose']['url'] = 'https://github.com/docker/compose/releases/download/1.17.1/docker-compose-Linux-x86_64'
 default['travis_docker']['compose']['sha256sum'] = 'db0a7b79d195dc021461d5628a8d53eeb2e556d2548b764770fccabb0a319dd8'
 default['travis_docker']['update_grub'] = true
-default['travis_docker']['binary']['url'] = "https://download.docker.com/linux/static/stable/#{node['kernel']['machine']}/docker-17.09.0-ce.tgz"
+default['travis_docker']['binary']['url'] = "https://download.docker.com/linux/static/stable/#{node['kernel']['machine']}/docker-17.12.1-ce.tgz"
 default['travis_docker']['binary']['version'] = '17.12.1-ce'
 default['travis_docker']['binary']['checksum'] = 'f1722346f98463e44018dc9f1ce5c56c4b203d4237ecc8f4b4fbc1eac1693f18'
 if node['kernel']['machine'] == 'ppc64le'
