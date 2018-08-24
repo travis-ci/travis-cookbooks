1826b73610188d482a8334ed936ca790cf68fb1a
Joep van Delft <joep@travis-ci.org>
2018-08-24 14:50:38 +0200

    Adjust jvm target path

--- cookbooks/travis_jdk/attributes/default.rb
+++ cookbooks/travis_jdk/attributes/default.rb
@@ -2,4 +2,4 @@ default['travis_jdk']['versions'] = []
 default['travis_jdk']['default'] = nil
 
 default['travis_jdk']['install-jdk.sh_path'] = '/usr/local/bin/install-jdk.sh'
-default['travis_jdk']['destination_path'] = '/usr/local/lib'
+default['travis_jdk']['destination_path'] = '/usr/local/lib/jvm'
