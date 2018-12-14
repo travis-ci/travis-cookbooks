024e199587e4ddbc7cf4e0efb693b324480d9f17
Joep van Delft <joep@travis-ci.org>
2018-12-14 09:30:27 +0100

    Test for FC001

--- cookbooks/tmate_remote_tmux/recipes/rename_openssh_port.rb
+++ cookbooks/tmate_remote_tmux/recipes/rename_openssh_port.rb
@@ -5,7 +5,7 @@ sshd_config_file = "/etc/ssh/sshd_config"
 ruby_block "Change SSH port" do
   block do
     file = Chef::Util::FileEdit.new(sshd_config_file)
-    file.search_file_replace(/^Port [0-9]+$/, "Port #{node[:tmate_remote_tmux][:openssh_port]}")
+    file.search_file_replace(/^Port [0-9]+$/, "Port #{node['tmate_remote_tmux']['openssh_port']}")
     file.write_file
   end
 
