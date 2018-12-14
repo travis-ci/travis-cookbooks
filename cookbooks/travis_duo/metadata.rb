159932b8d0778ee3ab3dd65ae3306f061e06c7c5
Joep van Delft <joep@travis-ci.org>
2018-12-14 10:39:45 +0100

    Test for FC068

--- cookbooks/travis_duo/metadata.rb
+++ cookbooks/travis_duo/metadata.rb
@@ -3,6 +3,7 @@
 name 'travis_duo'
 maintainer 'Travis CI GmbH'
 maintainer_email 'contact+travis-cookbooks-travis-duo@travis-ci.org'
+license 'MIT'
 issues_url 'https://github.com/travis-ci/travis-cookbooks/issues'
 source_url 'https://github.com/travis-ci/travis-cookbooks/master/cookbooks/travis_duo'
 version '0.1.0'
