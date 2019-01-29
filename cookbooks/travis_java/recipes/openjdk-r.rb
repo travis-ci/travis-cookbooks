67ac072caa55ed691ec4c9e099c11609c9305784
Akhilesh Bhople <abhople@us.ibm.com>
2019-01-29 15:36:06 +0530

    Update installation of openjdk-r repo

--- cookbooks/travis_java/recipes/openjdk-r.rb
+++ cookbooks/travis_java/recipes/openjdk-r.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 apt_repository 'openjdk-r-java-ppa' do
-  uri 'http://ppa.launchpad.net/openjdk-r/ppa/ubuntu'
+  uri 'ppa:openjdk-r/ppa'
   distribution node['lsb']['codename']
   components %w[main]
-  key '86F44E2A'
-  keyserver 'hkp://ha.pool.sks-keyservers.net'
   retries 2
   retry_delay 30
   action :add
