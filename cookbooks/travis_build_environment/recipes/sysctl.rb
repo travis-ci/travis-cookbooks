dabb927907de783ca19305158f6154f2f786870f
Joep van Delft <joep@travis-ci.org>
2019-02-13 13:19:51 +0100

    Revert "Disable some network fiddling"
    This reverts commit c094021c9add0a94c5bd80c5df15c0a36e2ec181.

--- cookbooks/travis_build_environment/recipes/sysctl.rb
+++ cookbooks/travis_build_environment/recipes/sysctl.rb
@@ -13,33 +13,33 @@ file '/etc/sysctl.d/30-travis-shm.conf' do
   notifies :run, 'execute[update sysctl travis-shm]'
 end
 
-# execute 'update sysctl travis-disable-ipv6' do
-#   command 'sysctl -p /etc/sysctl.d/99-travis-disable-ipv6.conf'
-#   action :nothing
-# end
-# 
-# file '/etc/sysctl.d/99-travis-disable-ipv6.conf' do
-#   content <<-IPV6_CONFIG.gsub(/^\s+> ?/, '')
-#     > net.ipv6.conf.all.disable_ipv6 = 1
-#     > net.ipv6.conf.default.disable_ipv6 = 1
-#   IPV6_CONFIG
-#   owner 'root'
-#   group 'root'
-#   mode 0o644
-#   only_if { node['travis_build_environment']['sysctl_disable_ipv6'] }
-#   notifies :run, 'execute[update sysctl travis-disable-ipv6]'
-# end
-# 
-# execute 'update sysctl travis-enable-ipv4-forwarding' do
-#   command 'sysctl -p /etc/sysctl.d/99-travis-enable-ipv4-forwarding.conf'
-#   action :nothing
-# end
-# 
-# file '/etc/sysctl.d/99-travis-enable-ipv4-forwarding.conf' do
-#   content "net.ipv4.ip_forward = 1\n"
-#   owner 'root'
-#   group 'root'
-#   mode 0o644
-#   only_if { node['travis_build_environment']['sysctl_enable_ipv4_forwarding'] }
-#   notifies :run, 'execute[update sysctl travis-enable-ipv4-forwarding]'
-# end
+execute 'update sysctl travis-disable-ipv6' do
+  command 'sysctl -p /etc/sysctl.d/99-travis-disable-ipv6.conf'
+  action :nothing
+end
+
+file '/etc/sysctl.d/99-travis-disable-ipv6.conf' do
+  content <<-IPV6_CONFIG.gsub(/^\s+> ?/, '')
+    > net.ipv6.conf.all.disable_ipv6 = 1
+    > net.ipv6.conf.default.disable_ipv6 = 1
+  IPV6_CONFIG
+  owner 'root'
+  group 'root'
+  mode 0o644
+  only_if { node['travis_build_environment']['sysctl_disable_ipv6'] }
+  notifies :run, 'execute[update sysctl travis-disable-ipv6]'
+end
+
+execute 'update sysctl travis-enable-ipv4-forwarding' do
+  command 'sysctl -p /etc/sysctl.d/99-travis-enable-ipv4-forwarding.conf'
+  action :nothing
+end
+
+file '/etc/sysctl.d/99-travis-enable-ipv4-forwarding.conf' do
+  content "net.ipv4.ip_forward = 1\n"
+  owner 'root'
+  group 'root'
+  mode 0o644
+  only_if { node['travis_build_environment']['sysctl_enable_ipv4_forwarding'] }
+  notifies :run, 'execute[update sysctl travis-enable-ipv4-forwarding]'
+end
