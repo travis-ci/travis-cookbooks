89f8828b44ed672196deddf358ef504e4a0c426d
Christian Clauss <cclauss@me.com>
2020-04-17 19:55:42 +0200

    pip.rb: python3 get-pip.py
    As requested by @native-api at https://github.com/travis-ci/docs-travis-ci-com/pull/2413#discussion_r410330159, now that [Python 2 is end of life](https://devguide.python.org/devcycle/#end-of-life-branches), let's use [Python 3](https://devguide.python.org/#status-of-python-branches) to install `pip`.

--- cookbooks/travis_build_environment/recipes/pip.rb
+++ cookbooks/travis_build_environment/recipes/pip.rb
@@ -9,7 +9,7 @@ end
 bash 'install-pip' do
   cwd Chef::Config[:file_cache_path]
   code <<-INSTALL_PIP
-    python get-pip.py
+    python3 get-pip.py
     pip install --upgrade pip setuptools wheel
   INSTALL_PIP
   not_if 'which pip'
