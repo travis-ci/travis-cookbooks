8c0c2da3db032c71d51f7eb9fba34f4d532065ce
Christian Clauss <cclauss@me.com>
2020-04-22 13:28:48 +0200

    Update cookbooks/travis_build_environment/recipes/pip.rb
    Co-Authored-By: native-api <ivan_pozdeev@mail.ru>

--- cookbooks/travis_build_environment/recipes/pip.rb
+++ cookbooks/travis_build_environment/recipes/pip.rb
@@ -10,6 +10,8 @@ bash 'install-pip' do
   cwd Chef::Config[:file_cache_path]
   code <<-INSTALL_PIP
     python3 get-pip.py
+    python2 get-pip.py
+    python get-pip.py
     pip install --upgrade pip setuptools wheel
   INSTALL_PIP
   not_if 'which pip'
