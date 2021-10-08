08c65258d1ef6684383a0361e05c81f6d8b891e4
Damian Szymański <damian@martnet.pl>
2021-10-08 15:57:09 +0200

    Update gimme.rb

--- cookbooks/travis_build_environment/recipes/gimme.rb
+++ cookbooks/travis_build_environment/recipes/gimme.rb
@@ -97,6 +97,5 @@ gimme_versions.each do |version|
     environment(install_env.merge('GIMME_GO_VERSION' => version))
     user node['travis_build_environment']['user']
     group node['travis_build_environment']['group']
-    umask 0o077
   end
 end
