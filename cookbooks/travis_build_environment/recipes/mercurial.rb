7670f8de055d21d91dfcc9570de7646a2ddb967b
Konrad Wrzos <konrad@travis-ci.org>
2022-06-30 15:16:12 +0200

    Security updates

--- cookbooks/travis_build_environment/recipes/mercurial.rb
+++ cookbooks/travis_build_environment/recipes/mercurial.rb
@@ -2,7 +2,6 @@
 
 apt_repository 'mercurial' do
   uri 'ppa:mercurial-ppa/releases'
-  distribution node['lsb']['codename']
   keyserver 'hkp://ha.pool.sks-keyservers.net'
   retries 2
   retry_delay 30
