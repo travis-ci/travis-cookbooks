7c484e18648f9c8796d9cfe42aeea0767592ace9
Konrad Wrzos <konrad@travis-ci.org>
2022-07-26 12:35:57 +0200

    Testing PyPa

--- cookbooks/travis_build_environment/recipes/pip.rb
+++ cookbooks/travis_build_environment/recipes/pip.rb
@@ -7,19 +7,26 @@ when 'xenial', 'bionic'
     mode '644'
     not_if 'which pip'
   end
+  bash 'install-pip' do
+    cwd Chef::Config[:file_cache_path]
+    code <<-INSTALL_PIP
+      python get-pip.py
+      pip install --upgrade pip setuptools wheel
+    INSTALL_PIP
+    not_if 'which pip'
+  end
 when 'focal', 'jammy'
   remote_file "#{Chef::Config[:file_cache_path]}/get-pip.py" do
     source 'https://bootstrap.pypa.io/get-pip.py'
     mode '644'
     not_if 'which pip'
   end
-end
-
-bash 'install-pip' do
-  cwd Chef::Config[:file_cache_path]
-  code <<-INSTALL_PIP
-    python get-pip.py
-    pip install --upgrade pip setuptools wheel
-  INSTALL_PIP
-  not_if 'which pip'
+  bash 'install-pip3' do
+    cwd Chef::Config[:file_cache_path]
+    code <<-INSTALL_PIP
+      python3 get-pip.py
+      pip3 install --upgrade pip setuptools wheel
+    INSTALL_PIP
+    not_if 'which pip'
+  end
 end
