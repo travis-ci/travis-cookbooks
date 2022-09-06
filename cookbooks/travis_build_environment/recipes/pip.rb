8c0157e6db56caf71b9da62a9636c85b9f915685
Robert Mazur <robxmazur@gmail.com>
2022-09-06 20:45:02 -0300

    Use pip 3.5 installer

--- cookbooks/travis_build_environment/recipes/pip.rb
+++ cookbooks/travis_build_environment/recipes/pip.rb
@@ -15,9 +15,23 @@ when 'bionic'
     INSTALL_PIP
     not_if 'which pip'
   end
-when 'xenial', 'focal', 'jammy'
+when 'focal', 'jammy'
   remote_file "#{Chef::Config[:file_cache_path]}/get-pip.py" do
     source 'https://bootstrap.pypa.io/get-pip.py'
+    mode '644'
+    not_if 'which pip'
+  end
+  bash 'install-pip3' do
+    cwd Chef::Config[:file_cache_path]
+    code <<-INSTALL_PIP
+      python3 get-pip.py
+      pip3 install --upgrade pip setuptools wheel
+    INSTALL_PIP
+    not_if 'which pip'
+  end
+when 'xenial'
+  remote_file "#{Chef::Config[:file_cache_path]}/get-pip.py" do
+    source 'https://bootstrap.pypa.io/pip/3.5/get-pip.py'
     mode '644'
     not_if 'which pip'
   end
