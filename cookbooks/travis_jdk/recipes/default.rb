16ec012a00e8e7779d5b744ba7b0618130e41881
Kamileit <kamilmaludzinski@gmail.com>
2025-01-28 15:10:45 +0100

    jdk instalation test

--- cookbooks/travis_jdk/recipes/default.rb
+++ cookbooks/travis_jdk/recipes/default.rb
@@ -25,9 +25,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-# install_jdk.sh does not support ppc64le
+# Skrypt install_jdk.sh nie obsługuje architektury ppc64le
 return if node['kernel']['machine'] == 'ppc64le'
 
+# Funkcja do generowania argumentów dla skryptu install-jdk.sh
 def install_jdk_args(jdk)
   m = jdk.match(/(?<vendor>[a-z]+)-?(?<version>.+)?/)
   if m[:vendor].start_with? 'oracle'
@@ -35,26 +36,19 @@ def install_jdk_args(jdk)
   elsif m[:vendor].start_with? 'openjdk'
     license = 'GPL'
   else
-    puts 'Houston is calling'
+    puts 'Houston is calling: Nieznany dostawca JDK!'
+    license = 'UNKNOWN'
   end
   "--feature #{m[:version]} --license #{license}"
 end
 
+# Lista wersji JDK do zainstalowania
 versions = [
   node['travis_jdk']['default'],
   node['travis_jdk']['versions'],
-].flatten.uniq
+].flatten.compact.uniq
 
-# remote_file 'install-jdk.sh' do
-#  source 'https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh'
-#  path node['travis_jdk']['install-jdk.sh_path']
-#  owner 'root'
-#  group 'root'
-#  mode '0755'
-#  action :create
-#  sensitive true
-# end
-
+# Kopiowanie skryptu install-jdk.sh do /usr/local/bin
 cookbook_file '/usr/local/bin/install-jdk.sh' do
   source 'install-jdk.sh'
   owner 'root'
@@ -64,14 +58,18 @@ cookbook_file '/usr/local/bin/install-jdk.sh' do
   sensitive true
 end
 
-apt_update do
+# Aktualizacja listy pakietów APT
+apt_update 'update_sources' do
   action :update
 end
 
+# Instalacja pakietu default-jre
 apt_package 'default_java' do
   package_name %w(default-jre)
+  action :install
 end
 
+# Instalacja poszczególnych wersji JDK
 versions.each do |jdk|
   next if jdk.nil?
 
@@ -81,17 +79,30 @@ versions.each do |jdk|
     node['travis_jdk']['destination_path'],
     jdk
   )
-  # Get system ca-certs symlinked
+  # Dodanie opcji --cacerts dla licencji GPL
   cacerts = '--cacerts' if args =~ /GPL/
 
   bash "Install #{jdk}" do
-    code "#{node['travis_jdk']['install-jdk.sh_path']}" \
-      " #{args} --target #{target} --workspace #{cache} #{cacerts}"
+    # Użycie heredoc do lepszej czytelności i obsługi wieloliniowego kodu
+    code <<-EOH
+      echo "===== Instalacja JDK #{jdk} ====="
+      echo "Parametry: #{args} --target #{target} --workspace #{cache} #{cacerts}"
+      set -x
+      /usr/local/bin/install-jdk.sh #{args} --target #{target} --workspace #{cache} #{cacerts}
+      set +x
+    EOH
+    # Włączenie streamowania outputu do logów Chef
+    live_stream true
+    # Opcjonalnie, możesz dodać retry lub inne atrybuty
+    retries 3
+    retry_delay 5
   end
 end
 
+# Dołączenie recipe dla profilu bash użytkownika travis
 include_recipe 'travis_build_environment::bash_profile_d'
 
+# Generowanie pliku konfiguracyjnego bash_profile.d/travis_jdk.bash
 template ::File.join(
   node['travis_build_environment']['home'],
   '.bash_profile.d/travis_jdk.bash'
@@ -99,7 +110,7 @@ template ::File.join(
   source 'travis_jdk.bash.erb'
   owner node['travis_build_environment']['user']
   group node['travis_build_environment']['group']
-  mode '644'
+  mode '0644'
   variables(
     jdk: node['travis_jdk']['default'],
     path: node['travis_jdk']['destination_path'],
