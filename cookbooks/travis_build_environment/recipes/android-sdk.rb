5b25c8474b411665893f30011e717938c233138d
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-02-21 18:00:25 +0100

    fix

--- cookbooks/travis_build_environment/recipes/android-sdk.rb
+++ cookbooks/travis_build_environment/recipes/android-sdk.rb
@@ -1,43 +1,63 @@
-# recipes/android-sdk.rb
-
 require 'digest'
 
+# Ustawienia ścieżek
 setup_root       = node['android-sdk']['setup_root'].to_s.empty? ? node['ark']['prefix_home'] : node['android-sdk']['setup_root']
 android_home     = File.join(setup_root, node['android-sdk']['name'])
-android_bin      = File.join(android_home, 'tools', 'android')
+android_bin      = File.join(android_home, 'cmdline-tools', 'latest', 'bin', 'sdkmanager')
 temp_file        = "/tmp/android-sdk.zip"
 android_sdk_url  = node['android-sdk']['download_url']
 
+# Pobranie pliku Android SDK przed jego instalacją
 remote_file temp_file do
   source android_sdk_url
   action :create
 end
 
-ruby_block "calculate_checksum" do
-  block do
-    checksum = Digest::SHA256.file(temp_file).hexdigest
-    node.override['android-sdk']['checksum'] = checksum
-    Chef::Log.info("✅ Dynamic checksum: #{checksum}")
-  end
-  action :run
+# Obliczenie sumy kontrolnej SHA256
+directory "#{setup_root}" do
+  owner node['android-sdk']['owner']
+  group node['android-sdk']['group']
+  mode '755'
+  recursive true
+  action :create
 end
 
-if node['platform'] == 'ubuntu'
+ruby_block "calculate_checksum" do
+  block do
+    if File.exist?(temp_file)
+      checksum = Digest::SHA256.file(temp_file).hexdigest
+      node.run_state['android_sdk_checksum'] = checksum
+      Chef::Log.info("✅ Dynamic checksum: #{checksum}")
+    else
+      Chef::Log.error("❌ Plik #{temp_file} nie istnieje! Nie można obliczyć sumy kontrolnej.")
+    end
+  end
+  action :run
+  only_if { ::File.exist?(temp_file) }
+end
+
+# Instalacja pakietów zależnych
+if platform?('ubuntu')
   package 'libgl1-mesa-dev'
   if node['kernel']['machine'] != 'i686'
-    if Chef::VersionConstraint.new('>= 13.04').include?(node['platform_version'])
-      package 'lib32stdc++6'
-    elsif Chef::VersionConstraint.new('>= 11.10').include?(node['platform_version'])
-      package 'libstdc++6:i386'
-    end
+    package 'lib32stdc++6' if node['platform_version'].to_f >= 13.04
+    package 'libstdc++6:i386' if node['platform_version'].to_f >= 11.10
     package 'lib32z1'
   end
+end
+
+# Pobranie i instalacja Android SDK
+directory android_home do
+  owner node['android-sdk']['owner']
+  group node['android-sdk']['group']
+  mode '755'
+  action :create
 end
 
 ark node['android-sdk']['name'] do
   url android_sdk_url
   path node['android-sdk']['setup_root']
-  checksum lazy { node['android-sdk']['checksum'] } 
+  checksum lazy { node.run_state['android_sdk_checksum'] }
   version node['android-sdk']['version']
   prefix_root node['android-sdk']['setup_root']
   prefix_home node['android-sdk']['setup_root']
@@ -47,13 +67,7 @@ ark node['android-sdk']['name'] do
   action node['android-sdk']['with_symlink'] ? :install : :put
 end
 
-
-directory File.join(android_home, 'tools') do
-  mode '755'
-  user node['android-sdk']['owner']
-  group node['android-sdk']['group']
-end
-
+# Nadanie uprawnień
 execute 'Grant all users to read android files' do
   command "chmod -R a+r #{android_home}/*"
   user node['android-sdk']['owner']
@@ -61,53 +75,31 @@ execute 'Grant all users to read android files' do
 end
 
 execute 'Grant all users to execute android tools' do
-  command "chmod -R a+X #{File.join(android_home, 'tools')}/*"
+  command "chmod -R a+X #{File.join(android_home, 'cmdline-tools', 'latest', 'bin')}/*"
   user node['android-sdk']['owner']
   group node['android-sdk']['group']
 end
 
-
-template "/etc/profile.d/#{node['android-sdk']['name']}.sh" do
-  source 'android-sdk.sh.erb'
-  mode '644'
-  owner node['android-sdk']['owner']
-  group node['android-sdk']['group']
-  variables(
-    android_home: android_home
-  )
-  only_if { node['android-sdk']['set_environment_variables'] }
-end
-
-package 'expect'
-
-unless File.exist?("#{setup_root}/#{node['android-sdk']['name']}/temp")
+# Instalacja składników SDK
+if File.exist?(android_bin)
   node['android-sdk']['components'].each do |sdk_component|
-    script "Install Android SDK component #{sdk_component}" do
-      interpreter 'expect'
-      environment 'ANDROID_HOME' => android_home
+    execute "Install Android SDK component #{sdk_component}" do
+      command "#{android_bin} --install #{sdk_component} --sdk_root=#{android_home}"
       user node['android-sdk']['owner']
       group node['android-sdk']['group']
-      code <<-EOF
-        spawn #{android_bin} update sdk --no-ui --all --filter #{sdk_component}
-        set timeout 1800
-        expect {
-          -regexp "Do you accept the license '(#{node['android-sdk']['license']['white_list'].join('|')})'.*" {
-                exp_send "y\r"
-                exp_continue
-          }
-          -regexp "Do you accept the license '(#{node['android-sdk']['license']['black_list'].join('|')})'.*" {
-                exp_send "n\r"
-                exp_continue
-          }
-          "Do you accept the license '*-license-*'*" {
-                exp_send "#{node['android-sdk']['license']['default_answer']}\r"
-                exp_continue
-          }
-          eof
-        }
-      EOF
     end
   end
+else
+  Chef::Log.error("❌ Android SDK Manager nie został znaleziony: #{android_bin}")
+end
+
+# Kopiowanie dodatkowych skryptów
+directory node['android-sdk']['scripts']['path'] do
+  owner node['android-sdk']['scripts']['owner']
+  group node['android-sdk']['scripts']['group']
+  mode '755'
+  recursive true
+  action :create
 end
 
 %w(android-accept-licenses android-wait-for-emulator).each do |android_helper_script|
@@ -127,7 +119,3 @@ end
     mode '755'
   end
 end
-
-#
-#
-#include_recipe 'android-sdk::maven_rescue' if node['android-sdk']['maven_rescue']
