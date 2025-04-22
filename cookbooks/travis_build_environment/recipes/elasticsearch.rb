16351429d632e057d3cec902cc1bbfd6f13cdd9b
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-04-22 19:26:35 +0200

    elasticsearch fix

--- cookbooks/travis_build_environment/recipes/elasticsearch.rb
+++ cookbooks/travis_build_environment/recipes/elasticsearch.rb
@@ -1,3 +1,4 @@
+# recipes/elasticsearch.rb
 # frozen_string_literal: true
 
 package_name = node['travis_build_environment']['elasticsearch']['package_name']
@@ -8,26 +9,25 @@ deb_download_dest = File.join(
 
 remote_file deb_download_dest do
   source "https://artifacts.elastic.co/downloads/elasticsearch/#{package_name}"
-  owner node['travis_build_environment']['user']
-  group node['travis_build_environment']['group']
+  owner  node['travis_build_environment']['user']
+  group  node['travis_build_environment']['group']
+  mode   '0644'
 end
 
 dpkg_package package_name do
   source deb_download_dest
-  notifies :run, 'ruby_block[create-symbolic-links]'
   action :install
   not_if 'which elasticsearch'
+  notifies :run, 'ruby_block[create-symbolic-links]', :immediately
 end
 
 ruby_block 'create-symbolic-links' do
   block do
     Dir.foreach('/usr/share/elasticsearch/bin') do |file|
-      unless File.exist? "/usr/local/bin/#{file}"
-        File.symlink(
-          "/usr/share/elasticsearch/bin/#{file}",
-          "/usr/local/bin/#{file}"
-        )
-      end
+      next if file == '.' || file == '..'
+      src = "/usr/share/elasticsearch/bin/#{file}"
+      dst = "/usr/local/bin/#{file}"
+      File.symlink(src, dst) unless File.exist?(dst)
     end
   end
   action :nothing
@@ -35,20 +35,32 @@ end
 
 template '/etc/elasticsearch/jvm.options' do
   source 'etc-elasticsearch-jvm.options.erb'
-  owner node['travis_build_environment']['user']
-  group node['travis_build_environment']['group']
-  mode '644'
+  owner  node['travis_build_environment']['user']
+  group  node['travis_build_environment']['group']
+  mode   '0644'
   variables(
     jvm_heap: node['travis_build_environment']['elasticsearch']['jvm_heap']
   )
+  notifies :restart, 'service[elasticsearch]', :delayed
+end
+
+template '/etc/elasticsearch/elasticsearch.yml' do
+  source 'etc-elasticsearch-elasticsearch.yml.erb'
+  owner  node['travis_build_environment']['user']
+  group  node['travis_build_environment']['group']
+  mode   '0644'
+  variables(
+    service_enabled: node['travis_build_environment']['elasticsearch']['service_enabled']
+  )
+  notifies :restart, 'service[elasticsearch]', :delayed
 end
 
 service 'elasticsearch' do
   if node['travis_build_environment']['elasticsearch']['service_enabled']
     action %i(enable start)
   else
-    action %i(disable start)
+    action %i(disable stop)
   end
-  retries 4
+  retries     4
   retry_delay 30
 end
