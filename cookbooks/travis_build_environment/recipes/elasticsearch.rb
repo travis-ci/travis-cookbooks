f742a48c40a7f9d7ad19ff7a1c63f384ac0f1c25
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-04-25 11:57:52 +0200

    elasticsearch.rb final version

--- cookbooks/travis_build_environment/recipes/elasticsearch.rb
+++ cookbooks/travis_build_environment/recipes/elasticsearch.rb
@@ -1,11 +1,7 @@
-# recipes/elasticsearch.rb
 # frozen_string_literal: true
 
-package_name = node['travis_build_environment']['elasticsearch']['package_name']
-deb_download_dest = File.join(
-  Chef::Config[:file_cache_path],
-  package_name
-)
+package_name      = node['travis_build_environment']['elasticsearch']['package_name']
+deb_download_dest = File.join(Chef::Config[:file_cache_path], package_name)
 
 remote_file deb_download_dest do
   source "https://artifacts.elastic.co/downloads/elasticsearch/#{package_name}"
@@ -15,16 +11,17 @@ remote_file deb_download_dest do
 end
 
 dpkg_package package_name do
-  source deb_download_dest
-  action :install
-  not_if 'which elasticsearch'
-  notifies :run, 'ruby_block[create-symbolic-links]', :immediately
+  source     deb_download_dest
+  action     :install
+  not_if     'which elasticsearch'
+  notifies   :run, 'ruby_block[create-symbolic-links]', :immediately
+  notifies   :run, 'ruby_block[disable-xpack-security]', :immediately
 end
 
 ruby_block 'create-symbolic-links' do
   block do
     Dir.foreach('/usr/share/elasticsearch/bin') do |file|
-      next if file == '.' || file == '..'
+      next if %w(. ..).include?(file)
       src = "/usr/share/elasticsearch/bin/#{file}"
       dst = "/usr/local/bin/#{file}"
       File.symlink(src, dst) unless File.exist?(dst)
@@ -33,31 +30,48 @@ ruby_block 'create-symbolic-links' do
   action :nothing
 end
 
+ruby_block 'disable-xpack-security' do
+  block do
+    require 'chef/util/file_edit'
+    edit = Chef::Util::FileEdit.new('/etc/elasticsearch/elasticsearch.yml')
+    edit.search_file_replace_line(
+      /^xpack\.security\.enabled\s*:/,
+      'xpack.security.enabled: false'
+    )
+    edit.insert_line_if_no_match(
+      /^xpack\.security\.enabled\s*:/,
+      'xpack.security.enabled: false'
+    )
+    edit.write_file
+  end
+  action :nothing
+end
+
 template '/etc/elasticsearch/jvm.options' do
-  source 'etc-elasticsearch-jvm.options.erb'
-  owner  node['travis_build_environment']['user']
-  group  node['travis_build_environment']['group']
-  mode   '0644'
+  source   'etc-elasticsearch-jvm.options.erb'
+  owner    node['travis_build_environment']['user']
+  group    node['travis_build_environment']['group']
+  mode     '0644'
   variables(
     jvm_heap: node['travis_build_environment']['elasticsearch']['jvm_heap']
   )
-  notifies :restart, 'service[elasticsearch]', :delayed
 end
 
-template '/etc/elasticsearch/elasticsearch.yml' do
-  source 'etc-elasticsearch-elasticsearch.yml.erb'
-  owner  node['travis_build_environment']['user']
-  group  node['travis_build_environment']['group']
-  mode   '0644'
-  variables(
-    service_enabled: node['travis_build_environment']['elasticsearch']['service_enabled']
-  )
-  notifies :restart, 'service[elasticsearch]', :delayed
+ruby_block 'print-elasticsearch-config' do
+  block do
+    cfg = ::File.read('/etc/elasticsearch/elasticsearch.yml')
+    Chef::Log.info("=== Elasticsearch configuration (elasticsearch.yml) ===\n\n#{cfg}")
+  end
+  action :nothing
 end
 
 service 'elasticsearch' do
-  supports status: true, restart: true
-  action [:enable, :start]
+  if node['travis_build_environment']['elasticsearch']['service_enabled']
+    action %i(enable start)
+  else
+    action %i(disable start)
+  end
   retries     4
   retry_delay 30
+  notifies :run, 'ruby_block[print-elasticsearch-config]', :immediately
 end
