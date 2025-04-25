74180bf9c171fefcb950032df266336610519ff4
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-04-25 11:15:38 +0200

    elasticsearch.rb added more logs 3

--- cookbooks/travis_build_environment/recipes/elasticsearch.rb
+++ cookbooks/travis_build_environment/recipes/elasticsearch.rb
@@ -1,7 +1,11 @@
+# recipes/elasticsearch.rb
 # frozen_string_literal: true
 
-package_name       = node['travis_build_environment']['elasticsearch']['package_name']
-deb_download_dest  = File.join(Chef::Config[:file_cache_path], package_name)
+package_name = node['travis_build_environment']['elasticsearch']['package_name']
+deb_download_dest = File.join(
+  Chef::Config[:file_cache_path],
+  package_name
+)
 
 remote_file deb_download_dest do
   source "https://artifacts.elastic.co/downloads/elasticsearch/#{package_name}"
@@ -11,9 +15,9 @@ remote_file deb_download_dest do
 end
 
 dpkg_package package_name do
-  source  deb_download_dest
-  action  :install
-  not_if  'which elasticsearch'
+  source deb_download_dest
+  action :install
+  not_if 'which elasticsearch'
   notifies :run, 'ruby_block[create-symbolic-links]', :immediately
 end
 
@@ -37,6 +41,7 @@ template '/etc/elasticsearch/jvm.options' do
   variables(
     jvm_heap: node['travis_build_environment']['elasticsearch']['jvm_heap']
   )
+  notifies :restart, 'service[elasticsearch]', :delayed
 end
 
 template '/etc/elasticsearch/elasticsearch.yml' do
@@ -44,27 +49,15 @@ template '/etc/elasticsearch/elasticsearch.yml' do
   owner  node['travis_build_environment']['user']
   group  node['travis_build_environment']['group']
   mode   '0644'
+  variables(
+    service_enabled: node['travis_build_environment']['elasticsearch']['service_enabled']
+  )
+  notifies :restart, 'service[elasticsearch]', :delayed
 end
 
 service 'elasticsearch' do
-  action node['travis_build_environment']['elasticsearch']['service_enabled'] ? [:enable] : [:disable]
-end
-
-execute 'start elasticsearch' do
-  command 'systemctl start elasticsearch.service'
-  retries 4
-  retry_delay 90
-  not_if  'systemctl is-active --quiet elasticsearch.service'
-end
-
-execute 'log elasticsearch errors' do
-  command <<~EOH
-    echo "=== Elasticsearch service status ==="
-    systemctl status elasticsearch.service
-    echo
-    echo "=== Elasticsearch journal (last 50 lines) ==="
-    journalctl -xeu elasticsearch.service -n 50
-  EOH
-  action :run
-  only_if 'systemctl is-failed --quiet elasticsearch.service'
+  supports status: true, restart: true
+  action [:enable, :start]
+  retries     4
+  retry_delay 30
 end
