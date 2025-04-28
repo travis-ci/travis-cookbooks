ad1dcc834639e6e3bad1640dff714d3822dd313f
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-04-28 11:00:53 +0200

    bionic fix

--- cookbooks/travis_build_environment/recipes/elasticsearch.rb
+++ cookbooks/travis_build_environment/recipes/elasticsearch.rb
@@ -1,33 +1,83 @@
 # frozen_string_literal: true
 
 package_name      = node['travis_build_environment']['elasticsearch']['package_name']
-deb_download_dest = File.join(Chef::Config[:file_cache_path], package_name)
+deb_download_dest = ::File.join(Chef::Config[:file_cache_path], package_name)
 
-remote_file deb_download_dest do
-  source "https://artifacts.elastic.co/downloads/elasticsearch/#{package_name}"
-  owner  node['travis_build_environment']['user']
-  group  node['travis_build_environment']['group']
-  mode   '0644'
+log 'remote_file_downloaded' do
+  message "Elasticsearch package #{package_name} downloaded to #{deb_download_dest}"
+  level   :info
+  action  :nothing
 end
 
+log 'package_installed' do
+  message "Elasticsearch package #{package_name} installed"
+  level   :info
+  action  :nothing
+end
+
+log 'symlinks_created' do
+  message 'Elasticsearch CLI symlinks created in /usr/local/bin'
+  level   :info
+  action  :nothing
+end
+
+log 'jvm_template_applied' do
+  message '/etc/elasticsearch/jvm.options template applied'
+  level   :info
+  action  :nothing
+end
+
+log 'xpack_disabled' do
+  message 'xpack.security.enabled set to false in elasticsearch.yml'
+  level   :info
+  action  :nothing
+end
+
+log 'service_started' do
+  message 'Elasticsearch service enabled/disabled and started'
+  level   :info
+  action  :nothing
+end
+
+remote_file deb_download_dest do
+  source   "https://artifacts.elastic.co/downloads/elasticsearch/#{package_name}"
+  owner    node['travis_build_environment']['user']
+  group    node['travis_build_environment']['group']
+  mode     '0644'
+  not_if   { ::File.exist?(deb_download_dest) }
+  notifies :write, 'log[remote_file_downloaded]', :immediately
+end
+
+
 dpkg_package package_name do
-  source     deb_download_dest
-  action     :install
-  not_if     'which elasticsearch'
-  notifies   :run, 'ruby_block[create-symbolic-links]', :immediately
-  notifies   :run, 'ruby_block[disable-xpack-security]', :immediately
+  source   deb_download_dest
+  action   :install
+  not_if   'which elasticsearch'
+  notifies :run,   'ruby_block[create-symbolic-links]', :immediately
+  notifies :run,   'ruby_block[disable-xpack-security]', :immediately
+  notifies :write, 'log[package_installed]', :immediately
 end
 
 ruby_block 'create-symbolic-links' do
   block do
+    created = []
     Dir.foreach('/usr/share/elasticsearch/bin') do |file|
       next if %w(. ..).include?(file)
       src = "/usr/share/elasticsearch/bin/#{file}"
       dst = "/usr/local/bin/#{file}"
-      File.symlink(src, dst) unless File.exist?(dst)
+      unless ::File.exist?(dst)
+        File.symlink(src, dst)
+        created << file
+      end
+    end
+    if created.any?
+      Chef::Log.info("Symlinks created for: #{created.join(', ')}")
+    else
+      Chef::Log.info('No new symlinks needed; all already exist')
     end
   end
   action :nothing
+  notifies :write, 'log[symlinks_created]', :immediately
 end
 
 template '/etc/elasticsearch/jvm.options' do
@@ -38,7 +88,10 @@ template '/etc/elasticsearch/jvm.options' do
   variables(
     jvm_heap: node['travis_build_environment']['elasticsearch']['jvm_heap']
   )
+  notifies :write,   'log[jvm_template_applied]', :immediately
+  notifies :restart, 'service[elasticsearch]',       :delayed
 end
+
 
 ruby_block 'print-elasticsearch-config' do
   block do
@@ -61,8 +114,11 @@ ruby_block 'disable-xpack-security' do
       'xpack.security.enabled: false'
     )
     edit.write_file
+    Chef::Log.info('Patched elasticsearch.yml: xpack.security.enabled=false')
   end
   action :nothing
+  notifies :write,   'log[xpack_disabled]',           :immediately
+  notifies :restart, 'service[elasticsearch]',       :delayed
 end
 
 service 'elasticsearch' do
@@ -73,5 +129,6 @@ service 'elasticsearch' do
   end
   retries     4
   retry_delay 30
-  notifies :run, 'ruby_block[print-elasticsearch-config]', :immediately
+  notifies :run,   'ruby_block[print-elasticsearch-config]', :immediately
+  notifies :write, 'log[service_started]',                   :immediately
 end
